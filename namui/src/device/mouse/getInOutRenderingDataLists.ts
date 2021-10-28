d9308e482db4d6bd41569b1c06f8c648dcc6454b
namse <skatpgusskat@naver.com>
2021-10-29 03:43:34 +0900

    Support image mouse in/out

--- namui/src/device/mouse/getInOutRenderingDataLists.ts
+++ namui/src/device/mouse/getInOutRenderingDataLists.ts
@@ -55,34 +55,40 @@ function isVectorInDrawCommand(
   drawCommand: DrawCommand,
   vector: Vector,
 ): boolean {
-  if ("path" in drawCommand) {
-    const { paint } = drawCommand;
-    const path = drawCommand.path.copy();
-    try {
-      if (path.contains(vector.x, vector.y)) {
-        return true;
-      }
+  switch (drawCommand.type) {
+    case "path": {
+      const { paint } = drawCommand;
+      const path = drawCommand.path.copy();
+      try {
+        if (path.contains(vector.x, vector.y)) {
+          return true;
+        }
 
-      const stroked = path.stroke({
-        cap: paint.getStrokeCap(),
-        join: paint.getStrokeJoin(),
-        width: paint.getStrokeWidth(),
-        miter_limit: paint.getStrokeMiter(),
-      });
-      // NOTE : I'm not sure this is right way to handle stroke information of paint.
-      if (!stroked) {
-        return false;
+        const stroked = path.stroke({
+          cap: paint.getStrokeCap(),
+          join: paint.getStrokeJoin(),
+          width: paint.getStrokeWidth(),
+          miter_limit: paint.getStrokeMiter(),
+        });
+        // NOTE : I'm not sure this is right way to handle stroke information of paint.
+        if (!stroked) {
+          return false;
+        }
+        const result = stroked.contains(vector.x, vector.y);
+        return result;
+      } finally {
+        path?.delete();
       }
-      const result = stroked.contains(vector.x, vector.y);
-      return result;
-    } finally {
-      path?.delete();
     }
+    case "image": {
+      return (
+        drawCommand.x <= vector.x &&
+        vector.x <= drawCommand.x + drawCommand.size.width &&
+        drawCommand.y <= vector.y &&
+        vector.y <= drawCommand.y + drawCommand.size.height
+      );
+    }
+    // TODO : Support for text
   }
-
-  console.debug(
-    "this type of drawCommand is not supported yet for checking vector being in",
-    drawCommand,
-  );
   return false;
 }
