929c2321ea3a3a58246274a73e3b2c192860b28a
namse <skatpgusskat@naver.com>
2021-11-04 19:13:04 +0900

    Add some xywh functions in Mathu

--- namui/src/Mathu/Mathu.ts
+++ namui/src/Mathu/Mathu.ts
@@ -1,6 +1,21 @@
+import { XywhRect, Vector } from "..";
 import { IMathu } from "./IMathu";
 
 export class Mathu implements IMathu {
+  translate(rect: XywhRect, vector: Vector): XywhRect {
+    return {
+      x: rect.x + vector.x,
+      y: rect.y + vector.y,
+      width: rect.width,
+      height: rect.height,
+    };
+  }
+  contains(rect: XywhRect, point: Vector): boolean {
+    return (
+      this.in(point.x, rect.x, rect.x + rect.width) &&
+      this.in(point.y, rect.y, rect.y + rect.height)
+    );
+  }
   clamp(value: number, min: number, max: number): number {
     return Math.min(Math.max(value, min), max);
   }
