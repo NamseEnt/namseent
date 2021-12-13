0594543f41523c2fdc3e90584960eb4e5a48f9d5
namse <skatpgusskat@naver.com>
2021-12-13 19:12:02 +0900

    Move namui rust to root

--- namui/src/lib.rs
+++ namui/src/lib.rs
@@ -4,12 +4,6 @@ use crate::engine::{RectParam, RectStroke, RectStyle};
 use std::any::{Any, TypeId};
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
