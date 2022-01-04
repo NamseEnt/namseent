69e5c18279449dab38304e8d37524802c523e787
namse <skatpgusskat@naver.com>
2022-01-04 20:30:30 +0900

    Prevent key down default for alt key only

--- namui/src/namui/manager/web/keyboard_manager/mod.rs
+++ namui/src/namui/manager/web/keyboard_manager/mod.rs
@@ -32,7 +32,9 @@ impl KeyboardManager {
             let code_string = event.code();
             let code = Code::from_str(&code_string).unwrap();
             pressing_code_set_key_down.write().unwrap().insert(code);
-            event.prevent_default();
+            if event.key() == "Alt" {
+                event.prevent_default();
+            }
             namui::log(format!("key down: {}", code_string));
         }) as Box<dyn FnMut(_)>);
 
