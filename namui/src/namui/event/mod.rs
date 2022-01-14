1d73a565ac0e2e6c6fa3d1ec0fad90ee010ed4d5
namse <skatpgusskat@naver.com>
2022-01-14 17:17:43 +0900

    Test event system

--- namui/src/namui/event/mod.rs
+++ namui/src/namui/event/mod.rs
@@ -24,3 +24,20 @@ pub enum NamuiEvent {
     ScreenResize(namui::Wh<i16>),
     Wheel(namui::Xy<f32>),
 }
+
+#[cfg(test)]
+mod tests {
+    #[tokio::test]
+    #[wasm_bindgen_test::wasm_bindgen_test]
+    async fn received_event_should_be_able_to_downcast() {
+        let mut event_receiver = super::init();
+        #[derive(Debug, PartialEq)]
+        enum Event {
+            Test,
+        }
+        super::send(Event::Test);
+        let event = event_receiver.recv().await.unwrap();
+        let downcasted = event.downcast_ref::<Event>().unwrap();
+        assert_eq!(downcasted, &Event::Test);
+    }
+}
