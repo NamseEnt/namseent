62d21dead5ae7cda71bb7a366c8702e8baba7d6d
namse <skatpgusskat@naver.com>
2022-01-24 22:48:53 +0900

    Set up test

--- luda-editor/luda-editor-client/src/lib.rs
+++ luda-editor/luda-editor-client/src/lib.rs
@@ -1,6 +1,10 @@
 use wasm_bindgen::prelude::*;
 mod app;
 
+#[cfg(test)]
+#[cfg(target_family = "wasm")]
+wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+
 #[wasm_bindgen]
 pub async fn start() {
     app::main().await;
