e8ac3c100f973ac8c108db9aadb3bc9122fd266b
bigfoodK <38313680+bigfoodK@users.noreply.github.com>
2022-01-26 01:55:29 +0900

    Add go-back button in editor page (#94)
    * Add go-back button in editor page
    
    * Remove space between imports
    
    * Revert import changes while rebase

---
+++ luda-editor/luda-editor-client/src/app/editor/top_bar/go_back_button.rs
@@ -0,0 +1,44 @@
+use crate::app::editor::top_bar::events::TopBarEvent;
+use namui::{
+    render, Color, FontType, FontWeight, Language, RectFill, RectParam, RectRound, RectStyle,
+    RenderingTree, TextAlign, TextBaseline, TextStyle, Wh,
+};
+
+pub fn render_go_back_button(wh: Wh<f32>) -> RenderingTree {
+    render![
+        namui::rect(RectParam {
+            x: 0.0,
+            y: 0.0,
+            width: wh.width,
+            height: wh.height,
+            style: RectStyle {
+                stroke: None,
+                fill: Some(RectFill {
+                    color: Color::from_u8(107, 185, 240, 255),
+                }),
+                round: Some(RectRound { radius: 4.0 }),
+            },
+        })
+        .with_mouse_cursor(namui::MouseCursor::Pointer)
+        .attach_event(move |builder| {
+            builder.on_mouse_down(move |_| namui::event::send(TopBarEvent::GoBackButtonClicked))
+        }),
+        namui::text(namui::TextParam {
+            text: "Go back".to_string(),
+            x: wh.width / 2.0,
+            y: wh.height / 2.0,
+            align: TextAlign::Center,
+            baseline: TextBaseline::Middle,
+            font_type: FontType {
+                serif: false,
+                size: (wh.height / 3.0 * 2.0) as i16,
+                language: Language::Ko,
+                font_weight: FontWeight::REGULAR,
+            },
+            style: TextStyle {
+                color: Color::from_u8(255, 255, 255, 255),
+                ..Default::default()
+            },
+        })
+    ]
+}
--- luda-editor/luda-editor-client/src/app/events.rs
+++ luda-editor/luda-editor-client/src/app/events.rs
@@ -4,7 +4,5 @@ type PageInitializer<T> = Box<dyn Fn(&AppContext) -> T + Send + Sync>;
 
 pub enum RouterEvent {
     PageChangeToEditorEvent(PageInitializer<Editor>),
-    // TODO: this gonna be used to make back button in editor
-    #[allow(dead_code)]
     PageChangeToSequenceListEvent(PageInitializer<SequenceList>),
 }
