baff345dd98ebd34b8c69ed8dc18901d4895c734
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2022-02-03 23:32:24 +0900

    Display error message on fetch fail

--- namui/src/namui/namui_web/fetch.rs
+++ namui/src/namui/namui_web/fetch.rs
@@ -10,20 +10,22 @@ pub async fn fetch_get(url: &str) -> Result<Response, String> {
     let request = Request::new_with_str_and_init(url, &options).unwrap();
 
     let window = web_sys::window().unwrap();
-    let response_value = JsFuture::from(window.fetch_with_request(&request))
-        .await
-        .unwrap();
-    assert!(response_value.is_instance_of::<Response>());
-    let response: Response = response_value.dyn_into().unwrap();
+    match JsFuture::from(window.fetch_with_request(&request)).await {
+        Ok(response_value) => {
+            assert!(response_value.is_instance_of::<Response>());
+            let response: Response = response_value.dyn_into().unwrap();
 
-    if !response.ok() {
-        return Err(response.status_text());
+            if !response.ok() {
+                return Err(response.status_text());
+            }
+            Result::Ok(response)
+        }
+        Err(error) => return Err(format!("fail to fetch {} - {:?}", url, error)),
     }
-    Result::Ok(response)
 }
 
 pub async fn fetch_get_array_buffer(url: &str) -> Result<ArrayBuffer, String> {
-    let response: Response = fetch_get(&url).await.unwrap();
+    let response: Response = fetch_get(&url).await?;
 
     let array_buffer = JsFuture::from(response.array_buffer().unwrap())
         .await
@@ -35,7 +37,7 @@ pub async fn fetch_get_array_buffer(url: &str) -> Result<ArrayBuffer, String> {
 }
 
 pub async fn fetch_get_json<T: for<'a> serde::Deserialize<'a>>(url: &str) -> Result<T, String> {
-    let response: Response = fetch_get(&url).await.unwrap();
+    let response: Response = fetch_get(&url).await?;
 
     let json = JsFuture::from(response.json().unwrap()).await.unwrap();
 
@@ -43,7 +45,7 @@ pub async fn fetch_get_json<T: for<'a> serde::Deserialize<'a>>(url: &str) -> Res
 }
 
 pub async fn fetch_get_vec_u8(url: &str) -> Result<Vec<u8>, String> {
-    let array_buffer = fetch_get_array_buffer(url).await.unwrap();
+    let array_buffer = fetch_get_array_buffer(url).await?;
     let array_buffer_view = Uint8Array::new(&array_buffer);
     let bytes = array_buffer_view.to_vec();
     Result::Ok(bytes)
