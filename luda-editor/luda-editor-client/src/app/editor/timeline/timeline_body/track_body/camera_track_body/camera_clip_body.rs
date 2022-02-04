d72374215950a4a34b4356ef83abcd9a251ca9f4
namse <skatpgusskat@naver.com>
2022-02-04 20:16:13 +0900

    Restore default caemra clip background

--- luda-editor/luda-editor-client/src/app/editor/timeline/timeline_body/track_body/camera_track_body/camera_clip_body.rs
+++ luda-editor/luda-editor-client/src/app/editor/timeline/timeline_body/track_body/camera_track_body/camera_clip_body.rs
@@ -29,51 +29,69 @@ impl CameraClipBody {
         };
         let is_highlight = props.context.selected_clip_ids.contains(&&props.clip.id);
 
+        let background = namui::rect(namui::RectParam {
+            x: clip_rect.x,
+            y: clip_rect.y,
+            width: clip_rect.width,
+            height: clip_rect.height,
+            style: namui::RectStyle {
+                fill: Some(namui::RectFill {
+                    color: namui::Color::from_f01(0.4, 0.4, 0.8, 1.0),
+                }),
+                round: Some(namui::RectRound {
+                    radius: CAMERA_CLIP_ROUND_RADIUS,
+                }),
+                ..Default::default()
+            },
+            ..Default::default()
+        });
+
+        let border = namui::rect(namui::RectParam {
+            x: clip_rect.x,
+            y: clip_rect.y,
+            width: clip_rect.width,
+            height: clip_rect.height,
+            style: namui::RectStyle {
+                stroke: Some(if is_highlight {
+                    namui::RectStroke {
+                        color: namui::Color::RED,
+                        width: 3.0,
+                        border_position: namui::BorderPosition::Inside,
+                    }
+                } else {
+                    namui::RectStroke {
+                        color: namui::Color::BLACK,
+                        width: 1.0,
+                        border_position: namui::BorderPosition::Inside,
+                    }
+                }),
+                round: Some(namui::RectRound {
+                    radius: CAMERA_CLIP_ROUND_RADIUS,
+                }),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .attach_event(move |builder| {
+            let clip_id = props.clip.id.clone();
+            builder.on_mouse_down(move |event| {
+                let event = EditorEvent::CameraClipBodyMouseDownEvent {
+                    clip_id: clip_id.clone(),
+                    click_in_time: timeline_start_at + PixelSize(event.local_xy.x) * time_per_pixel,
+                };
+                namui::event::send(event);
+            })
+        });
+
         namui::render![
+            background,
             render_camera_clip_preview(
                 &clip_rect,
                 props.track_body_wh.width,
                 &props.clip.camera_angle,
                 LudaEditorServerCameraAngleImageLoader {},
             ),
-            namui::rect(namui::RectParam {
-                x: clip_rect.x,
-                y: clip_rect.y,
-                width: clip_rect.width,
-                height: clip_rect.height,
-                style: namui::RectStyle {
-                    fill: None,
-                    stroke: Some(if is_highlight {
-                        namui::RectStroke {
-                            color: namui::Color::RED,
-                            width: 3.0,
-                            border_position: namui::BorderPosition::Inside,
-                        }
-                    } else {
-                        namui::RectStroke {
-                            color: namui::Color::BLACK,
-                            width: 1.0,
-                            border_position: namui::BorderPosition::Inside,
-                        }
-                    }),
-                    round: Some(namui::RectRound {
-                        radius: CAMERA_CLIP_ROUND_RADIUS,
-                    }),
-                    ..Default::default()
-                },
-                ..Default::default()
-            })
-            .attach_event(move |builder| {
-                let clip_id = props.clip.id.clone();
-                builder.on_mouse_down(move |event| {
-                    let event = EditorEvent::CameraClipBodyMouseDownEvent {
-                        clip_id: clip_id.clone(),
-                        click_in_time: timeline_start_at
-                            + PixelSize(event.local_xy.x) * time_per_pixel,
-                    };
-                    namui::event::send(event);
-                })
-            }),
+            border,
         ]
     }
 }
