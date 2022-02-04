2c1b97d5fa336b37602da35e64987aa223f4c5b7
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2022-02-04 22:44:11 +0900

    Show camera clip preview on timeline (#108)
    * Show camera clip preview on timeline
    
    * Restore default caemra clip background
    
    * Move out background from canera angle to preview

--- luda-editor/luda-editor-client/src/app/editor/timeline/timeline_body/track_body/camera_track_body/camera_clip_body.rs
+++ luda-editor/luda-editor-client/src/app/editor/timeline/timeline_body/track_body/camera_track_body/camera_clip_body.rs
@@ -1,6 +1,6 @@
 use crate::app::{
     editor::{events::EditorEvent, TimelineRenderContext},
-    types::{CameraClip, PixelSize},
+    types::*,
 };
 use namui::prelude::*;
 
@@ -10,6 +10,8 @@ pub struct CameraClipBodyProps<'a> {
     pub clip: &'a CameraClip,
     pub context: &'a TimelineRenderContext<'a>,
 }
+const CAMERA_CLIP_ROUND_RADIUS: f32 = 5.0;
+
 impl CameraClipBody {
     pub fn render(props: &CameraClipBodyProps) -> RenderingTree {
         let timeline_start_at = props.context.start_at;
@@ -27,7 +29,7 @@ impl CameraClipBody {
         };
         let is_highlight = props.context.selected_clip_ids.contains(&&props.clip.id);
 
-        namui::rect(namui::RectParam {
+        let background = namui::rect(namui::RectParam {
             x: clip_rect.x,
             y: clip_rect.y,
             width: clip_rect.width,
@@ -36,6 +38,20 @@ impl CameraClipBody {
                 fill: Some(namui::RectFill {
                     color: namui::Color::from_f01(0.4, 0.4, 0.8, 1.0),
                 }),
+                round: Some(namui::RectRound {
+                    radius: CAMERA_CLIP_ROUND_RADIUS,
+                }),
+                ..Default::default()
+            },
+            ..Default::default()
+        });
+
+        let border = namui::rect(namui::RectParam {
+            x: clip_rect.x,
+            y: clip_rect.y,
+            width: clip_rect.width,
+            height: clip_rect.height,
+            style: namui::RectStyle {
                 stroke: Some(if is_highlight {
                     namui::RectStroke {
                         color: namui::Color::RED,
@@ -49,7 +65,9 @@ impl CameraClipBody {
                         border_position: namui::BorderPosition::Inside,
                     }
                 }),
-                round: Some(namui::RectRound { radius: 5.0 }),
+                round: Some(namui::RectRound {
+                    radius: CAMERA_CLIP_ROUND_RADIUS,
+                }),
                 ..Default::default()
             },
             ..Default::default()
@@ -63,6 +81,92 @@ impl CameraClipBody {
                 };
                 namui::event::send(event);
             })
-        })
+        });
+
+        namui::render![
+            background,
+            render_camera_clip_preview(
+                &clip_rect,
+                props.track_body_wh.width,
+                &props.clip.camera_angle,
+                LudaEditorServerCameraAngleImageLoader {},
+            ),
+            border,
+        ]
+    }
+}
+
+fn render_camera_clip_preview(
+    camera_clip_rect: &XywhRect<f32>,
+    track_body_width: f32,
+    camera_angle: &CameraAngle,
+    camera_angle_image_loader: impl CameraAngleImageLoader,
+) -> RenderingTree {
+    let xywh: XywhRect<f32> = get_camera_clip_preview_xywh(camera_clip_rect, track_body_width);
+
+    if xywh.width <= 8.0 {
+        return RenderingTree::Empty;
+    }
+    let width_by_fixed_height = xywh.height * 16.0 / 9.0;
+
+    let letter_box_half_width = (width_by_fixed_height - xywh.width) / 2.0;
+    let background = rect(RectParam {
+        x: 0.0,
+        y: 0.0,
+        width: width_by_fixed_height,
+        height: xywh.height,
+        style: RectStyle {
+            fill: Some(RectFill {
+                color: Color::WHITE,
+            }),
+
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+
+    translate(
+        xywh.x - letter_box_half_width,
+        xywh.y,
+        clip(
+            PathBuilder::new().add_rrect(
+                &LtrbRect {
+                    left: letter_box_half_width,
+                    top: 0.0,
+                    right: xywh.width + letter_box_half_width,
+                    bottom: xywh.height,
+                },
+                CAMERA_CLIP_ROUND_RADIUS,
+                CAMERA_CLIP_ROUND_RADIUS,
+            ),
+            ClipOp::Intersect,
+            render![
+                background,
+                camera_angle.render(
+                    &Wh {
+                        width: width_by_fixed_height,
+                        height: xywh.height,
+                    },
+                    &camera_angle_image_loader,
+                ),
+            ],
+        ),
+    )
+}
+
+fn get_camera_clip_preview_xywh(
+    camera_clip_rect: &XywhRect<f32>,
+    track_body_width: f32,
+) -> XywhRect<f32> {
+    // NOTE : The coordinate is based on the timeline.start_at as a zero point.
+    let camera_clip_right = camera_clip_rect.x + camera_clip_rect.width;
+    let preview_right = camera_clip_right.min(track_body_width);
+    let preview_x = camera_clip_rect.x.max(0.0);
+    let preview_width = preview_right - preview_x;
+    XywhRect {
+        x: preview_x,
+        y: camera_clip_rect.y,
+        width: preview_width,
+        height: camera_clip_rect.height,
     }
 }
