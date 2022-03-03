17056ce828dd643e4e31b2b4db2a69f71107f88c
namse <skatpgusskat@naver.com>
2022-03-04 02:58:22 +0900

    Draw background on camera angle rendering

--- luda-editor/luda-editor-client/src/app/types/camera_angle.rs
+++ luda-editor/luda-editor-client/src/app/types/camera_angle.rs
@@ -28,6 +28,59 @@ impl CameraAngleCharacter {
             },
         }
     }
+    pub fn render(
+        &self,
+        wh: &Wh<f32>,
+        camera_angle_image_loader: &dyn CameraAngleImageLoader,
+    ) -> RenderingTree {
+        let image_source = camera_angle_image_loader.get_character_image_source(self);
+        let image = match image_source {
+            ImageSource::Url(url) => namui::managers().image_manager.clone().try_load(&url),
+            ImageSource::Image(image) => Some(image),
+        };
+        if image.is_none() {
+            return RenderingTree::Empty;
+        }
+        let image = image.unwrap();
+
+        let clip_rect = LtrbRect {
+            left: num::clamp(self.crop_screen_01_rect.left, 0.0, 1.0) * wh.width,
+            top: num::clamp(self.crop_screen_01_rect.top, 0.0, 1.0) * wh.height,
+            right: num::clamp(self.crop_screen_01_rect.right, 0.0, 1.0) * wh.width,
+            bottom: num::clamp(self.crop_screen_01_rect.bottom, 0.0, 1.0) * wh.height,
+        };
+
+        let image_length = self.source_01_circumscribed.radius * 2.0 * wh.length();
+        let image_source_size = image.size();
+        let image_source_length = image_source_size.length();
+        let image_size = Wh {
+            width: image_source_size.width * (image_length / image_source_length),
+            height: image_source_size.height * (image_length / image_source_length),
+        };
+        let image_center = Xy {
+            x: self.source_01_circumscribed.center.x * wh.width,
+            y: self.source_01_circumscribed.center.y * wh.height,
+        };
+        let image_xywh = XywhRect {
+            x: image_center.x - image_size.width / 2.0,
+            y: image_center.y - image_size.height / 2.0,
+            width: image_size.width,
+            height: image_size.height,
+        };
+
+        clip(
+            PathBuilder::new().add_rect(&clip_rect),
+            ClipOp::Intersect,
+            namui::image(ImageParam {
+                source: ImageSource::Image(image),
+                xywh: image_xywh,
+                style: ImageStyle {
+                    fit: ImageFit::Fill,
+                    paint_builder: None,
+                },
+            }),
+        )
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -44,6 +97,57 @@ impl CameraAngleBackground {
                 radius: 0.5,
             },
         }
+    }
+    pub fn render(
+        &self,
+        wh: &Wh<f32>,
+        camera_angle_image_loader: &dyn CameraAngleImageLoader,
+    ) -> RenderingTree {
+        let image_source = camera_angle_image_loader.get_background_image_source(self);
+        let image = match image_source {
+            ImageSource::Url(url) => namui::managers().image_manager.clone().try_load(&url),
+            ImageSource::Image(image) => Some(image),
+        };
+        if image.is_none() {
+            return RenderingTree::Empty;
+        }
+        let image = image.unwrap();
+
+        let clip_rect = LtrbRect {
+            left: 0.0,
+            top: 0.0,
+            right: wh.width,
+            bottom: wh.height,
+        };
+
+        let image_source_size = image.size();
+        let image_source_length = image_source_size.length();
+        let circumscribed_to_wh_ratio =
+            wh.length() / (self.source_01_circumscribed.radius * 2.0 * image_source_length);
+        let zoomed_size = Wh {
+            width: image_source_size.width * circumscribed_to_wh_ratio,
+            height: image_source_size.height * circumscribed_to_wh_ratio,
+        };
+
+        let image_xywh = XywhRect {
+            x: -self.source_01_circumscribed.center.x * zoomed_size.width + wh.width / 2.0,
+            y: -self.source_01_circumscribed.center.y * zoomed_size.height + wh.height / 2.0,
+            width: zoomed_size.width,
+            height: zoomed_size.height,
+        };
+
+        clip(
+            PathBuilder::new().add_rect(&clip_rect),
+            ClipOp::Intersect,
+            namui::image(ImageParam {
+                source: ImageSource::Image(image),
+                xywh: image_xywh,
+                style: ImageStyle {
+                    fit: ImageFit::Fill,
+                    paint_builder: None,
+                },
+            }),
+        )
     }
 }
 
@@ -77,58 +181,17 @@ impl CameraAngle {
         wh: &Wh<f32>,
         camera_angle_image_loader: &dyn CameraAngleImageLoader,
     ) -> RenderingTree {
-        let character = self.character.as_ref();
-        if character.is_none() {
-            return RenderingTree::Empty;
-        }
-        let character = character.unwrap();
-
-        let image_source = camera_angle_image_loader.get_character_image_source(character);
-        let image = match image_source {
-            ImageSource::Url(url) => namui::managers().image_manager.clone().try_load(&url),
-            ImageSource::Image(image) => Some(image),
-        };
-        if image.is_none() {
-            return RenderingTree::Empty;
-        }
-        let image = image.unwrap();
-
-        let clip_rect = LtrbRect {
-            left: num::clamp(character.crop_screen_01_rect.left, 0.0, 1.0) * wh.width,
-            top: num::clamp(character.crop_screen_01_rect.top, 0.0, 1.0) * wh.height,
-            right: num::clamp(character.crop_screen_01_rect.right, 0.0, 1.0) * wh.width,
-            bottom: num::clamp(character.crop_screen_01_rect.bottom, 0.0, 1.0) * wh.height,
-        };
-
-        let image_length = character.source_01_circumscribed.radius * 2.0 * wh.length();
-        let image_source_size = image.size();
-        let image_source_length = image_source_size.length();
-        let image_size = Wh {
-            width: image_source_size.width * (image_length / image_source_length),
-            height: image_source_size.height * (image_length / image_source_length),
-        };
-        let image_center = Xy {
-            x: character.source_01_circumscribed.center.x * wh.width,
-            y: character.source_01_circumscribed.center.y * wh.height,
-        };
-        let image_xywh = XywhRect {
-            x: image_center.x - image_size.width / 2.0,
-            y: image_center.y - image_size.height / 2.0,
-            width: image_size.width,
-            height: image_size.height,
-        };
-
-        clip(
-            PathBuilder::new().add_rect(&clip_rect),
-            ClipOp::Intersect,
-            namui::image(ImageParam {
-                source: ImageSource::Image(image),
-                xywh: image_xywh,
-                style: ImageStyle {
-                    fit: ImageFit::Fill,
-                    paint_builder: None,
-                },
-            }),
-        )
+        render![
+            self.background
+                .as_ref()
+                .map_or(RenderingTree::Empty, |background| {
+                    background.render(wh, camera_angle_image_loader)
+                }),
+            self.character
+                .as_ref()
+                .map_or(RenderingTree::Empty, |character| {
+                    character.render(wh, camera_angle_image_loader)
+                }),
+        ]
     }
 }
