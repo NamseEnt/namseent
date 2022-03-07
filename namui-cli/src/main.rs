e1701fd0c1ce2e1d7d9c0d986323af039f8477d6
namse <skatpgusskat@naver.com>
2022-03-08 05:26:01 +0900

    Remove all cli matches

--- namui-cli/src/main.rs
+++ namui-cli/src/main.rs
@@ -1,30 +1,15 @@
 mod procedures;
 mod services;
 mod util;
-use clap::{App, Arg};
 use procedures::dev_wasm_web;
-use std::{env::current_dir, path::PathBuf};
+use std::env::current_dir;
 mod types;
 
 #[tokio::main]
 async fn main() {
-    let matches = App::new("Namui")
-        .arg(
-            Arg::with_name("manifest_path")
-                .help("Target directory to run 'cargo build'. Mostly, root dir of crate")
-                .index(1)
-                .required(false),
-        )
-        .get_matches();
-
-    let manifest_path = matches
-        .value_of("manifest_path")
-        .map(|manifest_path| PathBuf::from(manifest_path))
-        .unwrap_or(
-            current_dir()
-                .expect("No current dir found")
-                .join("Cargo.toml"),
-        );
+    let manifest_path = current_dir()
+        .expect("No current dir found")
+        .join("Cargo.toml");
 
     let result = dev_wasm_web(&manifest_path);
 
