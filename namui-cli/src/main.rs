d900055d2bf01b62300ba41173208761e7c94f8a
bigfood <satcom1600@hanmail.net>
2022-03-29 17:53:53 +0900

    Split cli code from main.rs

--- namui-cli/src/main.rs
+++ namui-cli/src/main.rs
@@ -1,31 +1,12 @@
+mod cli;
 mod procedures;
 mod services;
-mod util;
-use procedures::{dev_wasm_electron, dev_wasm_web, release_wasm_electron, release_wasm_web};
-use services::electron_package_service::{Arch, Platform};
-use std::{env::current_dir, str::FromStr};
 mod types;
-use clap::{ArgEnum, Parser, Subcommand};
-
-#[derive(Parser)]
-#[clap(version)]
-struct Cli {
-    #[clap(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    DevWasmWeb {},
-    DevWasmElectron {},
-    ReleaseWasmWeb {},
-    ReleaseWasmElectron {
-        #[clap(short, long, arg_enum, default_value = "auto")]
-        platform: ElectronPackagePlatform,
-        #[clap(short, long, arg_enum, default_value = "auto")]
-        arch: ElectronPackageArch,
-    },
-}
+mod util;
+use clap::StructOpt;
+use cli::{Cli, Commands};
+use procedures::{dev_wasm_electron, dev_wasm_web, release_wasm_electron, release_wasm_web};
+use std::env::current_dir;
 
 #[tokio::main]
 async fn main() {
@@ -48,61 +29,6 @@ async fn main() {
         Err(error) => {
             eprintln!("{}", error);
             std::process::exit(1);
-        }
-    }
-}
-
-#[derive(Clone, ArgEnum)]
-#[clap(short, long)]
-enum ElectronPackagePlatform {
-    #[clap(rename_all = "camelCase")]
-    Auto,
-    Win32,
-    Linux,
-}
-impl FromStr for ElectronPackagePlatform {
-    type Err = String;
-    fn from_str(tr: &str) -> Result<Self, Self::Err> {
-        match tr {
-            "auto" => Ok(Self::Auto),
-            "win32" => Ok(Self::Win32),
-            "linux" => Ok(Self::Linux),
-            _ => Err(format!("Unknown platform")),
-        }
-    }
-}
-impl Into<Option<Platform>> for &ElectronPackagePlatform {
-    fn into(self) -> Option<Platform> {
-        match self {
-            ElectronPackagePlatform::Auto => None,
-            ElectronPackagePlatform::Win32 => Some(Platform::Win32),
-            ElectronPackagePlatform::Linux => Some(Platform::Linux),
-        }
-    }
-}
-
-#[derive(Clone, ArgEnum)]
-#[clap(short, long)]
-enum ElectronPackageArch {
-    #[clap(rename_all = "camelCase")]
-    Auto,
-    X64,
-}
-impl FromStr for ElectronPackageArch {
-    type Err = String;
-    fn from_str(tr: &str) -> Result<Self, Self::Err> {
-        match tr {
-            "auto" => Ok(Self::Auto),
-            "win32" => Ok(Self::X64),
-            _ => Err(format!("Unknown arch")),
-        }
-    }
-}
-impl Into<Option<Arch>> for &ElectronPackageArch {
-    fn into(self) -> Option<Arch> {
-        match self {
-            ElectronPackageArch::Auto => None,
-            ElectronPackageArch::X64 => Some(Arch::X64),
         }
     }
 }
