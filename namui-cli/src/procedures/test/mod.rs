b876ea66dd567e1c45737fc96f360e45609633ce
namse <skatpgusskat@naver.com>
2022-04-18 02:00:59 +0900

    Test with docker

--- namui-cli/src/procedures/test/mod.rs
+++ namui-cli/src/procedures/test/mod.rs
@@ -2,10 +2,12 @@ use crate::cli::Target;
 use std::{error::Error, path::PathBuf};
 
 pub fn test(target: &Target, manifest_path: &PathBuf) -> Result<(), Box<dyn Error>> {
+    let manifest_path = std::fs::canonicalize(manifest_path)?;
+
     if cfg!(target_os = "linux") {
         use super::linux;
         match target {
-            Target::WasmUnknownWeb => linux::wasm_unknown_web::test(manifest_path),
+            Target::WasmUnknownWeb => linux::wasm_unknown_web::test(&manifest_path),
         }
     } else {
         Result::Err(format!("{} is unsupported os", std::env::consts::OS).into())
