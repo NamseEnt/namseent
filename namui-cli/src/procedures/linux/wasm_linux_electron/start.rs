29bdba07f699ea8b4ea63c683a7a688097d8e5eb
bigfood <satcom1600@hanmail.net>
2022-05-02 21:24:13 +0900

    Serve bundle through BundleWebServer

--- namui-cli/src/procedures/linux/wasm_linux_electron/start.rs
+++ namui-cli/src/procedures/linux/wasm_linux_electron/start.rs
@@ -2,6 +2,7 @@ use crate::{
     cli::Target,
     debug_println,
     services::{
+        bundle_metadata_service::BundleMetadataService,
         electron_dev_service::{start_electron_dev_service, CrossPlatform},
         rust_build_service::{BuildOption, BuildResult, RustBuildService},
         rust_project_watch_service::RustProjectWatchService,
@@ -24,8 +25,10 @@ pub fn start(manifest_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let build_dist_path = manifest_path.parent().unwrap().join("pkg");
     let project_root_path = manifest_path.parent().unwrap().to_path_buf();
 
+    let bundle_metadata_service = Arc::new(BundleMetadataService::new());
     let rust_project_watch_service = Arc::new(RustProjectWatchService::new());
-    let wasm_bundle_web_server = WasmBundleWebServer::start(PORT, &build_dist_path);
+    let wasm_bundle_web_server =
+        WasmBundleWebServer::start(PORT, &build_dist_path, bundle_metadata_service.clone());
     let rust_build_service = Arc::new(RustBuildService::new());
 
     tokio::spawn(build(
@@ -33,6 +36,7 @@ pub fn start(manifest_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
         rust_build_service.clone(),
         build_dist_path.clone(),
         project_root_path.clone(),
+        bundle_metadata_service.clone(),
     ));
     rust_project_watch_service.watch(manifest_path, {
         let wasm_bundle_web_server = wasm_bundle_web_server.clone();
@@ -45,6 +49,7 @@ pub fn start(manifest_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
                 rust_build_service.clone(),
                 build_dist_path.clone(),
                 project_root_path.clone(),
+                bundle_metadata_service.clone(),
             ));
         }
     })?;
@@ -57,6 +62,7 @@ async fn build(
     rust_build_service: Arc<RustBuildService>,
     build_dist_path: PathBuf,
     project_root_path: PathBuf,
+    bundle_metadata_service: Arc<BundleMetadataService>,
 ) {
     debug_println!("build fn run");
     match rust_build_service.cancel_and_start_build(&BuildOption {
@@ -69,7 +75,14 @@ async fn build(
             debug_println!("build canceled");
         }
         BuildResult::Successful(cargo_build_result) => {
-            print_build_result(&cargo_build_result.error_messages, &vec![]);
+            let mut cli_error_messages = Vec::new();
+            if let Err(error) = bundle_metadata_service.load_bundle_manifest(&project_root_path) {
+                cli_error_messages.push(format!(
+                    "could not load bundle manifest for bundle metadata service: {}",
+                    error
+                ))
+            }
+            print_build_result(&cargo_build_result.error_messages, &cli_error_messages);
             wasm_bundle_web_server
                 .on_build_done(&cargo_build_result)
                 .await;
