9ba1610f9089a73e0efd8d1c5951eb215437a438
namse <skatpgusskat@naver.com>
2022-05-12 02:15:55 +0900

    Prebuilt scroll and list view (#166)

---
+++ namui-prebuilt/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod list_view;
+pub mod scroll_view;
---
+++ namui-prebuilt/src/scroll_view.rs
@@ -0,0 +1,134 @@
+use namui::prelude::*;
+
+#[derive(Debug)]
+pub struct ScrollView {
+    pub scroll_y: f32,
+}
+
+pub struct Props {
+    pub x: f32,
+    pub y: f32,
+    pub scroll_bar_width: f32,
+    pub height: f32,
+    pub content: RenderingTree,
+}
+
+pub enum Event {
+    Scrolled(f32),
+}
+
+impl ScrollView {
+    pub fn new() -> Self {
+        Self { scroll_y: 0.0 }
+    }
+    pub fn update(&mut self, event: &dyn std::any::Any) {
+        if let Some(event) = event.downcast_ref::<Event>() {
+            match event {
+                Event::Scrolled(scroll_y) => {
+                    self.scroll_y = *scroll_y;
+                }
+                _ => {}
+            }
+        }
+    }
+    pub fn render(&self, props: &Props) -> RenderingTree {
+        let content_bounding_box = props.content.get_bounding_box();
+        if content_bounding_box.is_none() {
+            return RenderingTree::Empty;
+        }
+        let content_bounding_box = content_bounding_box.unwrap();
+
+        let scroll_y = namui::math::num::clamp(
+            self.scroll_y,
+            0.0,
+            (0.0_f32).max(content_bounding_box.height - props.height),
+        );
+
+        let inner = namui::clip(
+            namui::PathBuilder::new().add_rect(&namui::LtrbRect {
+                left: 0.0,
+                top: 0.0,
+                right: content_bounding_box.width,
+                bottom: props.height,
+            }),
+            namui::ClipOp::Intersect,
+            namui::translate(0.0, -scroll_y, props.content.clone()),
+        );
+
+        let scroll_bar_handle_height = props.height * (props.height / content_bounding_box.height);
+
+        let scroll_bar_y = (props.height - scroll_bar_handle_height) * scroll_y
+            / (content_bounding_box.height - props.height);
+
+        let scroll_bar = match content_bounding_box.height > props.height {
+            true => namui::render![rect(RectParam {
+                x: content_bounding_box.width - props.scroll_bar_width, // iOS Style!
+                y: scroll_bar_y,
+                width: props.scroll_bar_width,
+                height: scroll_bar_handle_height,
+                style: RectStyle {
+                    fill: Some(RectFill {
+                        color: Color::grayscale_f01(0.5),
+                    }),
+                    ..Default::default()
+                },
+                ..Default::default()
+            }),],
+            false => RenderingTree::Empty,
+        };
+        let whole_rect_id = namui::nanoid();
+        let whole_rect = rect(RectParam {
+            x: 0.0,
+            y: 0.0,
+            width: content_bounding_box.width + props.scroll_bar_width,
+            height: props.height,
+            style: RectStyle {
+                stroke: Some(RectStroke {
+                    width: 1.0,
+                    border_position: BorderPosition::Middle,
+                    color: Color::BLACK,
+                }),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .with_id(&whole_rect_id)
+        .attach_event(move |builder| {
+            let width = content_bounding_box.width + props.scroll_bar_width;
+            let height = props.height;
+            let whole_rect_id = whole_rect_id.clone();
+            builder.on_wheel(move |event| {
+                let managers = namui::managers();
+
+                let mouse_manager = &managers.mouse_manager;
+                let mouse_position = mouse_manager.mouse_position();
+                let whole_rect_xy = event
+                    .namui_context
+                    .get_rendering_tree_xy(&whole_rect_id)
+                    .unwrap();
+
+                let is_mouse_in_timeline = mouse_position.x as f32 >= whole_rect_xy.x
+                    && mouse_position.x as f32 <= whole_rect_xy.x + width
+                    && mouse_position.y as f32 >= whole_rect_xy.y
+                    && mouse_position.y as f32 <= whole_rect_xy.y + height;
+                if !is_mouse_in_timeline {
+                    return;
+                }
+
+                let next_scroll_y = namui::math::num::clamp(
+                    scroll_y + event.delta_xy.y,
+                    0.0,
+                    (0.0_f32).max(content_bounding_box.height - height),
+                );
+
+                namui::event::send(Event::Scrolled(next_scroll_y));
+            })
+        });
+
+        namui::translate(
+            props.x,
+            props.y,
+            namui::render![whole_rect, inner, scroll_bar],
+        )
+    }
+}
