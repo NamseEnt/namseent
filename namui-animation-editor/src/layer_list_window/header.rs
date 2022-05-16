0d842949a98a7cd554457f638323f3f2f3e43a47
namse <skatpgusskat@naver.com>
2022-05-16 22:48:09 +0900

    Renew dubu to rect_slice(2)

--- namui-animation-editor/src/layer_list_window/header.rs
+++ namui-animation-editor/src/layer_list_window/header.rs
@@ -1,5 +1,5 @@
 use namui::prelude::*;
-use namui_prebuilt::dubu::Dubu;
+use namui_prebuilt::rect_slice;
 
 pub(crate) struct Header {}
 
@@ -15,7 +15,10 @@ impl UpdateOnEvent for Header {
 
 pub struct Props();
 
-impl Dubu<Props> for Header {
+impl<'a> rect_slice::traits::Row<'a, Props> for Header {
+    fn get_height(&self, _parent_wh: Wh<f32>) -> rect_slice::Size<'a> {
+        rect_slice::Size::Fixed(20.0)
+    }
     fn render(&self, wh: Wh<f32>, _props: Props) -> RenderingTree {
         let button_xywh = XywhRect {
             x: 2.0,
