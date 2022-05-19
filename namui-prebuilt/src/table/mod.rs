31a02964cf530ca8904e82e8f0d777767c2a6087
namse <skatpgusskat@naver.com>
2022-05-20 03:39:41 +0900

    Simplify TableCell using box and iter

--- namui-prebuilt/src/table/mod.rs
+++ namui-prebuilt/src/table/mod.rs
@@ -4,6 +4,11 @@ pub trait CellRender<Props> {
     fn render(&self, wh: Wh<f32>, props: Props) -> RenderingTree;
 }
 
+pub struct TableCell<'a> {
+    unit: Unit,
+    render: Box<dyn FnOnce(Wh<f32>) -> RenderingTree + 'a>,
+}
+
 pub enum Unit {
     Ratio(f32),
     Fixed(f32),
@@ -14,17 +19,21 @@ pub fn ratio_trait<'a, Props: 'a>(
     ratio: f32,
     cell_render_trait: &'a impl CellRender<Props>,
     props: Props,
-) -> (Unit, impl FnOnce(Wh<f32>) -> RenderingTree + 'a) {
-    (Unit::Ratio(ratio), |wh: Wh<f32>| {
-        cell_render_trait.render(wh, props)
-    })
+) -> TableCell<'a> {
+    TableCell {
+        unit: Unit::Ratio(ratio),
+        render: Box::new(move |wh| cell_render_trait.render(wh, props)),
+    }
 }
 
 pub fn ratio_closure<'a>(
     ratio: f32,
     cell_render_closure: impl FnOnce(Wh<f32>) -> RenderingTree + 'a,
-) -> (Unit, impl FnOnce(Wh<f32>) -> RenderingTree + 'a) {
-    (Unit::Ratio(ratio), cell_render_closure)
+) -> TableCell<'a> {
+    TableCell {
+        unit: Unit::Ratio(ratio),
+        render: Box::new(cell_render_closure),
+    }
 }
 
 #[macro_export]
@@ -41,17 +50,21 @@ pub fn fixed_trait<'a, Props: 'a>(
     pixel: f32,
     cell_render_trait: &'a impl CellRender<Props>,
     props: Props,
-) -> (Unit, impl FnOnce(Wh<f32>) -> RenderingTree + 'a) {
-    (Unit::Fixed(pixel), |wh: Wh<f32>| {
-        cell_render_trait.render(wh, props)
-    })
+) -> TableCell<'a> {
+    TableCell {
+        unit: Unit::Fixed(pixel),
+        render: Box::new(move |wh| cell_render_trait.render(wh, props)),
+    }
 }
 
 pub fn fixed_closure<'a>(
     pixel: f32,
     cell_render_closure: impl FnOnce(Wh<f32>) -> RenderingTree + 'a,
-) -> (Unit, impl FnOnce(Wh<f32>) -> RenderingTree + 'a) {
-    (Unit::Fixed(pixel), cell_render_closure)
+) -> TableCell<'a> {
+    TableCell {
+        unit: Unit::Fixed(pixel),
+        render: Box::new(cell_render_closure),
+    }
 }
 
 #[macro_export]
@@ -68,21 +81,21 @@ pub fn calculative_trait<'a, Props: 'a>(
     from_parent_wh: impl FnOnce(Wh<f32>) -> f32 + 'static,
     cell_render_trait: &'a impl CellRender<Props>,
     props: Props,
-) -> (Unit, impl FnOnce(Wh<f32>) -> RenderingTree + 'a) {
-    (
-        Unit::Calculative(Box::new(from_parent_wh)),
-        |wh: Wh<f32>| cell_render_trait.render(wh, props),
-    )
+) -> TableCell<'a> {
+    TableCell {
+        unit: Unit::Calculative(Box::new(from_parent_wh)),
+        render: Box::new(move |wh| cell_render_trait.render(wh, props)),
+    }
 }
 
 pub fn calculative_closure<'a>(
     from_parent_wh: impl FnOnce(Wh<f32>) -> f32 + 'static,
     cell_render_closure: impl FnOnce(Wh<f32>) -> RenderingTree + 'a,
-) -> (Unit, impl FnOnce(Wh<f32>) -> RenderingTree + 'a) {
-    (
-        Unit::Calculative(Box::new(from_parent_wh)),
-        cell_render_closure,
-    )
+) -> TableCell<'a> {
+    TableCell {
+        unit: Unit::Calculative(Box::new(from_parent_wh)),
+        render: Box::new(cell_render_closure),
+    }
 }
 
 #[macro_export]
@@ -96,139 +109,134 @@ macro_rules! calculative {
 }
 
 #[macro_export]
-macro_rules! horizontal {
-    // item = (Unit, impl FnOnce(Wh<f32>) + 'a)
-    ($($item: expr),* $(,)?) => {
-        |wh: Wh<f32>| {
-            let mut unit_vec = Vec::new();
-            let mut render_fn_queue: std::collections::VecDeque<Box<dyn FnOnce(Wh<f32>) -> RenderingTree>>
-                = std::collections::VecDeque::new();
-            let mut rendering_tree_list: Vec<RenderingTree> = Vec::new();
+macro_rules! vertical {
+    // item = CellEntity<'a>
+    ($($item: expr),* $(,)?) => {{
+        let render_fn_boxed_items = [
+            // $(($item.0, Box::new($item.1) as Box<dyn FnOnce(Wh<f32>) -> RenderingTree>),)*
+            $($item,)*
+        ];
 
-            $(
-                let (unit, render_fn) = $item;
-                unit_vec.push(unit);
-                let render_fn = Box::new(render_fn);
-                render_fn_queue.push_back(render_fn);
-            )*
-
-            let ratio_sum = unit_vec.iter().fold(0.0, |sum, unit| match unit {
-                $crate::table::Unit::Ratio(ratio) => sum + ratio,
-                _ => sum,
-            });
-
-            let width_or_ratio_list = unit_vec
-                .into_iter()
-                .map(|unit| {
-                    let (width, ratio) = match unit {
-                        $crate::table::Unit::Ratio(ratio) => (None, Some(ratio)),
-                        $crate::table::Unit::Fixed(pixel_size) => (Some(pixel_size), None),
-                        $crate::table::Unit::Calculative(calculative_fn) => (Some(calculative_fn(wh)), None),
-                    };
-                    (width, ratio)
-                })
-                .collect::<Vec<_>>();
-
-            let non_ratio_width_sum = width_or_ratio_list
-                .iter()
-                .filter_map(|(width, _ratio)| *width)
-                .fold(0.0, |sum, width| sum + width);
-
-            let widths = width_or_ratio_list.iter().map(|(width, ratio)| {
-                if let Some(width) = width {
-                    *width
-                } else {
-                    (wh.width - non_ratio_width_sum) * ratio.unwrap() / ratio_sum
-                }
-            });
-
-            let mut x = 0.0;
-
-            for width in widths {
-                let render_fn = render_fn_queue.pop_front().unwrap();
-                let rendering_tree = namui::translate(
-                    x,
-                    0.0,
-                    render_fn(Wh {
-                        width,
-                        height: wh.height,
-                    }),
-                );
-
-                rendering_tree_list.push(rendering_tree);
-                x += width;
-            }
-            RenderingTree::Children(rendering_tree_list)
-        }
-    };
+        $crate::table::vertical(render_fn_boxed_items.into_iter())
+    }};
 }
 
 #[macro_export]
-macro_rules! vertical {
-    // item = (Unit, impl FnOnce(Wh<f32>) + 'a)
-    ($($item: expr),* $(,)?) => {
-        |wh: Wh<f32>| {
-            let mut unit_vec = Vec::new();
-            let mut render_fn_queue: std::collections::VecDeque<Box<dyn FnOnce(Wh<f32>) -> RenderingTree>>
-                = std::collections::VecDeque::new();
-            let mut rendering_tree_list: Vec<RenderingTree> = Vec::new();
+macro_rules! horizontal {
+    // item = CellEntity<'a>
+    ($($item: expr),* $(,)?) => {{
+        let render_fn_boxed_items = [
+            // $(($item.0, Box::new($item.1) as Box<dyn FnOnce(Wh<f32>) -> RenderingTree>),)*
+            $($item,)*
+        ];
 
-            $(
-                let (unit, render_fn) = $item;
-                unit_vec.push(unit);
-                let render_fn = Box::new(render_fn);
-                render_fn_queue.push_back(render_fn);
-            )*
+        $crate::table::horizontal(render_fn_boxed_items.into_iter())
+    }};
+}
 
-            let ratio_sum = unit_vec.iter().fold(0.0, |sum, unit| match unit {
-                $crate::table::Unit::Ratio(ratio) => sum + ratio,
-                _ => sum,
-            });
+pub fn vertical<'a>(
+    items: impl Iterator<Item = TableCell<'a>> + 'a,
+) -> impl FnOnce(Wh<f32>) -> RenderingTree + 'a {
+    slice_internal(Direction::Vertical, items)
+}
 
-            let height_or_ratio_list = unit_vec
-                .into_iter()
-                .map(|unit| {
-                    let (height, ratio) = match unit {
-                        $crate::table::Unit::Ratio(ratio) => (None, Some(ratio)),
-                        $crate::table::Unit::Fixed(pixel_size) => (Some(pixel_size), None),
-                        $crate::table::Unit::Calculative(calculative_fn) => (Some(calculative_fn(wh)), None),
-                    };
-                    (height, ratio)
-                })
-                .collect::<Vec<_>>();
+pub fn horizontal<'a>(
+    items: impl Iterator<Item = TableCell<'a>> + 'a,
+) -> impl FnOnce(Wh<f32>) -> RenderingTree + 'a {
+    slice_internal(Direction::Horizontal, items)
+}
 
-            let non_ratio_height_sum = height_or_ratio_list
-                .iter()
-                .filter_map(|(height, _ratio)| *height)
-                .fold(0.0, |sum, height| sum + height);
+enum Direction {
+    Vertical,
+    Horizontal,
+}
+fn slice_internal<'a>(
+    direction: Direction,
+    items: impl Iterator<Item = TableCell<'a>> + 'a,
+) -> impl FnOnce(Wh<f32>) -> RenderingTree + 'a {
+    let mut units = Vec::new();
+    let mut render_fns = std::collections::VecDeque::new();
 
-            let heights = height_or_ratio_list.iter().map(|(height, ratio)| {
-                if let Some(height) = height {
-                    *height
-                } else {
-                    (wh.height - non_ratio_height_sum) * ratio.unwrap() / ratio_sum
-                }
-            });
+    for item in items {
+        units.push(item.unit);
+        render_fns.push_back(item.render);
+    }
 
-            let mut y = 0.0;
+    move |wh: Wh<f32>| {
+        let direction_pixel_size = match direction {
+            Direction::Vertical => wh.height,
+            Direction::Horizontal => wh.width,
+        };
 
-            for height in heights {
-                let render_fn = render_fn_queue.pop_front().unwrap();
-                let rendering_tree = namui::translate(
-                    0.0,
-                    y,
-                    render_fn(Wh {
-                        width: wh.width,
-                        height,
-                    }),
-                );
+        let mut rendering_tree_list: Vec<RenderingTree> = Vec::new();
 
-                rendering_tree_list.push(rendering_tree);
-                y += height;
+        let ratio_sum = units.iter().fold(0.0, |sum, unit| match unit {
+            Unit::Ratio(ratio) => sum + ratio,
+            _ => sum,
+        });
+
+        let pixel_size_or_ratio_list = units
+            .into_iter()
+            .map(|unit| {
+                let (pixel_size, ratio) = match unit {
+                    Unit::Ratio(ratio) => (None, Some(ratio)),
+                    Unit::Fixed(pixel_size) => (Some(pixel_size), None),
+                    Unit::Calculative(calculative_fn) => (Some(calculative_fn(wh)), None),
+                };
+                (pixel_size, ratio)
+            })
+            .collect::<Vec<_>>();
+
+        let non_ratio_pixel_size_sum = pixel_size_or_ratio_list
+            .iter()
+            .filter_map(|(pixel_size, _ratio)| *pixel_size)
+            .fold(0.0, |sum, pixel_size| sum + pixel_size);
+
+        let pixel_sizes = pixel_size_or_ratio_list.iter().map(|(pixel_size, ratio)| {
+            if let Some(pixel_size) = pixel_size {
+                *pixel_size
+            } else {
+                (direction_pixel_size - non_ratio_pixel_size_sum) * ratio.unwrap() / ratio_sum
             }
-            RenderingTree::Children(rendering_tree_list)
+        });
+
+        let mut advanced_pixel_size = 0.0;
+
+        for pixel_size in pixel_sizes {
+            let render_fn = render_fns.pop_front().unwrap();
+            let xywh = match direction {
+                Direction::Vertical => XywhRect {
+                    x: 0.0,
+                    y: advanced_pixel_size,
+                    width: wh.width,
+                    height: pixel_size,
+                },
+                Direction::Horizontal => XywhRect {
+                    x: advanced_pixel_size,
+                    y: 0.0,
+                    width: pixel_size,
+                    height: wh.height,
+                },
+            };
+            let rendering_tree = namui::translate(xywh.x, xywh.y, render_fn(xywh.wh()));
+
+            rendering_tree_list.push(rendering_tree);
+            advanced_pixel_size += pixel_size;
         }
-    };
+        RenderingTree::Children(rendering_tree_list)
+    }
+}
+
+#[macro_export]
+macro_rules! chains {
+    // item = CellEntity<'a>
+    ($($item_iterator: expr),* $(,)?) => {{
+        let render_fn_boxed_items = [].into_iter()
+        $(.chain($item_iterator.into_iter()))*
+        ;
+
+        render_fn_boxed_items.into_iter()
+    }};
 }
 
 #[cfg(test)]
