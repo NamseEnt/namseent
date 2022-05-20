8377170ee8385db8bf47da81298fbc79d934676a
namse <skatpgusskat@naver.com>
2022-05-21 00:20:36 +0900

    Render property units

--- namui-animation-editor/src/property_window/mod.rs
+++ namui-animation-editor/src/property_window/mod.rs
@@ -1,5 +1,9 @@
-use namui::{animation::Layer, prelude::*};
-use namui_prebuilt::{table::vertical, *};
+use namui::{
+    animation::{KeyframeGraph, KeyframeValue, Layer},
+    prelude::*,
+    types::{PixelSize, Time},
+};
+use namui_prebuilt::{table::vertical, typography::center_text, *};
 
 pub(crate) struct PropertyWindow {}
 
@@ -16,42 +20,86 @@ impl PropertyWindow {
 
 impl table::CellRender<Props<'_>> for PropertyWindow {
     fn render(&self, wh: Wh<f32>, props: Props<'_>) -> RenderingTree {
-        let properties = [
-            Property {
-                name: "X".to_string(),
-            },
-            Property {
-                name: "Y".to_string(),
-            },
-            Property {
-                name: "Width".to_string(),
-            },
-            Property {
-                name: "Height".to_string(),
-            },
-            Property {
-                name: "Rotation".to_string(),
-            },
-            Property {
-                name: "Sprite".to_string(),
-            },
-            Property {
-                name: "Visibility".to_string(),
-            },
-            Property {
-                name: "Left Line".to_string(),
-            },
-            Property {
-                name: "Right Line".to_string(),
-            },
-        ];
+        // let properties = match props.layer {
+        //     Some(_) => [
+        //         Property {
+        //             name: "X".to_string(),
+        //             unit: "px".to_string(),
+        //         },
+        //         Property {
+        //             name: "Y".to_string(),
+        //             unit: "px".to_string(),
+        //         },
+        //         Property {
+        //             name: "Width".to_string(),
+        //             unit: "px".to_string(),
+        //         },
+        //         Property {
+        //             name: "Height".to_string(),
+        //             unit: "px".to_string(),
+        //         },
+        //         Property {
+        //             name: "Rotation".to_string(),
+        //             unit: "°".to_string(),
+        //         },
+        //         Property {
+        //             name: "Sprite".to_string(),
+        //             unit: "/ 10".to_string(),
+        //         },
+        //         Property {
+        //             name: "Visibility".to_string(),
+        //             unit: "".to_string(),
+        //         },
+        //         Property {
+        //             name: "Left Line".to_string(),
+        //             unit: "".to_string(),
+        //         },
+        //         Property {
+        //             name: "Right Line".to_string(),
+        //             unit: "".to_string(),
+        //         },
+        //     ],
+        //     None => [],
+        // };
         render![
             simple_rect(wh, Color::BLACK, 1.0, Color::WHITE),
             vertical(chains![
                 [fixed!(20.0, |wh| render_header(wh))],
-                properties
-                    .iter()
-                    .map(|property| ratio!(1.0, |wh| render_property_row(wh, property)))
+                [
+                    ratio!(1.0, |wh| render_property_row(
+                        wh,
+                        "X",
+                        &props.layer.unwrap().image.x
+                    )),
+                    ratio!(1.0, |wh| render_property_row(
+                        wh,
+                        "Y",
+                        &props.layer.unwrap().image.y
+                    )),
+                    ratio!(1.0, |wh| render_property_row(
+                        wh,
+                        "Width",
+                        &props.layer.unwrap().image.width
+                    )),
+                    ratio!(1.0, |wh| render_property_row(
+                        wh,
+                        "Height",
+                        &props.layer.unwrap().image.height
+                    )),
+                    ratio!(1.0, |wh| render_property_row(
+                        wh,
+                        "Rotation",
+                        &props.layer.unwrap().image.x
+                    )),
+                    ratio!(1.0, |wh| render_property_row(
+                        wh,
+                        "Visibility",
+                        &props.layer.unwrap().image.x
+                    )),
+                ],
+                // properties
+                //     .iter()
+                //     .map(|property| ratio!(1.0, |wh| render_property_row(wh, property)))
             ])(wh)
         ]
     }
@@ -81,11 +129,11 @@ fn render_header(wh: Wh<f32>) -> RenderingTree {
     ]
 }
 
-struct Property {
-    name: String,
-}
-
-fn render_property_row(wh: Wh<f32>, property: &Property) -> RenderingTree {
+fn render_property_row<T: PropertyEditCell>(
+    wh: Wh<f32>,
+    name: &str,
+    property: &T,
+) -> RenderingTree {
     render![
         simple_rect(wh, Color::BLACK, 1.0, Color::WHITE),
         horizontal![
@@ -95,15 +143,33 @@ fn render_property_row(wh: Wh<f32>, property: &Property) -> RenderingTree {
             ratio!(3.0, |wh| {
                 render![
                     simple_rect(wh, Color::BLACK, 1.0, Color::WHITE),
-                    center_text(wh, &property.name, Color::BLACK),
+                    typography::body::center(wh, &name, Color::BLACK),
                 ]
             }),
-            ratio!(3.0, |wh| { simple_rect(wh, Color::BLACK, 1.0, Color::RED) }),
-            ratio!(1.0, |wh| {
-                simple_rect(wh, Color::BLACK, 1.0, Color::WHITE)
-            }),
+            ratio!(3.0, |wh| { property.render_property_edit_cell(wh) }),
         ](wh),
     ]
+}
+
+trait PropertyEditCell {
+    fn render_property_edit_cell(&self, wh: Wh<f32>) -> RenderingTree;
+}
+
+impl PropertyEditCell for KeyframeGraph<PixelSize> {
+    fn render_property_edit_cell(&self, wh: Wh<f32>) -> RenderingTree {
+        render![
+            simple_rect(wh, Color::BLACK, 1.0, Color::WHITE),
+            typography::body::right(
+                wh,
+                format!(
+                    "{:?} {}",
+                    self.get_value(&Time::from_ms(0.0)),
+                    PixelSize::unit()
+                ),
+                Color::BLACK
+            ),
+        ]
+    }
 }
 
 fn render_graph_visible_toggle_cell(wh: Wh<f32>) -> RenderingTree {
