8fe5666f959201bc7290f973233bd8ce89fb311c
namse <skatpgusskat@naver.com>
2022-05-21 00:20:21 +0900

    Interpolate and expose unit

--- namui/src/namui/animation/animatable_image.rs
+++ namui/src/namui/animation/animatable_image.rs
@@ -11,6 +11,31 @@ pub struct AnimatableImage {
     pub opacity: KeyframeGraph<OneZero>,
 }
 
+impl KeyframeValue for PixelSize {
+    fn interpolate(&self, next: &Self, ratio: f32) -> Self {
+        self * (1.0 - ratio) + next * ratio
+    }
+    fn unit() -> &'static str {
+        "px"
+    }
+}
+impl KeyframeValue for Angle {
+    fn interpolate(&self, next: &Self, ratio: f32) -> Self {
+        self * (1.0 - ratio) + next * ratio
+    }
+    fn unit() -> &'static str {
+        "°"
+    }
+}
+impl KeyframeValue for OneZero {
+    fn interpolate(&self, next: &Self, ratio: f32) -> Self {
+        Self::new(self.0 * (1.0 - ratio) + next.0 * ratio)
+    }
+    fn unit() -> &'static str {
+        ""
+    }
+}
+
 impl Animate for AnimatableImage {
     fn render(&self, time: &Time) -> RenderingTree {
         try_render! {
@@ -32,6 +57,49 @@ impl Animate for AnimatableImage {
                 },
                 source: ImageSource::Url(self.image_source_url.clone()),
             }))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use float_cmp::approx_eq;
+    use wasm_bindgen_test::wasm_bindgen_test;
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn one_zero_should_be_interpolated() {
+        let mut graph = KeyframeGraph::new(KeyframePoint {
+            time: Time::from_ms(0.0),
+            value: OneZero::new(0.0),
+        });
+        graph.push(
+            KeyframePoint {
+                time: Time::from_ms(10.0),
+                value: OneZero::new(100.0), // become 1.0
+            },
+            KeyframeLine::Linear,
+        );
+        graph.push(
+            KeyframePoint {
+                time: Time::from_ms(20.0),
+                value: OneZero::new(0.5),
+            },
+            KeyframeLine::Linear,
+        );
+        for time in 0..10 {
+            let value = graph.get_value(&Time::from_ms(time as f32));
+            assert_eq!(value, Some((time as f32 / 10.0).into()));
+        }
+        for time in 10..20 {
+            let value = graph.get_value(&Time::from_ms(time as f32));
+            assert!(approx_eq!(
+                f32,
+                value.unwrap().into(),
+                1.0 - (time - 10) as f32 / 20.0,
+                ulps = 2
+            ));
         }
     }
 }
