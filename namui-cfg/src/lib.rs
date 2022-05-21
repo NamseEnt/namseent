5d8e1751d88ed9398a7dbf30069d550a4db2865f
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2022-05-21 20:31:22 +0900

    Update lib.rs

--- namui-cfg/src/lib.rs
+++ namui-cfg/src/lib.rs
@@ -32,6 +32,13 @@ pub fn namui_cfg(attr: TokenStream, item: TokenStream) -> TokenStream {
 }
 
 fn load_cfg_from_namui_cli(cfg_map: &mut CfgMap) -> Result<(), Box<dyn std::error::Error>> {
+    println!("{}", String::from_utf8(
+        Command::new("which")
+            .args(["namui"])
+            .output()
+            .map_err(|error| format!("Could not run which {:?}", error))?
+            .stdout,
+    )?);
     let namui_cli_print_cfg_output = String::from_utf8(
         Command::new("namui")
             .args(["print", "cfg"])
