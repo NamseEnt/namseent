78f6e19d3d3eaf5ac28c9f73ac22857a42b4e25f
namse <skatpgusskat@naver.com>
2022-05-25 03:21:25 +0900

    Wrap typeface with arc

--- namui/src/namui/manager/common/typeface_manager.rs
+++ namui/src/namui/manager/common/typeface_manager.rs
@@ -1,9 +1,9 @@
-use dashmap::DashMap;
-
 use crate::namui::{self, Typeface, TypefaceType};
+use dashmap::DashMap;
+use std::sync::Arc;
 
 pub struct TypefaceManager {
-    typefaces: DashMap<TypefaceType, Typeface>,
+    typefaces: DashMap<TypefaceType, Arc<Typeface>>,
 }
 
 impl TypefaceManager {
@@ -12,14 +12,14 @@ impl TypefaceManager {
             typefaces: DashMap::new(),
         }
     }
-    pub fn get_typeface<'a>(&'a self, option: &'a TypefaceType) -> Option<Typeface> {
+    pub fn get_typeface<'a>(&'a self, option: &'a TypefaceType) -> Option<Arc<Typeface>> {
         namui::log(format!("typefaces: {:?}", self.typefaces.len()));
-        self.typefaces.get(option).map(|x| x.clone())
+        self.typefaces.get(option).map(|typeface| typeface.clone())
     }
     pub fn load_typeface(&self, option: &TypefaceType, bytes: &Vec<u8>) {
         let typeface = Typeface::new(bytes);
         namui::log(format!("Loaded typeface: {:?}", option));
 
-        self.typefaces.insert(*option, typeface);
+        self.typefaces.insert(*option, Arc::new(typeface));
     }
 }
