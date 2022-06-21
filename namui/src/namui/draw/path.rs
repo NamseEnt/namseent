166b71c04ca1ae27e1ece4863c09d5b8ac0b9358
namse <skatpgusskat@naver.com>
2022-06-21 23:57:30 +0900

    Check xy in rendering tree and clean some code

--- namui/src/namui/draw/path.rs
+++ namui/src/namui/draw/path.rs
@@ -1,8 +1,62 @@
-use super::PathDrawCommand;
-use crate::namui::{self, NamuiContext};
+use super::*;
+use crate::namui::NamuiContext;
 
-pub fn draw_path(namui_context: &NamuiContext, command: &PathDrawCommand) {
-    let path = command.path_builder.build();
-    let paint = command.paint_builder.build();
-    namui_context.surface.canvas().draw_path(&path, &paint);
+#[derive(Debug, Serialize, Clone)]
+pub struct PathDrawCommand {
+    #[serde(skip_serializing)]
+    pub path_builder: PathBuilder,
+    #[serde(skip_serializing)]
+    pub paint_builder: PaintBuilder,
+}
+
+impl PathDrawCommand {
+    pub fn draw(&self, namui_context: &NamuiContext) {
+        let path = self.path_builder.build();
+        let paint = self.paint_builder.build();
+        namui_context.surface.canvas().draw_path(&path, &paint);
+    }
+    pub fn get_bounding_box(&self) -> Option<crate::LtrbRect> {
+        let path = self.path_builder.build();
+        let paint = self.paint_builder.build();
+
+        let mut stroke_path_builder = self.path_builder.clone();
+        let stroke_result = stroke_path_builder.stroke(StrokeOptions {
+            cap: Some(paint.get_stroke_cap()),
+            join: Some(paint.get_stroke_join()),
+            width: Some(paint.get_stroke_width()),
+            miter_limit: Some(paint.get_stroke_miter()),
+            precision: None,
+        });
+
+        let path = match stroke_result {
+            Ok(()) => stroke_path_builder.build(),
+            Err(()) => path,
+        };
+
+        path.get_bounding_box()
+    }
+    pub fn is_xy_in(&self, xy: Xy<f32>) -> bool {
+        let path = self.path_builder.build();
+        if path.contains(xy) {
+            return true;
+        }
+
+        let paint = self.paint_builder.build();
+
+        let mut stroke_path_builder = self.path_builder.clone();
+        let stroke_result = stroke_path_builder.stroke(StrokeOptions {
+            cap: Some(paint.get_stroke_cap()),
+            join: Some(paint.get_stroke_join()),
+            width: Some(paint.get_stroke_width()),
+            miter_limit: Some(paint.get_stroke_miter()),
+            precision: None,
+        });
+
+        let path = match stroke_result {
+            Ok(()) => stroke_path_builder.build(),
+            Err(()) => path,
+        };
+
+        path.contains(xy)
+    }
 }
