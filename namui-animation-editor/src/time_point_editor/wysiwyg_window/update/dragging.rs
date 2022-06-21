e8d5bafbe797618cf17071166795590585207b1e
namse <skatpgusskat@naver.com>
2022-06-21 17:07:24 +0900

    Resize in all way

--- namui-animation-editor/src/time_point_editor/wysiwyg_window/update/dragging.rs
+++ namui-animation-editor/src/time_point_editor/wysiwyg_window/update/dragging.rs
@@ -1,6 +1,9 @@
 use std::sync::Arc;
 
-use namui::{animation::KeyframePoint, types::Percent};
+use namui::{
+    animation::{KeyframePoint, Layer},
+    types::Percent,
+};
 
 use super::*;
 
@@ -41,24 +44,94 @@ impl WysiwygWindow {
                 let image = managers.image_manager.try_load(&image_url).unwrap();
                 let image_wh = image.size();
 
-                match location {
-                    ResizeCircleLocation::LeftTop => todo!(),
-                    ResizeCircleLocation::Top => todo!(),
-                    ResizeCircleLocation::RightTop => todo!(),
-                    ResizeCircleLocation::Left => todo!(),
-                    ResizeCircleLocation::Right => {
-                        let value = layer.image.width.get_value(self.playback_time).unwrap();
-                        let current_width: f32 = (value * image_wh.width).into();
-                        let next_width = current_width + delta_in_real.x;
-                        let next_value = Percent::from(next_width / image_wh.width);
-                        layer.image.width.put(
+                enum PlusMinus {
+                    Plus,
+                    Minus,
+                }
+                enum WidthHeight {
+                    Width,
+                    Height,
+                }
+                let update_size =
+                    |layer: &mut Layer, width_height: WidthHeight, plus_minus: PlusMinus| {
+                        let graph = match width_height {
+                            WidthHeight::Width => &mut layer.image.width,
+                            WidthHeight::Height => &mut layer.image.height,
+                        };
+                        let value = graph.get_value(self.playback_time).unwrap();
+                        let image_value = match width_height {
+                            WidthHeight::Width => image_wh.width,
+                            WidthHeight::Height => image_wh.height,
+                        };
+                        let current: f32 = (value * image_value).into();
+                        let next = current
+                            + (match plus_minus {
+                                PlusMinus::Plus => 1.0,
+                                PlusMinus::Minus => -1.0,
+                            }) * (match width_height {
+                                WidthHeight::Width => delta_in_real.x,
+                                WidthHeight::Height => delta_in_real.y,
+                            });
+                        let next_value = Percent::from(next / image_value);
+                        graph.put(
                             KeyframePoint::new(self.playback_time, next_value),
                             animation::KeyframeLine::Linear,
                         );
+                    };
+                enum XY {
+                    X,
+                    Y,
+                }
+                let update_xy = |layer: &mut Layer, x_y: XY, plus_minus: PlusMinus| {
+                    let graph = match x_y {
+                        XY::X => &mut layer.image.x,
+                        XY::Y => &mut layer.image.y,
+                    };
+                    let value = graph.get_value(self.playback_time).unwrap();
+                    let current: f32 = value.into();
+                    let next = current
+                        + (match plus_minus {
+                            PlusMinus::Plus => 1.0,
+                            PlusMinus::Minus => -1.0,
+                        }) * (match x_y {
+                            XY::X => delta_in_real.x,
+                            XY::Y => delta_in_real.y,
+                        });
+                    let next_value = next.into();
+                    graph.put(
+                        KeyframePoint::new(self.playback_time, next_value),
+                        animation::KeyframeLine::Linear,
+                    );
+                };
+
+                match location {
+                    ResizeCircleLocation::LeftTop
+                    | ResizeCircleLocation::Top
+                    | ResizeCircleLocation::RightTop => {
+                        update_size(&mut layer, WidthHeight::Height, PlusMinus::Minus);
+                        update_xy(&mut layer, XY::Y, PlusMinus::Plus);
                     }
-                    ResizeCircleLocation::LeftBottom => todo!(),
-                    ResizeCircleLocation::Bottom => todo!(),
-                    ResizeCircleLocation::RightBottom => todo!(),
+                    ResizeCircleLocation::LeftBottom
+                    | ResizeCircleLocation::Bottom
+                    | ResizeCircleLocation::RightBottom => {
+                        update_size(&mut layer, WidthHeight::Height, PlusMinus::Plus);
+                    }
+                    _ => {}
+                }
+
+                match location {
+                    ResizeCircleLocation::LeftTop
+                    | ResizeCircleLocation::Left
+                    | ResizeCircleLocation::LeftBottom => {
+                        update_size(&mut layer, WidthHeight::Width, PlusMinus::Minus);
+                        update_xy(&mut layer, XY::X, PlusMinus::Plus);
+                    }
+                    ResizeCircleLocation::RightTop
+                    | ResizeCircleLocation::Right
+                    | ResizeCircleLocation::RightBottom => {
+                        update_size(&mut layer, WidthHeight::Width, PlusMinus::Plus);
+                    }
+                    _ => {}
                 }
 
                 namui::event::send(crate::Event::UpdateLayer(Arc::new(layer)));
