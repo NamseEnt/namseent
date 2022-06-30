6538f3bcfad429cef8bf076164ec2cf4a8a6ead8
namse <skatpgusskat@naver.com>
2022-06-30 16:57:57 +0900

    Group imports

--- namui-prebuilt/src/lib.rs
+++ namui-prebuilt/src/lib.rs
@@ -1,11 +1,12 @@
-use namui::prelude::*;
+pub mod button;
 pub mod list_view;
 pub mod scroll_view;
 mod simple_rect;
-pub use simple_rect::simple_rect;
-pub mod button;
 pub mod table;
 pub mod typography;
+
+use namui::prelude::*;
+pub use simple_rect::simple_rect;
 
 #[cfg(test)]
 #[cfg(target_family = "wasm")]
