bf5a50b9562e4a6e2438461b3070bb013ede1046
bigfood <satcom1600@hanmail.net>
2022-07-19 23:31:59 +0900

    Fetch images in parallel for init speed

--- luda-editor/luda-editor-client/src/app/storage/fetch_background_images.rs
+++ luda-editor/luda-editor-client/src/app/storage/fetch_background_images.rs
@@ -2,6 +2,7 @@ use super::Storage;
 use crate::app::github_api::{Dirent, ReadDirError};
 use async_recursion::async_recursion;
 use dashmap::DashSet;
+use futures::future::join_all;
 
 pub const BACKGROUND_IMAGE_PATH_PREFIX: &str = "backgroundImages";
 
@@ -9,7 +10,10 @@ impl Storage {
     pub async fn fetch_background_images(&self) -> Result<(), FetchBackgroundImagesError> {
         let path_set = self.get_background_image_path_set();
         match self
-            .collect_background_image_url_recursively(BACKGROUND_IMAGE_PATH_PREFIX, path_set)
+            .collect_background_image_url_recursively(
+                BACKGROUND_IMAGE_PATH_PREFIX.to_string(),
+                path_set,
+            )
             .await
         {
             Ok(_) => Ok(()),
@@ -23,10 +27,11 @@ impl Storage {
     #[async_recursion(?Send)]
     async fn collect_background_image_url_recursively(
         &self,
-        path: &str,
+        path: String,
         path_set: &DashSet<String>,
     ) -> Result<(), ReadDirError> {
-        let dirent_list = self.get_github_api_client().read_dir(path).await?;
+        let dirent_list = self.get_github_api_client().read_dir(path.as_str()).await?;
+        let mut next_collect_future_list = Vec::new();
         for dirent in dirent_list {
             match dirent {
                 Dirent::File { path, .. } => {
@@ -35,15 +40,27 @@ impl Storage {
                         .to_string();
                     path_set.insert(path);
                 }
-                Dirent::Dir { path, .. } => {
-                    self.collect_background_image_url_recursively(path.as_str(), path_set)
-                        .await?
-                }
+                Dirent::Dir { path, .. } => next_collect_future_list
+                    .push(self.collect_background_image_url_recursively(path, path_set)),
                 _ => unimplemented!(),
             }
         }
+        let next_collect_result_list = join_all(next_collect_future_list).await;
+        throw_error_if_some_result_is_error(next_collect_result_list)?;
         Ok(())
     }
+}
+
+fn throw_error_if_some_result_is_error(
+    next_collect_result_list: Vec<Result<(), ReadDirError>>,
+) -> Result<(), ReadDirError> {
+    for next_collect_result in next_collect_result_list {
+        match next_collect_result {
+            Ok(_) => (),
+            Err(error) => return Err(error),
+        }
+    }
+    Ok(())
 }
 
 #[derive(Debug)]
