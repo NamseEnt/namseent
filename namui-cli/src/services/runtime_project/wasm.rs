d3e4a814da83a9c174833349d212a82abfe1d318
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2022-08-08 10:24:13 +0900

    Get project name from manifest (#300)

--- namui-cli/src/services/runtime_project/wasm.rs
+++ namui-cli/src/services/runtime_project/wasm.rs
@@ -1,9 +1,10 @@
 use super::GenerateRuntimeProjectArgs;
+use std::path::PathBuf;
 
 pub fn generate_runtime_project(
     args: GenerateRuntimeProjectArgs,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let project_name = args.project_path.file_name().unwrap().to_str().unwrap();
+    let project_name = get_project_name(args.project_path.clone());
 
     std::fs::create_dir_all(&args.target_dir.join("src"))?;
 
@@ -51,6 +52,21 @@ pub async fn start() {{
     Ok(())
 }
 
+fn get_project_name(project_path: PathBuf) -> String {
+    let manifest_path = project_path.join("Cargo.toml");
+    let manifest_contents = std::fs::read_to_string(&manifest_path).unwrap();
+    manifest_contents
+        .split("name = ")
+        .nth(1)
+        .unwrap()
+        .split("\n")
+        .next()
+        .unwrap()
+        .trim()
+        .replace('"', "")
+        .to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use crate::services::runtime_project::GenerateRuntimeProjectArgs;
@@ -59,10 +75,41 @@ mod tests {
 
     #[test]
     fn generate_runtime_project_wasm_should_work() {
+        let project_path = std::env::temp_dir().join("namui-cli/test/wasm");
+        let manifest_path = project_path.join("Cargo.toml");
+
+        std::fs::create_dir_all(&project_path).unwrap();
+        std::fs::write(
+            manifest_path,
+            format!(
+                r#"[package]
+    name = "namui-runtime-wasm"#
+            ),
+        )
+        .unwrap();
+
         generate_runtime_project(GenerateRuntimeProjectArgs {
             target_dir: std::env::temp_dir(),
-            project_path: std::env::temp_dir().join("namui-cli/test/wasm"),
+            project_path,
         })
         .unwrap();
+    }
+
+    #[test]
+    fn get_project_name_should_work() {
+        let project_path = std::env::temp_dir().join("namui-cli/test/wasm");
+        let manifest_path = project_path.join("Cargo.toml");
+
+        std::fs::create_dir_all(&project_path).unwrap();
+        std::fs::write(
+            manifest_path,
+            format!(
+                r#"[package]
+    name = "namui-runtime-wasm"#
+            ),
+        )
+        .unwrap();
+
+        assert_eq!("namui-runtime-wasm", super::get_project_name(project_path));
     }
 }
