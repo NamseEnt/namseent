7b8516c1750d616952a0d5960c8eaf712fa49087
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2022-09-02 23:52:03 +0900

    Delete create-lambda.sh

--- luda-editor/server/create-lambda.sh
+++
@@ -1,7 +0,0 @@
-aws lambda create-function --function-name rustTest \
-  --handler bootstrap \
-  --zip-file fileb://./target/lambda/release/server.zip \
-  --runtime provided.al2 \
-  --role arn:aws:iam::304616328860:role/luda-editor-lambda-role \
-  --environment Variables={RUST_BACKTRACE=1} \
-  --tracing-config Mode=Active
