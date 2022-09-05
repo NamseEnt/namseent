9b02e347cc35c1e0bba46c8e88f2f801be3c26e6
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2022-09-05 22:54:25 +0900

    Update cargo-check-all.py

--- cargo-check-all.py
+++ cargo-check-all.py
@@ -1,36 +1,9 @@
 import os
 
-ignores = [
-    ".git",
-    "target",
-    "node_modules",
-    "dist",
-    "build",
-    "vendor",
-    "bin",
-    "lib",
-    "obj",
-    "out",
-    "release",
-    "src",
-    "include",
-    "doc",
-]
-
-
-def search_cargo_project_and_run_check(path: str):
-    list = os.listdir(path)
-    for dir in list:
-        if dir in ignores:
-            continue
-        if os.path.isdir(os.path.join(path, dir)):
-            if os.path.isfile(os.path.join(path, dir, "Cargo.toml")):
-                print("cargo check " + os.path.join(path, dir))
-                exit = os.system(
-                    f"cd {os.path.join(path, dir)} && cargo check")
-                if exit != 0:
-                    exit(1)
-            search_cargo_project_and_run_check(os.path.join(path, dir))
-
-
-search_cargo_project_and_run_check(".")
+for menifast_path in os.popen("git ls-files | grep -e /Cargo.toml").read().splitlines():
+    print("cargo check " + menifast_path)
+    dir_path = os.path.dirname(menifast_path)
+    exit = os.system(
+        f"cd {dir_path} && cargo check")
+    if exit != 0:
+        exit(1)
