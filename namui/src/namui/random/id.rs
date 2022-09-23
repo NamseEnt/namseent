d5e1bedb3104d8011c0036f18b5d724590651841
bigfoodk <satcom1600@hanmail.net>
2022-09-23 10:27:02 +0900

    Use shorter values for Id

--- namui/src/namui/random/id.rs
+++ namui/src/namui/random/id.rs
@@ -1,9 +1,11 @@
 use crate::simple_error_impl;
 use base64::decode_config;
 
+const ID_BYTE_LENGTH: usize = 16;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Id {
-    values: [u8; 64],
+    values: [u8; ID_BYTE_LENGTH],
 }
 impl<'a> PartialEq<Id> for &'a Id {
     fn eq(&self, other: &Id) -> bool {
@@ -32,8 +34,8 @@ impl TryFrom<&str> for Id {
         let bytes = decode_config(value, base64::STANDARD_NO_PAD)
             .map_err(|error| IdTryFromStrError::Base64DecodeError(error))?;
         match bytes.len() {
-            64 => {
-                let mut values = [0; 64];
+            ID_BYTE_LENGTH => {
+                let mut values = [0; ID_BYTE_LENGTH];
                 values.copy_from_slice(&bytes);
                 Ok(Id { values })
             }
@@ -43,7 +45,7 @@ impl TryFrom<&str> for Id {
 }
 
 pub fn random_id() -> Id {
-    let mut array = [0u8; 64];
+    let mut array = [0u8; ID_BYTE_LENGTH];
     let window = web_sys::window().unwrap();
     let crypto = window.crypto().unwrap();
     crypto.get_random_values_with_u8_array(&mut array).unwrap();
@@ -58,13 +60,10 @@ mod tests {
     #[test]
     #[wasm_bindgen_test]
     fn try_from_base64_str() {
-        let base64_str = "ZdNAI2qYjIKZ/030AbSxGy09M3zb9Ea0rB6PMvdzzQfKFIBeGrpwfvxV3eHHI+tymIcLslZ8FMeUDqA5OCsrEw";
+        let base64_str = "ZdNAI2qYjIKZ/030AbSxGw";
         let expected = Id {
             values: [
-                101, 211, 64, 35, 106, 152, 140, 130, 153, 255, 77, 244, 1, 180, 177, 27, 45, 61,
-                51, 124, 219, 244, 70, 180, 172, 30, 143, 50, 247, 115, 205, 7, 202, 20, 128, 94,
-                26, 186, 112, 126, 252, 85, 221, 225, 199, 35, 235, 114, 152, 135, 11, 178, 86,
-                124, 20, 199, 148, 14, 160, 57, 56, 43, 43, 19,
+                101, 211, 64, 35, 106, 152, 140, 130, 153, 255, 77, 244, 1, 180, 177, 27,
             ],
         };
         let actual = Id::try_from(base64_str).unwrap();
@@ -74,10 +73,9 @@ mod tests {
     #[test]
     #[wasm_bindgen_test]
     fn try_from_base64_str_should_fail() {
-        let short_base64_str =
-            "ZdNAI2qYjIKZ/030AbSxGy09M3zb9Ea0rB6PMvdzzQfKFIBeGrpwfvxV3eHHI+tymIcLslZ8FMeUDqA5OCsr";
-        let long_base64_str = "ZdNAI2qYjIKZ/030AbSxGy09M3zb9Ea0rB6PMvdzzQfKFIBeGrpwfvxV3eHHI+tymIcLslZ8FMeUDqA5OCsrEwE";
-        let invalid_character_base64_str = "ZdNAI2qYjIKZ/030AbSxGy09M3zb9Ea0rB6PMvdzzQfKFIBeGrpwfvxV3eHHI+tymIcLslZ8FMeUDqA5OCsrEw_";
+        let short_base64_str = "ZdNAI2qYjIKZ/030AbSx";
+        let long_base64_str = "ZdNAI2qYjIKZ/030AbSxGy0";
+        let invalid_character_base64_str = "ZdNAI2qYjIKZ/030AbSxGw_";
 
         assert_eq!(
             Id::try_from(short_base64_str),
