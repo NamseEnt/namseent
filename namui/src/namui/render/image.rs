316824a18e74d29f846df4baa86b4254eee25130
namse <skatpgusskat@naver.com>
2022-09-30 12:58:33 +0900

    Explain ImageFit

--- namui/src/namui/render/image.rs
+++ namui/src/namui/render/image.rs
@@ -2,12 +2,24 @@ use crate::{draw::ImageDrawCommand, file::picker::File, *};
 use serde::Serialize;
 use std::sync::Arc;
 
+/// Example: https://developer.mozilla.org/ko/docs/Web/CSS/object-fit
 #[derive(Debug, Serialize, Clone, Copy)]
 pub enum ImageFit {
+    /// The replaced content is sized to fill the element's content box.
+    /// The entire object will completely fill the box.
+    /// If the object's aspect ratio does not match the aspect ratio of its box,
+    /// then the object will be stretched to fit.
     Fill,
+    /// The replaced content is scaled to maintain its aspect ratio while fitting within the element's content box.
+    /// The entire object is made to fill the box, while preserving its aspect ratio, so the object will be letterboxed
+    /// if its aspect ratio does not match the aspect ratio of the box.
     Contain,
+    /// The replaced content is sized to maintain its aspect ratio while filling the element's entire content box.
+    /// If the object's aspect ratio does not match the aspect ratio of its box, then the object will be clipped to fit.
     Cover,
+    /// The content is sized as if `none` or `contain` were specified, whichever would result in a smaller concrete object size.
     ScaleDown,
+    /// The replaced content is not resized.
     None,
 }
 
