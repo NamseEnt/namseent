62553f5e2224ff4baaf085292277ce26a8ab1881
namse <skatpgusskat@naver.com>
2022-10-02 07:36:25 +0000

    Get bundle with relative path

--- namui/src/namui/system/file/bundle/create_bundle_url.rs
+++ namui/src/namui/system/file/bundle/create_bundle_url.rs
@@ -1,5 +1,4 @@
 use crate::file::types::PathLike;
-use namui_cfg::namui_cfg;
 
 pub fn create_bundle_url(path_like: impl PathLike) -> String {
     let mut components: Vec<String> = Vec::new();
@@ -20,23 +19,15 @@ pub fn create_bundle_url(path_like: impl PathLike) -> String {
     components.join("/")
 }
 
-#[namui_cfg(not(all(target_env = "electron", not(watch_reload))))]
-pub fn bundle_url_prefix() -> String {
-    "/bundle".to_string()
-}
-
-#[namui_cfg(all(target_env = "electron", not(watch_reload)))]
 pub fn bundle_url_prefix() -> String {
     "bundle".to_string()
 }
 
 #[cfg(test)]
 mod tests {
-    use namui_cfg::namui_cfg;
     use std::path::PathBuf;
     use wasm_bindgen_test::wasm_bindgen_test;
 
-    #[namui_cfg(all(target_env = "electron", not(watch_reload)))]
     #[test]
     #[wasm_bindgen_test]
     fn create_bundle_url() {
@@ -50,30 +41,6 @@ mod tests {
             ("path/to/./some/where", "bundle/path/to/some/where"),
             ("path/to/../some/where", "bundle/path/some/where"),
             ("path/to/../../../some/where", "bundle/some/where"),
-        ]
-        .into_iter()
-        .map(|(path, url)| (PathBuf::from(path), url.to_string()))
-        .collect();
-        for (input, expected) in test_cases {
-            let output = super::create_bundle_url(input);
-            assert_eq!(output, expected);
-        }
-    }
-
-    #[namui_cfg(not(all(target_env = "electron", not(watch_reload))))]
-    #[test]
-    #[wasm_bindgen_test]
-    fn create_bundle_url() {
-        let test_cases: Vec<(PathBuf, String)> = vec![
-            ("", "/bundle"),
-            ("/", "/bundle"),
-            ("path", "/bundle/path"),
-            ("path/to/some/where", "/bundle/path/to/some/where"),
-            ("path/to/some/where/", "/bundle/path/to/some/where"),
-            ("/path/to/some/where", "/bundle/path/to/some/where"),
-            ("path/to/./some/where", "/bundle/path/to/some/where"),
-            ("path/to/../some/where", "/bundle/path/some/where"),
-            ("path/to/../../../some/where", "/bundle/some/where"),
         ]
         .into_iter()
         .map(|(path, url)| (PathBuf::from(path), url.to_string()))
