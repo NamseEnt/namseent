b06f40213156eb07089cdccb11c23f06d8e43ae9
bigfoodK <satcom1600@hanmail.net>
2022-10-02 21:26:46 +0900

    Use syn for export-known-ids-macro

--- luda-adventure/export-known-ids-macro/src/lib.rs
+++ luda-adventure/export-known-ids-macro/src/lib.rs
@@ -1,78 +1,72 @@
 use lazy_static::lazy_static;
-use proc_macro::{Ident, TokenStream, TokenTree};
-use proc_macro_error::{abort, proc_macro_error};
+use proc_macro::TokenStream;
+use quote::quote;
 use regex::bytes::Regex;
 use std::collections::BTreeMap;
+use syn::{
+    parse::Parse, parse_macro_input, punctuated::Punctuated, token::Colon, Ident, LitStr, Token,
+};
 
-#[proc_macro_error]
 #[proc_macro]
-pub fn export_known_ids(item: TokenStream) -> TokenStream {
-    let mut uuid_name_map = BTreeMap::<String, Ident>::new();
-    let mut item_iter = item.into_iter();
-    while let Some(name_ident) = item_iter.next() {
-        match name_ident {
-            TokenTree::Ident(name_ident) => {
-                let colon_punct = item_iter.next().unwrap();
-                if !check_token_tree_is_colon(&colon_punct) {
-                    abort!(colon_punct.span(), "Expected ':'. Found {:?}", colon_punct);
-                }
+pub fn export_known_ids(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as MacroInput);
 
-                let uuid_literal = item_iter.next().unwrap();
-                if !check_token_tree_is_literal(&uuid_literal) {
-                    abort!(
-                        uuid_literal.span(),
-                        "Expected literal. Found {:?}",
-                        uuid_literal
-                    );
-                }
-                if !validate_uuid(uuid_literal.to_string().as_str()) {
-                    abort!(uuid_literal.span(), "Invalid uuid");
-                }
-
-                let is_duplicated_uuid = uuid_name_map
-                    .insert(uuid_literal.to_string(), name_ident)
-                    .is_some();
-                if is_duplicated_uuid {
-                    abort!(uuid_literal.span(), "Duplicated UUID: {}", uuid_literal);
-                }
-            }
-            TokenTree::Punct(_) => continue,
-            _ => abort!(name_ident.span(), "Expected ':'. Found {:?}", name_ident),
+    let exports = input.uuid_name_map.into_iter().map(|(uuid, name)| {
+        quote! {
+            pub const #name: namui::Uuid = namui::uuid!(#uuid);
         }
-    }
+    });
 
-    create_known_id_exports(uuid_name_map)
+    TokenStream::from(quote! {
+        use namui::prelude::uuid;
+
+        #(#exports)*
+    })
 }
 
-fn check_token_tree_is_colon(token_tree: &TokenTree) -> bool {
-    match token_tree {
-        TokenTree::Punct(punct) => punct.as_char() == ':',
-        _ => false,
+struct MacroInput {
+    uuid_name_map: BTreeMap<String, Ident>,
+}
+impl Parse for MacroInput {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let uuid_name_pair_list = Punctuated::<NameUuidPair, Token!(,)>::parse_terminated(input)?;
+        let mut uuid_name_map = BTreeMap::<String, Ident>::new();
+        for NameUuidPair { name, uuid } in uuid_name_pair_list {
+            let is_duplicated_uuid = uuid_name_map.insert(uuid.value(), name).is_some();
+            if is_duplicated_uuid {
+                return Err(syn::Error::new(
+                    uuid.span(),
+                    format!("Duplicated UUID: {}", uuid.value()),
+                ));
+            }
+        }
+        Ok(Self { uuid_name_map })
     }
 }
 
-fn check_token_tree_is_literal(token_tree: &TokenTree) -> bool {
-    match token_tree {
-        TokenTree::Literal(_) => true,
-        _ => false,
+#[derive(Debug)]
+struct NameUuidPair {
+    name: Ident,
+    uuid: LitStr,
+}
+impl Parse for NameUuidPair {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let name: Ident = input.parse()?;
+        let _colon: Colon = input.parse()?;
+        let uuid: LitStr = input.parse()?;
+        if !validate_uuid(uuid.value().as_str()) {
+            return Err(syn::Error::new(uuid.span(), "Invalid uuid"));
+        }
+        Ok(Self { name, uuid })
     }
 }
 
 fn validate_uuid(uuid: &str) -> bool {
     lazy_static! {
         static ref UUID_REGEX: Regex = Regex::new(
-            "^\"[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}\"$"
+            "^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$"
         )
         .unwrap();
     }
     UUID_REGEX.is_match(uuid.as_ref())
-}
-
-fn create_known_id_exports(uuid_name_map: BTreeMap<String, Ident>) -> TokenStream {
-    let mut exports_string = "use namui::prelude::uuid;\n".to_string();
-    for (uuid, name) in uuid_name_map {
-        exports_string
-            .push_str(format!("pub const {name}: namui::Uuid = namui::uuid!({uuid});\n").as_str());
-    }
-    exports_string.parse().unwrap()
 }
