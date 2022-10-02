cdf038f2eee95dfd49308eb858cc16abf93eb621
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2022-10-02 19:46:29 +0900

    Fix wrong y baseline for text-input (#365)

--- namui/sample/text-input/src/lib.rs
+++ namui/sample/text-input/src/lib.rs
@@ -7,25 +7,49 @@ pub async fn main() {
 }
 
 struct TextInputExample {
-    left_text_input: namui::TextInput,
-    center_text_input: namui::TextInput,
-    right_text_input: namui::TextInput,
-    left_text: String,
-    center_text: String,
-    right_text: String,
-    left_value: Option<f32>,
+    text_input_3x3: [[namui::TextInput; 3]; 3],
+    text_3x3: [[String; 3]; 3],
+    left_top_value: Option<f32>,
 }
 
 impl TextInputExample {
     fn new() -> Self {
         Self {
-            left_text_input: namui::TextInput::new(),
-            center_text_input: namui::TextInput::new(),
-            right_text_input: namui::TextInput::new(),
-            left_text: "Left\nHelloy\n    SameText".to_string(),
-            center_text: "Center\nworldy\nSameText".to_string(),
-            right_text: "Right\n안녕하세요.\nSameText    ".to_string(),
-            left_value: None,
+            text_input_3x3: [
+                [
+                    namui::TextInput::new(),
+                    namui::TextInput::new(),
+                    namui::TextInput::new(),
+                ],
+                [
+                    namui::TextInput::new(),
+                    namui::TextInput::new(),
+                    namui::TextInput::new(),
+                ],
+                [
+                    namui::TextInput::new(),
+                    namui::TextInput::new(),
+                    namui::TextInput::new(),
+                ],
+            ],
+            text_3x3: [
+                [
+                    "Left Top\nHello you!\nmamama mimimi mo".to_string(),
+                    "Center Top\nHello you!\nmamama mimimi mo".to_string(),
+                    "Right Top\nHello you!\nmamama mimimi mo".to_string(),
+                ],
+                [
+                    "Left Center\nHello you!\nmamama mimimi mo".to_string(),
+                    "Center Center\nHello you!\nmamama mimimi mo".to_string(),
+                    "Right Center\nHello you!\nmamama mimimi mo".to_string(),
+                ],
+                [
+                    "Left Bottom\nHello you!\nmamama mimimi mo".to_string(),
+                    "Center Bottom\nHello you!\nmamama mimimi mo".to_string(),
+                    "Right Bottom\nHello you!\nmamama mimimi mo".to_string(),
+                ],
+            ],
+            left_top_value: None,
         }
     }
 }
@@ -34,111 +58,59 @@ impl Entity for TextInputExample {
     type Props = ();
 
     fn render(&self, _props: &Self::Props) -> RenderingTree {
-        let left = self.left_text_input.render(namui::text_input::Props {
-            rect: Rect::Xywh {
-                x: 200.0.px(),
-                y: px(200.0),
-                width: px(200.0),
-                height: px(200.0),
-            },
-            rect_style: RectStyle {
-                stroke: Some(RectStroke {
-                    border_position: BorderPosition::Inside,
-                    color: Color::BLACK,
-                    width: px(1.0),
-                }),
-                ..Default::default()
-            },
-            text_align: TextAlign::Left,
-            text_baseline: TextBaseline::Top,
-            text: self.left_text.clone(),
-            font_type: namui::FontType {
-                font_weight: namui::FontWeight::REGULAR,
-                language: namui::Language::Ko,
-                serif: false,
-                size: int_px(20),
-            },
-            text_style: namui::TextStyle {
-                color: namui::Color::BLACK,
-                ..Default::default()
-            },
-            event_handler: None,
-        });
+        let mut tree = vec![];
+        for x in 0..3 {
+            for y in 0..3 {
+                tree.push(self.text_input_3x3[x][y].render(text_input::Props {
+                    rect: Rect::Xywh {
+                        x: (x as f32 * 300.0 + 100.0).px(),
+                        y: (y as f32 * 300.0 + 100.0).px(),
+                        width: px(200.0),
+                        height: px(200.0),
+                    },
+                    rect_style: RectStyle {
+                        stroke: Some(RectStroke {
+                            border_position: BorderPosition::Inside,
+                            color: Color::BLACK,
+                            width: px(1.0),
+                        }),
+                        ..Default::default()
+                    },
+                    text_align: match x {
+                        x if x == 0 => TextAlign::Left,
+                        x if x == 1 => TextAlign::Center,
+                        x if x == 2 => TextAlign::Right,
+                        _ => unreachable!(),
+                    },
+                    text_baseline: match y {
+                        y if y == 0 => TextBaseline::Top,
+                        y if y == 1 => TextBaseline::Middle,
+                        y if y == 2 => TextBaseline::Bottom,
+                        _ => unreachable!(),
+                    },
+                    text: self.text_3x3[x][y].clone(),
+                    font_type: namui::FontType {
+                        font_weight: namui::FontWeight::REGULAR,
+                        language: namui::Language::Ko,
+                        serif: false,
+                        size: int_px(20),
+                    },
+                    text_style: namui::TextStyle {
+                        color: namui::Color::BLACK,
+                        ..Default::default()
+                    },
+                    event_handler: None,
+                }));
+            }
+        }
 
-        let center = self.center_text_input.render(namui::text_input::Props {
-            rect: Rect::Xywh {
-                x: px(500.0),
-                y: px(200.0),
-                width: px(200.0),
-                height: px(200.0),
-            },
-            rect_style: RectStyle {
-                stroke: Some(RectStroke {
-                    border_position: BorderPosition::Inside,
-                    color: Color::BLACK,
-                    width: px(1.0),
-                }),
-                ..Default::default()
-            },
-            text_align: TextAlign::Center,
-            text_baseline: TextBaseline::Top,
-            text: self.center_text.clone(),
-            font_type: namui::FontType {
-                font_weight: namui::FontWeight::REGULAR,
-                language: namui::Language::Ko,
-                serif: false,
-                size: int_px(20),
-            },
-            text_style: namui::TextStyle {
-                color: namui::Color::BLACK,
-                ..Default::default()
-            },
-            event_handler: None,
-        });
-
-        let right = self.right_text_input.render(namui::text_input::Props {
-            rect: Rect::Xywh {
-                x: px(800.0),
-                y: px(200.0),
-                width: px(200.0),
-                height: px(200.0),
-            },
-            rect_style: RectStyle {
-                stroke: Some(RectStroke {
-                    border_position: BorderPosition::Inside,
-                    color: Color::BLACK,
-                    width: px(1.0),
-                }),
-                ..Default::default()
-            },
-            text_align: TextAlign::Right,
-            text_baseline: TextBaseline::Top,
-            text: self.right_text.clone(),
-            font_type: namui::FontType {
-                font_weight: namui::FontWeight::REGULAR,
-                language: namui::Language::Ko,
-                serif: false,
-                size: int_px(20),
-            },
-            text_style: namui::TextStyle {
-                color: namui::Color::BLACK,
-                ..Default::default()
-            },
-            event_handler: Some(text_input::EventHandler::new().on_key_down(|event| {
-                if event.code == namui::Code::Digit7 {
-                    namui::log!("7 key is pressed, prevent default");
-                    event.prevent_default();
-                }
-            })),
-        });
-
-        let left_value_text = namui::text(TextParam {
-            x: px(200.0),
-            y: px(500.0),
+        let left_top_value_text = namui::text(TextParam {
+            x: px(10.0),
+            y: px(10.0),
             align: TextAlign::Left,
             baseline: TextBaseline::Top,
             text: self
-                .left_value
+                .left_top_value
                 .map(|v| v.to_string())
                 .unwrap_or("is't not f32".to_string()),
             font_type: namui::FontType {
@@ -154,25 +126,31 @@ impl Entity for TextInputExample {
             max_width: Some(100.px()),
         });
 
-        render![left, center, right, left_value_text]
+        tree.push(left_top_value_text);
+
+        render(tree)
     }
 
     fn update(&mut self, event: &dyn std::any::Any) {
         if let Some(event) = event.downcast_ref::<text_input::Event>() {
             match event {
                 text_input::Event::TextUpdated { id, text, .. } => {
-                    if self.left_text_input.get_id().eq(id) {
-                        self.left_text = text.clone();
-                        self.left_value = self.left_text.parse().ok(); // NOTE: You don't have to check value in here, it's would be better UX checking it on blur.
-                    } else if self.center_text_input.get_id().eq(id) {
-                        self.center_text = text.clone();
-                    } else if self.right_text_input.get_id().eq(id) {
-                        self.right_text = text.clone();
+                    for x in 0..3 {
+                        for y in 0..3 {
+                            if self.text_input_3x3[x][y].get_id() == id {
+                                self.text_3x3[x][y] = text.clone();
+
+                                if x == 0 && y == 0 {
+                                    self.left_top_value = self.text_3x3[x][y].parse().ok();
+                                    // NOTE: You don't have to check value in here, it's would be better UX checking it on blur.
+                                }
+                            }
+                        }
                     }
                 }
                 text_input::Event::Blur { id } => {
-                    if self.left_text_input.get_id().eq(id) {
-                        self.left_value = self.left_text.parse().ok();
+                    if self.text_input_3x3[0][0].get_id().eq(id) {
+                        self.left_top_value = self.text_3x3[0][0].parse().ok();
                     }
                 }
                 _ => {}
