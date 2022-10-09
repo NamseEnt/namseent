792125951a97c9cda0bbdc5c1d9a3ad924207532
bigfoodK <satcom1600@hanmail.net>
2022-10-09 15:32:16 +0900

    Remove export id macro

--- luda-adventure/src/app/game/known_id/quest.rs
+++ luda-adventure/src/app/game/known_id/quest.rs
@@ -1,5 +1,3 @@
-use export_known_ids_macro::export_known_ids;
+use namui::prelude::*;
 
-export_known_ids![
-    FIRST_QUEST: "3c8029c2-5f2f-4c4f-8116-c9b466f6ebc7",
-];
+pub const FIRST_QUEST: Uuid = uuid!("3c8029c2-5f2f-4c4f-8116-c9b466f6ebc7");
