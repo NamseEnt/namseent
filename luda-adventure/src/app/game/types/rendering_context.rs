a4d89326a76571cf471ec2b618dfbfda40b085cb
bigfoodK <satcom1600@hanmail.net>
2022-10-23 13:17:50 +0000

    Use state's currentime everywhere

--- luda-adventure/src/app/game/types/rendering_context.rs
+++ luda-adventure/src/app/game/types/rendering_context.rs
@@ -2,7 +2,6 @@ use super::Tile;
 use namui::prelude::*;
 
 pub struct RenderingContext {
-    pub current_time: Time,
     pub px_per_tile: Per<Px, Tile>,
     pub screen_rect: Rect<Tile>,
     pub interpolation_progress: f32,
