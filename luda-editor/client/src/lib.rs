447d7aad3f06bf92ca2e38e3e107e76d80e61fd4
namse <skatpgusskat@naver.com>
2022-11-01 10:01:28 +0000

    Add viewer

--- luda-editor/client/src/lib.rs
+++ luda-editor/client/src/lib.rs
@@ -4,21 +4,22 @@ mod late_init;
 mod pages;
 mod setting;
 mod storage;
+mod viewer;
+
+use namui::prelude::*;
 
 #[cfg(test)]
 #[cfg(target_family = "wasm")]
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
+static SETTING: late_init::LateInit<setting::Setting> =
+    late_init::LateInit::<setting::Setting>::new();
+static RPC: late_init::LateInit<rpc::Rpc> = late_init::LateInit::<rpc::Rpc>::new();
+
 pub async fn main() {
-    let is_auth_callback = web_sys::window()
-        .unwrap()
-        .location()
-        .search()
-        .unwrap()
-        .is_empty()
-        == false;
+    let search = web_sys::window().unwrap().location().search().unwrap();
+    let is_auth_callback = search.starts_with("?code=");
     if is_auth_callback {
-        // It's auth callback
         return;
     }
 
@@ -41,9 +42,50 @@ pub async fn main() {
     SETTING.init(setting);
     RPC.init(rpc::Rpc::new(SETTING.rpc_endpoint.clone()));
 
-    namui::start(namui_context, &mut app::App::new(), &()).await
+    let view_request = parse_view_request(&search);
+
+    match view_request {
+        Some(view_request) => {
+            namui::start(
+                namui_context,
+                &mut viewer::Viewer::new(view_request.sequence_id),
+                &(),
+            )
+            .await
+        }
+        None => namui::start(namui_context, &mut app::App::new(), &()).await,
+    }
 }
 
-static SETTING: late_init::LateInit<setting::Setting> =
-    late_init::LateInit::<setting::Setting>::new();
-static RPC: late_init::LateInit<rpc::Rpc> = late_init::LateInit::<rpc::Rpc>::new();
+struct ViewRequest {
+    sequence_id: Uuid,
+}
+fn parse_view_request(search: &str) -> Option<ViewRequest> {
+    if search.len() < 2 {
+        return None;
+    }
+
+    let query_tuples = search[1..].split('&').map(|s| {
+        let mut iter = s.split('=');
+        let key = iter.next().unwrap_or_default();
+        let value = iter.next().unwrap_or_default();
+        (key, value)
+    });
+    let mut sequence_id = None;
+    let mut is_view_request = false;
+    for (key, value) in query_tuples {
+        if key == "sequence_id" {
+            sequence_id = Some(value);
+        } else if key == "view" {
+            is_view_request = true;
+        }
+    }
+
+    if is_view_request && sequence_id.is_some() {
+        Some(ViewRequest {
+            sequence_id: sequence_id.unwrap().parse().unwrap(),
+        })
+    } else {
+        None
+    }
+}
