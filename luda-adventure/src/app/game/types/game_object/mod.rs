3b341809f005db0a498e1b918cd551c259840103
bigfoodK <satcom1600@hanmail.net>
2022-11-11 08:25:34 +0000

    Rename types to component

--- luda-adventure/src/app/game/types/game_object/mod.rs
+++ luda-adventure/src/app/game/types/game_object/mod.rs
@@ -1,9 +1,9 @@
+pub mod component;
 pub mod floor;
 pub mod player_character;
-pub mod types;
 pub mod wall;
 
+pub use component::*;
 pub use floor::*;
 pub use player_character::*;
-pub use types::*;
 pub use wall::*;
