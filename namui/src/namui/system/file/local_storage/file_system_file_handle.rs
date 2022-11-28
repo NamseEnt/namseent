186038ce2f024741a27de18c8fcba4f25bf4ceb2
bigfoodK <satcom1600@hanmail.net>
2022-11-28 10:19:24 +0000

    Import FileSystemFileHandle from js

---
+++ namui/src/namui/system/file/local_storage/file_system_file_handle.rs
@@ -0,0 +1,61 @@
+use super::file_system_handle::FileSystemHandle;
+use futures::Future;
+use js_sys::Promise;
+use std::pin::Pin;
+use wasm_bindgen::{prelude::*, JsCast};
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{File, WritableStream};
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(extends=FileSystemHandle, js_name=FileSystemFileHandle)]
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    #[doc = "The `FileSystemFileHandle` class."]
+    #[doc = ""]
+    #[doc = "[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/FileSystemFileHandle)"]
+    pub type FileSystemFileHandle;
+
+    #[wasm_bindgen(method, js_class="FileSystemFileHandle", js_name=getFile)]
+    #[doc = "The `getFile` method."]
+    #[doc = ""]
+    #[doc = "[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/FileSystemFileHandle/getFile)"]
+    fn get_file_unchecked(this: &FileSystemFileHandle) -> Promise;
+
+    #[wasm_bindgen(method, js_class="FileSystemFileHandle", js_name=createWritable)]
+    #[doc = "The `createWritable` method."]
+    #[doc = ""]
+    #[doc = "[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/FileSystemFileHandle/createWritable)"]
+    fn create_writable_unchecked(this: &FileSystemFileHandle) -> Promise;
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(extends=WritableStream, js_name=FileSystemWritableFileStream)]
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    #[doc = "The `FileSystemWritableFileStream` class."]
+    #[doc = ""]
+    #[doc = "[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/FileSystemWritableFileStream)"]
+    pub type FileSystemWritableFileStream;
+}
+
+impl<'a> FileSystemFileHandle {
+    pub fn get_file(&'a self) -> Pin<Box<dyn Future<Output = Result<File, JsValue>> + 'a>> {
+        Box::pin(async move {
+            let promise = self.get_file_unchecked();
+            let js_value = JsFuture::from(promise).await?;
+            let file: File = js_value.dyn_into()?;
+            Ok(file)
+        })
+    }
+
+    pub fn create_writable(
+        &'a self,
+    ) -> Pin<Box<dyn Future<Output = Result<FileSystemWritableFileStream, JsValue>> + 'a>> {
+        Box::pin(async move {
+            let promise = self.get_file_unchecked();
+            let js_value = JsFuture::from(promise).await?;
+            let writable_file_stream: FileSystemWritableFileStream = js_value.dyn_into()?;
+            Ok(writable_file_stream)
+        })
+    }
+}
--- namui/src/namui/system/file/local_storage/mod.rs
+++ namui/src/namui/system/file/local_storage/mod.rs
@@ -1,4 +1,5 @@
 mod file_system_directory_handle;
+mod file_system_file_handle;
 mod file_system_handle;
 mod get_root_directory;
 mod read_dir;
