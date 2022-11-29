5e05e593dbcd0642dd2bc58ec5220c36e6a320d4
bigfoodK <38313680+bigfoodK@users.noreply.github.com>
2022-11-29 16:45:22 +0900

    Update cargo dependencies (#469)

--- luda-adventure/src/component/collider/rigid_body/circle.rs
+++ luda-adventure/src/component/collider/rigid_body/circle.rs
@@ -1,26 +1,26 @@
-use geo::{Contains, Coordinate, EuclideanDistance};
+use geo::{Contains, Coord, EuclideanDistance};
 
 #[derive(serde::Serialize, serde::Deserialize)]
 pub struct Circle {
-    pub center: Coordinate<f64>,
+    pub center: Coord<f64>,
     pub radius: f64,
 }
 
 impl Circle {
-    pub fn new(center: Coordinate<f64>, radius: f64) -> Self {
+    pub fn new(center: Coord<f64>, radius: f64) -> Self {
         Self { center, radius }
     }
 
     pub fn translate(&self, x: f64, y: f64) -> Self {
         Self {
-            center: self.center + Coordinate { x, y },
+            center: self.center + Coord { x, y },
             radius: self.radius,
         }
     }
 }
 
-impl Contains<Coordinate<f64>> for Circle {
-    fn contains(&self, rhs: &Coordinate<f64>) -> bool {
+impl Contains<Coord<f64>> for Circle {
+    fn contains(&self, rhs: &Coord<f64>) -> bool {
         self.center.euclidean_distance(rhs) <= self.radius
     }
 }
