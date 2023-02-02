bb93f787e2f05b219d5b7185385473f23aff1c5a
bigfoodK <satcom1600@hanmail.net>
2023-02-02 05:41:12 +0000

    Rollback to previous structure

--- namui/src/namui/system/typeface/load_sans_typeface_of_all_languages.rs
+++ namui/src/namui/system/typeface/load_sans_typeface_of_all_languages.rs
@@ -26,14 +26,19 @@ async fn load_fallback_font_typefaces() -> Result<(), Box<dyn std::error::Error>
 
 async fn get_noto_color_emoji_typeface() -> Result<Typeface, Box<dyn std::error::Error>> {
     let url = crate::Url::parse("bundle:__system__/font/NotoColorEmoji.woff2")?;
-    get_typeface(url).await
+    let bytes = get_file_from_bundle_with_cached(&url)
+        .await
+        .expect(format!("Could not fetch {}", url).as_str());
+
+    Ok(Typeface::new(&bytes))
 }
 
 pub async fn load_sans_typeface_of_all_languages() -> Result<(), Box<dyn std::error::Error>> {
     let typeface_file_urls: TypefaceFileUrls = get_typeface_file_urls().await?;
 
-    let typefaces = get_typefaces_from_file_urls(typeface_file_urls).await?;
-    typefaces.into_iter().for_each(|(typeface_type, typeface)| {
+    let typeface_files = get_typeface_files(&typeface_file_urls).await?;
+    typeface_files.iter().for_each(|(typeface_type, bytes)| {
+        let typeface = Arc::new(Typeface::new(bytes));
         crate::typeface::load_typeface(&typeface_type, typeface.clone());
         load_default_font_of_typeface(typeface);
     });
@@ -41,44 +46,13 @@ pub async fn load_sans_typeface_of_all_languages() -> Result<(), Box<dyn std::er
     Ok(())
 }
 
-async fn get_typefaces_from_file_urls(
-    typeface_file_urls: TypefaceFileUrls,
-) -> Result<HashMap<TypefaceType, Arc<Typeface>>, Box<dyn std::error::Error>> {
-    let iter = try_join_all(typeface_file_urls.iter().map(
-        |(typeface_type, font_file_url)| async move {
-            let url = crate::Url::parse(font_file_url)?;
-            get_typeface(url)
-                .await
-                .map(|typeface| (*typeface_type, Arc::new(typeface)))
-        },
-    ))
-    .await?;
-    Ok(HashMap::from_iter(iter))
-}
-
-async fn get_typeface(url: Url) -> Result<Typeface, Box<dyn std::error::Error>> {
-    let typeface = match crate::cache::get(url.as_str()).await? {
-        Some(cached_bytes) => Typeface::new(&cached_bytes),
-        None => {
-            let bytes = crate::file::bundle::read(&url)
-                .await
-                .expect(format!("Could not fetch {}", url).as_str());
-            crate::cache::set(url.as_str(), bytes.as_ref()).await?;
-            Typeface::new(&bytes)
-        }
-    };
-    Ok(typeface)
-}
-
 async fn load_typeface_file_urls_file() -> Result<TypefaceFileUrlsFile, Box<dyn std::error::Error>>
 {
     let url = crate::Url::parse("bundle:__system__/font/map.json")?;
-    let Some(cached_typeface_file_urls_file) = crate::cache::get_serde(url.as_str()).await? else {
-        let typeface_file_urls_file = crate::file::bundle::read_json(url.clone()).await?;
-        crate::cache::set_serde(url.as_str(), &typeface_file_urls_file).await?;
-        return Ok(typeface_file_urls_file)
-    };
-    Ok(cached_typeface_file_urls_file)
+    match serde_json::from_slice(&get_file_from_bundle_with_cached(&url).await?) {
+        Ok(typeface_file_urls_file) => Ok(typeface_file_urls_file),
+        Err(error) => Err(format!("{error:?}").into()),
+    }
 }
 
 async fn get_typeface_file_urls() -> Result<TypefaceFileUrls, Box<dyn std::error::Error>> {
@@ -101,6 +75,43 @@ async fn get_typeface_file_urls() -> Result<TypefaceFileUrls, Box<dyn std::error
                 })
         })
         .collect())
+}
+
+async fn get_typeface_files(
+    typeface_file_urls: &TypefaceFileUrls,
+) -> Result<HashMap<TypefaceType, Vec<u8>>, Box<dyn std::error::Error>> {
+    let iter = try_join_all(typeface_file_urls.iter().map(
+        |(typeface_type, font_file_url)| async move {
+            let url = crate::Url::parse(font_file_url)?;
+
+            let result: Result<_, Box<dyn std::error::Error>> =
+                match get_file_from_bundle_with_cached(&url).await {
+                    Ok(bytes) => Ok((*typeface_type, bytes)),
+                    Err(error) => Err(format!("Could not fetch {font_file_url} - {error}").into()),
+                };
+            result
+        },
+    ))
+    .await?;
+
+    Ok(HashMap::from_iter(iter))
+}
+
+async fn get_file_from_bundle_with_cached(
+    url: &crate::Url,
+) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let file = match crate::cache::get(url.as_str()).await? {
+        Some(cached_file) => cached_file.to_vec(),
+        None => {
+            let file = crate::file::bundle::read(url.clone())
+                .await?
+                .as_ref()
+                .to_vec();
+            crate::cache::set(url.as_str(), &file).await?;
+            file
+        }
+    };
+    Ok(file)
 }
 
 #[cfg(test)]
