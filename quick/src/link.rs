8e867881554858dec6b16b7b01a1798a4af4b8f2
namse <skatpgusskat@naver.com>
2023-02-12 04:42:03 +0000

    remove comment

--- quick/src/link.rs
+++ quick/src/link.rs
@@ -34,30 +34,4 @@ pub fn link(link_text: impl ToString) -> Link {
     Link {
         text: link_text.to_string(),
     }
-    // table::fit(table::FitAlign::LeftTop, {
-    //     let outer_margin = 4.px();
-
-    //     namui::text(TextParam {
-    //         text: link_text.to_string(),
-    //         x: outer_margin * 2,
-    //         y: outer_margin,
-    //         align: TextAlign::Left,
-    //         baseline: TextBaseline::Top,
-    //         font_type: crate::typography::REGULAR,
-    //         style: TextStyle {
-    //             color: Color::grayscale_f01(0.8),
-    //             background: Some(TextStyleBackground {
-    //                 color: crate::color::BACKGROUND,
-    //                 margin: None,
-    //             }),
-    //             underline: Some(
-    //                 PaintBuilder::new()
-    //                     .set_stroke_width(1.px())
-    //                     .set_color(Color::grayscale_alpha_f01(0.8, 0.5)),
-    //             ),
-    //             ..Default::default()
-    //         },
-    //         max_width: None,
-    //     })
-    // })
 }
