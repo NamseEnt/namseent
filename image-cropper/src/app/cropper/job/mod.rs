aecc80765d5e5d7eb2153023f55eb2ac4b618109
bigfoodK <38313680+bigfoodK@users.noreply.github.com>
2023-07-03 00:26:54 +0900

    Delete image cropper (#559)
    Co-authored-by: Nam Se Hyun <3580430+namse@users.noreply.github.com>

--- image-cropper/src/app/cropper/job/mod.rs
+++
@@ -1,8 +0,0 @@
-mod job;
-mod poly_selection_create;
-mod rect_selection_create;
-mod rect_selection_resize;
-pub use job::*;
-pub use poly_selection_create::*;
-pub use rect_selection_create::*;
-pub use rect_selection_resize::*;
