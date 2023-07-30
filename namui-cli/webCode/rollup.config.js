ad9ef9402db248cce12e910c11380168deab97ff
namse <skatpgusskat@naver.com>
2023-07-30 13:44:41 +0000

    Add node resolve in rollup config

--- namui-cli/webCode/rollup.config.js
+++ namui-cli/webCode/rollup.config.js
@@ -1,13 +1,14 @@
 import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
 
 /** @type {import('rollup').RollupOptions} */
 const defaultConfig = {
     output: {
         dir: "../www",
-        format: "umd",
+        format: "iife",
         sourcemap: "inline",
     },
-    plugins: [typescript()],
+    plugins: [typescript(), resolve()],
 };
 
 const inputs = ["src/main.ts", "src/worker.ts"];
