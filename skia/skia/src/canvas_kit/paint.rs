36a44b1bd17689070ebfbe80ae3127a9bfb5c78c
namse <skatpgusskat@naver.com>
2023-08-23 15:44:25 +0000

    Remove unused code

--- skia/skia/src/canvas_kit/paint.rs
+++ skia/skia/src/canvas_kit/paint.rs
@@ -15,42 +15,6 @@ impl CkPaint {
 
         CkPaint { canvas_kit_paint }
     }
-    // pub(crate) fn set_shader(&self, shader: Option<&Arc<CkShader>>) {
-    //     if self.last_set_shader.lock().unwrap().as_ref() == shader {
-    //         return;
-    //     }
-    //     self.canvas_kit_paint
-    //         .setShader(shader.map(|shader| &shader.canvas_kit_shader));
-    // }
-    // pub(crate) fn get_shader(&self) -> Option<Arc<CkShader>> {
-    //     self.last_set_shader.lock().unwrap().clone()
-    // }
-    pub fn get_stroke_cap(&self) -> StrokeCap {
-        let stroke_cap = self.canvas_kit_paint.getStrokeCap();
-
-        match stroke_cap.value() {
-            value if STROKE_CAP_BUTT_VALUE.eq(&value) => StrokeCap::Butt,
-            value if STROKE_CAP_ROUND_VALUE.eq(&value) => StrokeCap::Round,
-            value if STROKE_CAP_SQUARE_VALUE.eq(&value) => StrokeCap::Square,
-            value => panic!("Unknown stroke_cap value: {}", value),
-        }
-    }
-    pub fn get_stroke_join(&self) -> StrokeJoin {
-        let stroke_join = self.canvas_kit_paint.getStrokeJoin();
-
-        match stroke_join.value() {
-            value if STROKE_JOIN_BEVEL_VALUE.eq(&value) => StrokeJoin::Bevel,
-            value if STROKE_JOIN_MITER_VALUE.eq(&value) => StrokeJoin::Miter,
-            value if STROKE_JOIN_ROUND_VALUE.eq(&value) => StrokeJoin::Round,
-            value => panic!("Unknown stroke_join value: {}", value),
-        }
-    }
-    pub fn get_stroke_width(&self) -> Px {
-        px(self.canvas_kit_paint.getStrokeWidth())
-    }
-    pub fn get_stroke_miter(&self) -> Px {
-        px(self.canvas_kit_paint.getStrokeMiter())
-    }
 
     pub(crate) fn canvas_kit(&self) -> &CanvasKitPaint {
         &self.canvas_kit_paint
