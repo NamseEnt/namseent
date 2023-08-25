fb6105e0c9ec3b00439741759253da02f44c73b3
bigfoodK <38313680+bigfoodK@users.noreply.github.com>
2023-08-25 18:12:39 +0900

    Ignore luda-adventure (#591)

--- cargo-check-and-fmt-all.py
+++ cargo-check-and-fmt-all.py
@@ -3,25 +3,35 @@
 import os
 from platform import uname
 
+ignored_projects = list(
+    filter(
+        lambda line: not line.startswith("#"),
+        open(".ignored_projects", "r").readlines(),
+    )
+)
+
 
 def in_wsl() -> bool:
-    return 'microsoft-standard' in uname().release
+    return "microsoft-standard" in uname().release
 
 
 def run():
-    for manifest_path in os.popen("git ls-files | grep -e /Cargo.toml").read().splitlines():
+    for manifest_path in (
+        os.popen("git ls-files | grep -e /Cargo.toml").read().splitlines()
+    ):
         dir_path = os.path.dirname(manifest_path)
+        if dir_path in ignored_projects:
+            continue
+
         print(f"cd {dir_path} && cargo check")
-        exit = os.system(
-            f"cd {dir_path} && cargo check")
+        exit = os.system(f"cd {dir_path} && cargo check")
         if exit != 0:
             print(f"\n\n-- fail cargo check on {dir_path}\n\n")
             return
 
         print(f"cd {dir_path} && cargo fmt")
         dir_path = os.path.dirname(manifest_path)
-        exit = os.system(
-            f"cd {dir_path} && cargo fmt")
+        exit = os.system(f"cd {dir_path} && cargo fmt")
         if exit != 0:
             print(f"\n\n-- fail cargo fmt on {dir_path}\n\n")
             return
--- cargo-clean-all.py
+++ cargo-clean-all.py
@@ -3,18 +3,28 @@
 import os
 from platform import uname
 
+ignored_projects = list(
+    filter(
+        lambda line: not line.startswith("#"),
+        open(".ignored_projects", "r").readlines(),
+    )
+)
+
 
 def in_wsl() -> bool:
-    return 'microsoft-standard' in uname().release
+    return "microsoft-standard" in uname().release
 
 
 def run():
-    for manifest_path in os.popen("git ls-files | grep -e /Cargo.toml").read().splitlines():
+    for manifest_path in (
+        os.popen("git ls-files | grep -e /Cargo.toml").read().splitlines()
+    ):
         dir_path = os.path.dirname(manifest_path)
+        if dir_path in ignored_projects:
+            continue
 
         print(f"cd {dir_path} && cargo clean")
-        exit = os.system(
-            f"cd {dir_path} && cargo clean")
+        exit = os.system(f"cd {dir_path} && cargo clean")
         if exit != 0:
             print(f"\n\n-- fail cargo clean on {dir_path}\n\n")
             return
