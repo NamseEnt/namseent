0b9b1e4bdedc6d23e678258e9fd66ae26353c5bd
bigfoodK <38313680+bigfoodK@users.noreply.github.com>
2023-11-02 19:34:33 +0900

    Increase time, memory limit for complete_psd (#715)
    Closes #697

--- luda-editor/cdk/lib/cdk-stack.ts
+++ luda-editor/cdk/lib/cdk-stack.ts
@@ -110,6 +110,9 @@ export class CdkStack extends cdk.Stack {
                     ],
                 }),
             ],
+            timeout: cdk.Duration.seconds(30),
+            // https://github.com/NamseEnt/namseent/pull/715#discussion_r1379615534
+            memorySize: 1621,
         });
 
         const fnUrl = lambda.addFunctionUrl({
