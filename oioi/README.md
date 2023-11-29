729fb8bcec507fa67ba5e8b63a52848cf7a9ec8a
namse <skatpgusskat@naver.com>
2023-11-29 08:50:31 +0000

    Hello oioi-agent!

---
+++ oioi/README.md
@@ -0,0 +1,31 @@
+# oioi
+
+`O`ne `I`nstance `O`ne `I`mage
+
+oioi is the docker orchestration system.
+
+Unlike kubernetes, oioi doesn't abstract instance layer.
+
+You can set the map between instance type and image, and the amount of desired number of instances.
+
+Autoscaling and monitoring are also supported.
+
+# oioi AMI(Amazon machine image)
+
+oioi AMI is light machine image which contains oioi agent as daemon.
+
+# oioi agent
+
+oioi agent is the simple rust program which manage instance's running container.
+
+oioi agent tries to synchronize the container's image to ensure it is up-to-date by fetching newest container image information from oioi store periodically.
+
+# oioi store
+
+oioi store is the simple store which save the newest docker image information.
+
+For now, oioi system saves docker images in AWS ECR.
+
+# Auto scaling
+
+oioi doesn't provide auto scaling feature directly. It uses AWS Ec2 Auto Scaling.
---
+++ oioi/oioi-agent/src/main.rs
@@ -0,0 +1,153 @@
+use anyhow::Result;
+use bollard::{container::ListContainersOptions, Docker};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    real_main().await
+}
+
+const INTERVAL: std::time::Duration = std::time::Duration::from_secs(5);
+const CONTAINER_NAME: &str = "oioi";
+const GRACEFUL_SHUTDOWN_TIMEOUT_SECS: i64 = 30;
+
+async fn real_main() -> Result<()> {
+    let group_name = std::env::var("GROUP_NAME")?;
+
+    let config = aws_config::load_defaults(aws_config::BehaviorVersion::v2023_11_09()).await;
+    let aws_ssm_client = aws_sdk_ssm::Client::new(&config);
+
+    let docker = Docker::connect_with_local_defaults()?;
+
+    loop {
+        let Some(image) = get_image(&aws_ssm_client, &group_name).await? else {
+            println!("No image found for group {}.", group_name);
+            tokio::time::sleep(INTERVAL).await;
+            continue;
+        };
+
+        let running_image = get_running_image(&docker).await?;
+
+        if let Some(running_image) = running_image {
+            if running_image == image {
+                println!("Good! Image {} is already running.", image);
+                tokio::time::sleep(INTERVAL).await;
+                continue;
+            }
+            stop_running_container(&docker).await?;
+        }
+
+        run_new_container(&docker, &image).await?;
+
+        docker_prune(&docker).await?;
+
+        tokio::time::sleep(INTERVAL).await;
+    }
+}
+
+async fn get_image(
+    aws_ssm_client: &aws_sdk_ssm::Client,
+    group_name: &str,
+) -> Result<Option<String>> {
+    let parameter_path = format!("/oioi/{group_name}/image");
+
+    let image = aws_ssm_client
+        .get_parameter()
+        .name(&parameter_path)
+        .send()
+        .await?
+        .parameter
+        .and_then(|p| p.value);
+
+    Ok(image)
+}
+
+async fn get_running_image(docker: &Docker) -> Result<Option<String>> {
+    let containers = docker
+        .list_containers(None::<ListContainersOptions<String>>)
+        .await?;
+
+    for container in containers {
+        let Some(names) = container.names else {
+            continue;
+        };
+
+        if !names.contains(&CONTAINER_NAME.to_string()) {
+            continue;
+        }
+
+        let Some(image) = container.image else {
+            anyhow::bail!("Container image should not be empty");
+        };
+
+        return Ok(Some(image));
+    }
+
+    Ok(None)
+}
+
+async fn stop_running_container(docker: &Docker) -> Result<()> {
+    docker
+        .stop_container(
+            CONTAINER_NAME,
+            Some(bollard::container::StopContainerOptions {
+                t: GRACEFUL_SHUTDOWN_TIMEOUT_SECS,
+            }),
+        )
+        .await?;
+
+    docker
+        .remove_container(
+            CONTAINER_NAME,
+            Some(bollard::container::RemoveContainerOptions {
+                force: true,
+                ..Default::default()
+            }),
+        )
+        .await?;
+
+    Ok(())
+}
+
+async fn run_new_container(docker: &Docker, image: &str) -> Result<()> {
+    docker
+        .create_container(
+            Some(bollard::container::CreateContainerOptions {
+                name: CONTAINER_NAME,
+                platform: None,
+            }),
+            bollard::container::Config {
+                image: Some(image.to_string()),
+                ..Default::default()
+            },
+        )
+        .await?;
+
+    docker
+        .start_container(
+            CONTAINER_NAME,
+            None::<bollard::container::StartContainerOptions<String>>,
+        )
+        .await?;
+
+    Ok(())
+}
+
+async fn docker_prune(docker: &Docker) -> Result<()> {
+    docker
+        .prune_containers(None::<bollard::container::PruneContainersOptions<String>>)
+        .await?;
+
+    docker
+        .prune_images(None::<bollard::image::PruneImagesOptions<String>>)
+        .await?;
+
+    docker
+        .prune_networks(None::<bollard::network::PruneNetworksOptions<String>>)
+        .await?;
+
+    docker
+        .prune_volumes(None::<bollard::volume::PruneVolumesOptions<String>>)
+        .await?;
+
+    Ok(())
+}
