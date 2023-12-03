6960bdb7bba0342b2b53abf85675430d637552ee
namse <skatpgusskat@naver.com>
2023-12-03 17:47:11 +0000

    cool~!

--- oioi/agent/src/bash.rs
+++ oioi/agent/src/bash.rs
@@ -4,11 +4,7 @@ pub(crate) async fn run(command: impl AsRef<str>) -> Result<Vec<u8>> {
     let command = command.as_ref();
 
     let output = tokio::process::Command::new("bash")
-        .arg("-c")
-        .arg(format!(
-            "\"{command}\"",
-            command = command.replace('"', "\\\"")
-        ))
+        .args(["-c", command])
         .output()
         .await
         .map_err(|e| anyhow::anyhow!("Failed to run {command}: {e}"))?;
@@ -21,4 +17,15 @@ pub(crate) async fn run(command: impl AsRef<str>) -> Result<Vec<u8>> {
     }
 
     Ok(output.stdout)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_run() {
+        let output = run("echo hello").await.unwrap();
+        assert_eq!(output, b"hello\n");
+    }
 }
