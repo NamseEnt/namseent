360bf65f4ffb80fd290ab7ffdb42c7f5ade70418
namse <skatpgusskat@naver.com>
2023-12-08 14:03:27 +0000

    Pulse view and edit

---
+++ hololive-beat/pulse-viewer/src/App.tsx
@@ -0,0 +1,173 @@
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
+import kickOpus from "/kick.opus";
+import Peaks, { PeaksInstance } from "peaks.js";
+import kickTxt from "/kick.txt";
+
+function App() {
+    const [audioBuffer, setAudioBuffer] = useState<AudioBuffer>();
+    const [peaksInstance, setPeaksInstance] = useState<PeaksInstance>();
+    const zoomviewRef = useRef<HTMLDivElement>(null);
+    const overviewRef = useRef<HTMLDivElement>(null);
+    const mediaElementRef = useRef<HTMLAudioElement>(null);
+    const [onsetText, setOnsetText] = useState<string>();
+    const [timeInSeconds, setTimeInSeconds] = useState<number>();
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(kickOpus);
+            const arrayBuffer = await response.arrayBuffer();
+
+            const audioContext = new AudioContext();
+            const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+
+            setAudioBuffer(audioBuffer);
+        })();
+    }, []);
+
+    useEffect(() => {
+        const id = setTimeout(async () => {
+            const response = await fetch(kickTxt);
+            const text = await response.text();
+            setOnsetText(text);
+        }, 1000);
+
+        return () => {
+            clearTimeout(id);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (!audioBuffer) {
+            return;
+        }
+        Peaks.init(
+            {
+                zoomview: {
+                    container: zoomviewRef.current,
+                    playheadPadding: 8,
+                },
+                overview: {
+                    container: overviewRef.current,
+                    playheadPadding: 8,
+                },
+                webAudio: {
+                    audioBuffer,
+                },
+                mediaElement: mediaElementRef.current ?? undefined,
+                zoomLevels: [16, 32, 64, 128, 256, 512, 1024],
+                keyboard: true,
+            },
+            (err, peaks) => {
+                if (err) {
+                    throw err;
+                }
+                setPeaksInstance(peaks);
+                peaks?.views;
+            },
+        );
+    }, [audioBuffer]);
+
+    const onWheel = (e: React.WheelEvent<HTMLDivElement>) => {
+        if (!peaksInstance) {
+            return;
+        }
+
+        peaksInstance.player.seek;
+        peaksInstance.zoom.setZoom(
+            peaksInstance.zoom.getZoom() + (e.deltaY < 0 ? -1 : 1),
+        );
+    };
+
+    useEffect(() => {
+        if (!peaksInstance) {
+            return;
+        }
+
+        peaksInstance.on("player.timeupdate", () => {
+            setTimeInSeconds(peaksInstance.player.getCurrentTime());
+        });
+
+        const onKeyDown = (e: KeyboardEvent) => {
+            switch (e.code) {
+                case "Space":
+                    {
+                        const isPlaying = !mediaElementRef.current?.paused;
+                        if (isPlaying) {
+                            peaksInstance.player.pause();
+                        } else {
+                            peaksInstance.player.play();
+                        }
+                    }
+                    break;
+                case "Enter":
+                    {
+                        setOnsetText((prev) => {
+                            if (!prev) {
+                                return prev;
+                            }
+                            return [
+                                ...prev.split("\n").map((x) => Number(x)),
+                                peaksInstance.player.getCurrentTime(),
+                            ]
+                                .sort((a, b) => a - b)
+                                .join("\n");
+                        });
+                        peaksInstance.player.getCurrentTime;
+                    }
+                    break;
+            }
+        };
+        window.addEventListener("keydown", onKeyDown);
+
+        return () => {
+            window.removeEventListener("keydown", onKeyDown);
+        };
+    }, [peaksInstance]);
+
+    useEffect(() => {
+        if (!peaksInstance || !onsetText) {
+            return;
+        }
+
+        peaksInstance.points.removeAll();
+
+        onsetText.split("\n").forEach((value) => {
+            peaksInstance.points.add({
+                time: Number(value),
+                color: "red",
+            });
+        });
+    }, [onsetText, peaksInstance]);
+
+    return (
+        <div>
+            <div
+                ref={zoomviewRef}
+                style={{
+                    width: "80vw",
+                    height: "40vh",
+                }}
+                onWheel={onWheel}
+            />
+            <div
+                ref={overviewRef}
+                style={{
+                    width: "80vw",
+                    height: "20vh",
+                }}
+                onWheel={onWheel}
+            />
+            <audio ref={mediaElementRef} src={kickOpus} controls />
+            <span>Time in seconds: {timeInSeconds}</span>
+            <textarea
+                value={onsetText}
+                onChange={(e) => {
+                    setOnsetText(e.target.value);
+                }}
+            />
+        </div>
+    );
+}
+
+export default App;
