d4505968cab835f2f2fddffe7e6644962b2cc380
namse <skatpgusskat@naver.com>
2023-12-29 12:54:26 +0000

    Support custom duration

--- namui/namui-type/src/types/units/time/instant.rs
+++ namui/namui-type/src/types/units/time/instant.rs
@@ -1,4 +1,5 @@
 use crate::Duration;
+use std::fmt::Debug;
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub struct Instant {
@@ -15,21 +16,81 @@ impl Instant {
     }
 }
 
+impl Debug for Instant {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.inner.fmt(f)
+    }
+}
+
 auto_ops::impl_op!(-|lhs: Instant, rhs: Instant| -> Duration { sub_instant(lhs, rhs) });
 auto_ops::impl_op!(-|lhs: &Instant, rhs: Instant| -> Duration { sub_instant(*lhs, rhs) });
 auto_ops::impl_op!(-|lhs: Instant, rhs: &Instant| -> Duration { sub_instant(lhs, *rhs) });
 auto_ops::impl_op!(-|lhs: &Instant, rhs: &Instant| -> Duration { sub_instant(*lhs, *rhs) });
 
+auto_ops::impl_op!(+|lhs: Instant, rhs: Duration| -> Instant { add_duration(lhs, rhs) });
+auto_ops::impl_op!(+|lhs: &Instant, rhs: Duration| -> Instant { add_duration(*lhs, rhs) });
+auto_ops::impl_op!(+|lhs: Instant, rhs: &Duration| -> Instant { add_duration(lhs, *rhs) });
+auto_ops::impl_op!(+|lhs: &Instant, rhs: &Duration| -> Instant { add_duration(*lhs, *rhs) });
+
+auto_ops::impl_op!(-|lhs: Instant, rhs: Duration| -> Instant { add_duration(lhs, -rhs) });
+auto_ops::impl_op!(-|lhs: &Instant, rhs: Duration| -> Instant { add_duration(*lhs, -rhs) });
+auto_ops::impl_op!(-|lhs: Instant, rhs: &Duration| -> Instant { add_duration(lhs, -*rhs) });
+auto_ops::impl_op!(-|lhs: &Instant, rhs: &Duration| -> Instant { add_duration(*lhs, -*rhs) });
+
 #[cfg(not(target_family = "wasm"))]
 fn sub_instant(lhs: Instant, rhs: Instant) -> Duration {
-    if lhs.inner > rhs.inner {
-        Duration::from_std(lhs.inner - rhs.inner).unwrap()
-    } else {
-        -Duration::from_std(rhs.inner - lhs.inner).unwrap()
+    let sign = lhs.inner > rhs.inner;
+    let later = if sign { lhs.inner } else { rhs.inner };
+    let earlier = if sign { rhs.inner } else { lhs.inner };
+    let std_duration = later.duration_since(earlier);
+
+    Duration::from_std(sign, std_duration)
+}
+
+#[cfg(not(target_family = "wasm"))]
+fn add_duration(lhs: Instant, rhs: Duration) -> Instant {
+    match rhs.sign {
+        true => Instant {
+            inner: lhs.inner + rhs.inner,
+        },
+        false => Instant {
+            inner: lhs.inner - rhs.inner,
+        },
     }
 }
 
-#[cfg(target_family = "wasm")]
-fn sub_instant(lhs: Instant, rhs: Instant) -> Duration {
-    todo!()
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::*;
+
+    #[test]
+    fn test_instant_sub() {
+        let std_instant_now = std::time::Instant::now();
+
+        let a = Instant {
+            inner: std_instant_now,
+        };
+        let b = Instant {
+            inner: std_instant_now
+                .checked_add(std::time::Duration::from_secs(1))
+                .unwrap(),
+        };
+
+        assert_eq!(b - a, 1.sec());
+        assert_eq!(a - b, -1.sec());
+    }
+
+    #[test]
+    fn test_instant_add_duration() {
+        let std_instant_now = std::time::Instant::now();
+
+        let a = Instant {
+            inner: std_instant_now,
+        };
+        let b = a + 1.sec();
+
+        assert_eq!(b - a, 1.sec());
+        assert_eq!(a - b, -1.sec());
+    }
 }
--- namui/namui-type/src/types/units/time/system_time.rs
+++ namui/namui-type/src/types/units/time/system_time.rs
@@ -1,4 +1,5 @@
 use crate::Duration;
+use std::fmt::Debug;
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub struct SystemTime {
@@ -15,12 +16,28 @@ impl SystemTime {
     }
 }
 
+impl Debug for SystemTime {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.inner.fmt(f)
+    }
+}
+
 auto_ops::impl_op!(-|lhs: SystemTime, rhs: SystemTime| -> Duration { sub_system_time(lhs, rhs) });
 auto_ops::impl_op!(-|lhs: &SystemTime, rhs: SystemTime| -> Duration { sub_system_time(*lhs, rhs) });
 auto_ops::impl_op!(-|lhs: SystemTime, rhs: &SystemTime| -> Duration { sub_system_time(lhs, *rhs) });
 auto_ops::impl_op!(-|lhs: &SystemTime, rhs: &SystemTime| -> Duration {
     sub_system_time(*lhs, *rhs)
 });
+
+auto_ops::impl_op!(+|lhs: SystemTime, rhs: Duration| -> SystemTime { add_duration(lhs, rhs) });
+auto_ops::impl_op!(+|lhs: &SystemTime, rhs: Duration| -> SystemTime { add_duration(*lhs, rhs) });
+auto_ops::impl_op!(+|lhs: SystemTime, rhs: &Duration| -> SystemTime { add_duration(lhs, *rhs) });
+auto_ops::impl_op!(+|lhs: &SystemTime, rhs: &Duration| -> SystemTime { add_duration(*lhs, *rhs) });
+
+auto_ops::impl_op!(-|lhs: SystemTime, rhs: Duration| -> SystemTime { add_duration(lhs, -rhs) });
+auto_ops::impl_op!(-|lhs: &SystemTime, rhs: Duration| -> SystemTime { add_duration(*lhs, -rhs) });
+auto_ops::impl_op!(-|lhs: SystemTime, rhs: &Duration| -> SystemTime { add_duration(lhs, -*rhs) });
+auto_ops::impl_op!(-|lhs: &SystemTime, rhs: &Duration| -> SystemTime { add_duration(*lhs, -*rhs) });
 
 #[cfg(not(target_family = "wasm"))]
 fn sub_system_time(lhs: SystemTime, rhs: SystemTime) -> Duration {
@@ -29,16 +46,91 @@ fn sub_system_time(lhs: SystemTime, rhs: SystemTime) -> Duration {
         Err(err) => err.duration(),
     };
 
-    let duration = Duration::from_std(duration).unwrap();
+    let sign = lhs.inner > rhs.inner;
+    Duration::from_std(sign, duration)
+}
 
-    if lhs.inner > rhs.inner {
-        duration
-    } else {
-        -duration
+#[cfg(not(target_family = "wasm"))]
+fn add_duration(lhs: SystemTime, rhs: Duration) -> SystemTime {
+    match rhs.sign {
+        true => SystemTime {
+            inner: lhs.inner + rhs.inner,
+        },
+        false => SystemTime {
+            inner: lhs.inner - rhs.inner,
+        },
     }
 }
 
-#[cfg(target_family = "wasm")]
-fn sub_system_time(lhs: SystemTime, rhs: SystemTime) -> Duration {
-    todo!()
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::*;
+
+    #[test]
+    fn test_system_time_sub() {
+        let std_system_time_now = std::time::SystemTime::now();
+        let std_system_time_1s_ago = std_system_time_now - std::time::Duration::from_secs(1);
+        let std_system_time_2s_ago = std_system_time_now - std::time::Duration::from_secs(2);
+        let std_system_time_3s_ago = std_system_time_now - std::time::Duration::from_secs(3);
+
+        let system_time_now = SystemTime {
+            inner: std_system_time_now,
+        };
+        let system_time_1s_ago = SystemTime {
+            inner: std_system_time_1s_ago,
+        };
+        let system_time_2s_ago = SystemTime {
+            inner: std_system_time_2s_ago,
+        };
+        let system_time_3s_ago = SystemTime {
+            inner: std_system_time_3s_ago,
+        };
+
+        assert_eq!(system_time_now - system_time_1s_ago, Duration::from_secs(1));
+        assert_eq!(system_time_now - system_time_2s_ago, Duration::from_secs(2));
+        assert_eq!(system_time_now - system_time_3s_ago, Duration::from_secs(3));
+
+        assert_eq!(
+            system_time_1s_ago - system_time_now,
+            Duration::from_secs(-1)
+        );
+        assert_eq!(
+            system_time_2s_ago - system_time_now,
+            Duration::from_secs(-2)
+        );
+        assert_eq!(
+            system_time_3s_ago - system_time_now,
+            Duration::from_secs(-3)
+        );
+    }
+
+    #[test]
+    fn test_system_time_add_duration() {
+        let std_system_time_now = std::time::SystemTime::now();
+        let std_system_time_1s_ago = std_system_time_now + std::time::Duration::from_secs(1);
+        let std_system_time_2s_ago = std_system_time_now + std::time::Duration::from_secs(2);
+        let std_system_time_3s_ago = std_system_time_now + std::time::Duration::from_secs(3);
+
+        let system_time_now = SystemTime {
+            inner: std_system_time_now,
+        };
+        let system_time_1s_ago = SystemTime {
+            inner: std_system_time_1s_ago,
+        };
+        let system_time_2s_ago = SystemTime {
+            inner: std_system_time_2s_ago,
+        };
+        let system_time_3s_ago = SystemTime {
+            inner: std_system_time_3s_ago,
+        };
+
+        assert_eq!(system_time_now + 1.sec(), system_time_1s_ago);
+        assert_eq!(system_time_now + 2.sec(), system_time_2s_ago);
+        assert_eq!(system_time_now + 3.sec(), system_time_3s_ago);
+
+        assert_eq!(system_time_1s_ago + 1.sec(), system_time_2s_ago);
+        assert_eq!(system_time_1s_ago + 2.sec(), system_time_3s_ago);
+        assert_eq!(system_time_2s_ago + 1.sec(), system_time_3s_ago);
+    }
 }
