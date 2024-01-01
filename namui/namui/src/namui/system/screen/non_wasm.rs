3c7484ef3490cf6964a1b05951035364a59e362f
namse <skatpgusskat@naver.com>
2024-01-01 06:53:12 +0000

    Divide main thread and tokio runtime

--- namui/namui/src/namui/system/screen/non_wasm.rs
+++ namui/namui/src/namui/system/screen/non_wasm.rs
@@ -42,9 +42,13 @@ pub(crate) fn take_main_thread() {
 
     event_loop
         .run(|event, target| {
+<<<<<<< HEAD
             target.set_control_flow(winit::event_loop::ControlFlow::WaitUntil(
                 std::time::Instant::now() + std::time::Duration::from_millis(10),
             ));
+=======
+            target.set_control_flow(winit::event_loop::ControlFlow::Poll);
+>>>>>>> bb9542ab (Divide main thread and tokio runtime)
 
             match event {
                 winit::event::Event::WindowEvent {
