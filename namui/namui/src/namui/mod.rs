581e6ddfce083fc6700e4959ca093eb48b29a28e
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2024-01-01 22:13:53 +0900

    Fix error

--- namui/namui/src/namui/mod.rs
+++ namui/namui/src/namui/mod.rs
@@ -51,9 +51,7 @@ pub fn start<C: Component>(component: impl Send + Sync + Fn() -> C + 'static) {
 
                 crate::log!("Namui system initialized");
 
-                tokio::task::block_in_place(|| crate::hooks::run_loop(component))
-                    .await
-                    .unwrap();
+                tokio::task::block_in_place(|| crate::hooks::run_loop(component));
             })
     });
 
