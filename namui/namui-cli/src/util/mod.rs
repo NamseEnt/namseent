552f22a3a6fd7b65f907b6b4e2aa17029887422a
namse <skatpgusskat@naver.com>
2024-01-03 15:51:20 +0000

    Turn on target feature based on steam hw survey

--- namui/namui-cli/src/util/mod.rs
+++ namui/namui-cli/src/util/mod.rs
@@ -3,6 +3,7 @@ mod debug_println;
 mod get_cli_root_path;
 mod get_electron_root_path;
 mod print_build_result;
+mod recreate_dir_all;
 mod user_config;
 
 #[allow(unused_imports)]
@@ -10,4 +11,5 @@ pub use debug_println::*;
 pub use get_cli_root_path::*;
 pub use get_electron_root_path::*;
 pub use print_build_result::*;
+pub use recreate_dir_all::*;
 pub use user_config::*;
