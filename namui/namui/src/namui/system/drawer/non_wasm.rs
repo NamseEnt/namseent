11178cd3362c1ff5c9cc05044427dfb919d148d2
namse <skatpgusskat@naver.com>
2024-01-10 09:41:24 +0000

    Redraw forcibly and load image in tokio

--- namui/namui/src/namui/system/drawer/non_wasm.rs
+++ namui/namui/src/namui/system/drawer/non_wasm.rs
@@ -3,25 +3,55 @@
 use crate::{image::ImageBitmap, system::InitResult, *};
 use std::sync::{Mutex, OnceLock};
 
-static RENDERING_TREE_TX: OnceLock<std::sync::mpsc::Sender<RenderingTree>> = OnceLock::new();
+static DRAW_COMMAND_TX: OnceLock<tokio::sync::mpsc::UnboundedSender<DrawingCommand>> =
+    OnceLock::new();
+
+#[derive(Debug)]
+enum DrawingCommand {
+    Draw { rendering_tree: RenderingTree },
+    Redraw,
+}
 
 pub(crate) async fn init() -> InitResult {
-    let (tx, rx) = std::sync::mpsc::channel();
-    RENDERING_TREE_TX.set(tx).unwrap();
+    let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+    DRAW_COMMAND_TX.set(tx).unwrap();
 
-    spawn_drawing_thread(rx);
+    spawn_drawing_task(rx);
 
     Ok(())
 }
 
-fn spawn_drawing_thread(rx: std::sync::mpsc::Receiver<RenderingTree>) {
-    std::thread::spawn(move || {
-        while let Ok(mut rendering_tree) = rx.recv() {
-            rx.try_iter().for_each(|next_rendering_tree| {
-                rendering_tree = next_rendering_tree;
-            });
+fn spawn_drawing_task(mut rx: tokio::sync::mpsc::UnboundedReceiver<DrawingCommand>) {
+    tokio::spawn(async move {
+        let mut last_rendering_tree = None;
+        let mut rendering_tree_changed = false;
+        let mut redraw_requested = false;
 
-            system::skia::render(DrawInput { rendering_tree });
+        while let Some(command) = rx.recv().await {
+            let mut on_command = |command| match command {
+                DrawingCommand::Draw { rendering_tree } => {
+                    if last_rendering_tree.as_ref() != Some(&rendering_tree) {
+                        last_rendering_tree = Some(rendering_tree.clone());
+                        rendering_tree_changed = true;
+                    }
+                }
+                DrawingCommand::Redraw => {
+                    redraw_requested = true;
+                }
+            };
+
+            on_command(command);
+            while let Ok(next_command) = rx.try_recv() {
+                on_command(next_command);
+            }
+
+            if redraw_requested || rendering_tree_changed {
+                if let Some(rendering_tree) = last_rendering_tree.clone() {
+                    system::skia::render(DrawInput { rendering_tree });
+                    rendering_tree_changed = false;
+                    redraw_requested = false;
+                }
+            }
         }
     });
 }
@@ -36,10 +66,10 @@ pub(crate) fn request_draw_rendering_tree(rendering_tree: namui_type::RenderingT
         *last_rendering_tree = Some(rendering_tree.clone());
     }
 
-    RENDERING_TREE_TX
+    DRAW_COMMAND_TX
         .get()
         .unwrap()
-        .send(rendering_tree)
+        .send(DrawingCommand::Draw { rendering_tree })
         .unwrap();
 }
 
@@ -51,6 +81,11 @@ pub(crate) fn load_image(_image_source: &ImageSource, _image_bitmap: ImageBitmap
     // nothing. already loaded
 }
 
+/// This function redraw forcibly.
 pub(crate) fn redraw() {
-    // NOTE: We may not need this because we redraw by polling on winit event loop.
+    DRAW_COMMAND_TX
+        .get()
+        .unwrap()
+        .send(DrawingCommand::Redraw)
+        .unwrap();
 }
