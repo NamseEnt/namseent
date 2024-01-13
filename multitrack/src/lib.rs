14056447b51818735383b636a0f268682b248f8f
namse <skatpgusskat@naver.com>
2024-01-13 18:11:37 +0000

    Drag to select

--- multitrack/src/lib.rs
+++ multitrack/src/lib.rs
@@ -14,6 +14,16 @@ pub fn main() {
 #[namui::component]
 struct App;
 
+#[derive(Debug)]
+enum DragState {
+    None,
+    Dragging {
+        track_index: usize,
+        start_sample_index: usize,
+        cursor_sample_index: usize,
+    },
+}
+
 impl Component for App {
     fn render(self, ctx: &RenderCtx) -> RenderDone {
         let screen_wh: Wh<Px> = namui::screen::size().into_type();
@@ -21,13 +31,14 @@ impl Component for App {
         let (audio, set_audio) = ctx.state(|| None);
         let (window_size, set_window_size) = ctx.state(|| SAMPLE_RATE as usize);
         let (start_sample_index, set_start_sample_index) = ctx.state(|| 0_usize);
+        let (drag_state, set_drag_state) = ctx.state(|| DragState::None);
 
         let range = *start_sample_index..(*start_sample_index + *window_size);
 
         ctx.effect("load raw audio", || {
             namui::spawn(async move {
                 let raw_audio: media::audio::RawAudio = namui::media::audio::RawAudio::load(
-                    &&namui::system::file::bundle::to_real_path("bundle:resources/snare.opus")
+                    &namui::system::file::bundle::to_real_path("bundle:resources/snare.opus")
                         .unwrap(),
                     Some(SAMPLE_RATE),
                 )
@@ -48,6 +59,15 @@ impl Component for App {
 
             let audio_length = audio.channels[0].len();
 
+            let sample_index_on_x = |x: Px| {
+                let cursor_x_ratio = x / screen_wh.width;
+
+                let cursor_sample_index =
+                    *start_sample_index as f32 + (*window_size as f32 * cursor_x_ratio);
+
+                cursor_sample_index.clamp(0.0, audio_length as f32) as usize
+            };
+
             table::hooks::vertical([
                 table::hooks::fixed(120.px(), |wh, ctx| {
                     ctx.add(MiniMap {
@@ -58,13 +78,54 @@ impl Component for App {
                 }),
                 table::hooks::ratio(
                     1,
-                    table::hooks::vertical((0..5).map(|_| {
-                        table::hooks::ratio(1, |wh, ctx| {
-                            ctx.add(Track {
-                                wh,
-                                audio: audio.clone(),
-                                range: range.clone(),
-                            });
+                    table::hooks::vertical((0..5).map(|track_index| {
+                        let range = range.clone();
+
+                        table::hooks::ratio(1, move |wh, ctx| {
+                            ctx.add(
+                                Track {
+                                    wh,
+                                    audio: audio.clone(),
+                                    visual_range: range,
+                                    selection_range: match *drag_state {
+                                        DragState::None => None,
+                                        DragState::Dragging {
+                                            track_index: drag_track_index,
+                                            start_sample_index,
+                                            cursor_sample_index,
+                                        } => {
+                                            if track_index == drag_track_index {
+                                                Some(
+                                                    start_sample_index.min(cursor_sample_index)
+                                                        ..start_sample_index
+                                                            .max(cursor_sample_index),
+                                                )
+                                            } else {
+                                                None
+                                            }
+                                        }
+                                    },
+                                }
+                                .attach_event(move |event| {
+                                    if let DragState::None = *drag_state {
+                                        if let Event::MouseDown { event } = event {
+                                            if !event.is_local_xy_in() {
+                                                return;
+                                            }
+
+                                            set_drag_state.set(DragState::Dragging {
+                                                track_index,
+                                                start_sample_index: sample_index_on_x(
+                                                    event.local_xy().x,
+                                                ),
+                                                cursor_sample_index: sample_index_on_x(
+                                                    event.local_xy().x,
+                                                ),
+                                            });
+                                        }
+                                    }
+                                }),
+                            );
                         })
                     })),
                 ),
@@ -73,37 +134,67 @@ impl Component for App {
             ctx.add(
                 simple_rect(screen_wh, Color::TRANSPARENT, 0.px(), Color::TRANSPARENT)
                     .attach_event(|event| {
-                        if let Event::Wheel { event } = event {
-                            if namui::keyboard::ctrl_press() {
-                                // zoom
+                        match event {
+                            Event::Wheel { event } => {
+                                if namui::keyboard::ctrl_press() {
+                                    // zoom
 
-                                let zoom_delta = 1.0 + event.delta_xy.y / 10.0;
-                                let next_window_size = (zoom_delta * *window_size as f32) as usize;
+                                    let zoom_delta = 1.0 + event.delta_xy.y / 10.0;
+                                    let next_window_size =
+                                        (zoom_delta * *window_size as f32) as usize;
 
-                                set_window_size.set(next_window_size);
+                                    set_window_size.set(next_window_size);
 
-                                let cursor_x_ratio = event.local_xy().x / screen_wh.width;
+                                    let cursor_x_ratio = event.local_xy().x / screen_wh.width;
 
-                                let cursor_sample_index = *start_sample_index as f32
-                                    + (*window_size as f32 * cursor_x_ratio);
+                                    let cursor_sample_index = *start_sample_index as f32
+                                        + (*window_size as f32 * cursor_x_ratio);
 
-                                let next_start_sample_index = cursor_sample_index
-                                    - zoom_delta * (*window_size as f32 * cursor_x_ratio);
+                                    let next_start_sample_index = cursor_sample_index
+                                        - zoom_delta * (*window_size as f32 * cursor_x_ratio);
 
-                                set_start_sample_index.set(
-                                    next_start_sample_index
-                                        .clamp(0.0, audio_length as f32 - next_window_size as f32)
-                                        as usize,
-                                );
-                            } else {
-                                // move
+                                    set_start_sample_index.set(
+                                        next_start_sample_index.clamp(
+                                            0.0,
+                                            audio_length as f32 - next_window_size as f32,
+                                        ) as usize,
+                                    );
+                                } else {
+                                    // move
 
-                                let delta = (event.delta_xy.y / 10.0) * *window_size as f32;
-                                set_start_sample_index.set(
-                                    ((*start_sample_index as f32 - delta) as usize)
-                                        .clamp(0, audio_length - *window_size),
-                                );
+                                    let delta = (event.delta_xy.y / 10.0) * *window_size as f32;
+                                    set_start_sample_index.set(
+                                        ((*start_sample_index as f32 - delta) as usize)
+                                            .clamp(0, audio_length - *window_size),
+                                    );
+                                }
                             }
+                            Event::MouseMove { event } => {
+                                if let DragState::Dragging {
+                                    track_index,
+                                    start_sample_index,
+                                    cursor_sample_index: _,
+                                } = *drag_state
+                                {
+                                    set_drag_state.set(DragState::Dragging {
+                                        track_index,
+                                        start_sample_index,
+                                        cursor_sample_index: sample_index_on_x(event.local_xy().x),
+                                    });
+                                }
+                            }
+                            Event::MouseUp { event: _ } => {
+                                if let DragState::Dragging {
+                                    track_index: _,
+                                    start_sample_index: _,
+                                    cursor_sample_index: _,
+                                } = *drag_state
+                                {
+                                    // TODO
+                                    set_drag_state.set(DragState::None);
+                                }
+                            }
+                            _ => (),
                         }
                     }),
             );
--- multitrack/src/track.rs
+++ multitrack/src/track.rs
@@ -6,27 +6,33 @@ use std::sync::Arc;
 pub struct Track {
     pub wh: Wh<Px>,
     pub audio: Arc<RawAudio>,
-    pub range: std::ops::Range<usize>,
+    pub visual_range: std::ops::Range<usize>,
+    pub selection_range: Option<std::ops::Range<usize>>,
 }
 
 impl Component for Track {
     fn render(self, ctx: &RenderCtx) -> RenderDone {
-        let Self { wh, range, audio } = self;
+        let Self {
+            wh,
+            audio,
+            visual_range,
+            selection_range,
+        } = self;
 
         let (waveform_path, set_waveform_path) = ctx.state(Path::new);
         let wh = ctx.track_eq(&wh);
-        let range = ctx.track_eq(&range);
+        let visual_range = ctx.track_eq(&visual_range);
 
         ctx.effect("calculate waveform", || {
             let mut path = namui::Path::new();
-            let step = (range.len() as f32 / wh.width.as_f32()).floor() as usize;
+            let step = (visual_range.len() as f32 / wh.width.as_f32()).floor() as usize;
             for (channel_index, steps) in audio
                 .channels
                 .iter()
                 .map(|channel| {
                     channel
                         .as_slice()
-                        .get((*range).clone())
+                        .get((*visual_range).clone())
                         .unwrap_or(&[])
                         .chunks(step)
                         .map(|chunk| chunk.first().copied().unwrap_or(0.0))
@@ -64,14 +70,34 @@ impl Component for Track {
                 .set_stroke_width(1.px()),
         ));
 
-        let background = simple_rect(
+        ctx.component(selection_range.as_ref().map(|selection_range| {
+            let left_sample_index = selection_range.start.max(visual_range.start);
+            let right_sample_index = selection_range.end.min(visual_range.end);
+
+            let left_x = wh.width
+                * ((left_sample_index - visual_range.start) as f32 / visual_range.len() as f32);
+            let width_px = (wh.width
+                * ((right_sample_index - left_sample_index) as f32 / visual_range.len() as f32))
+                .min(wh.width);
+
+            namui::translate(
+                left_x,
+                0.px(),
+                simple_rect(
+                    Wh::new(width_px, wh.height),
+                    Color::from_u8(134, 74, 249, 255),
+                    1.px(),
+                    Color::from_u8(134, 74, 249, 255),
+                ),
+            )
+        }));
+
+        ctx.component(simple_rect(
             *wh,
             Color::grayscale_f01(0.6),
             1.px(),
             Color::grayscale_f01(0.5),
-        );
-
-        ctx.component(background);
+        ));
 
         ctx.done()
     }
