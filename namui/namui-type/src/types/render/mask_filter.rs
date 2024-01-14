50964f3318f056af57318a836d4e49bf0c845dc8
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2024-01-14 16:47:16 +0900

    Convert radius to sigma, vice versa

--- namui/namui-type/src/types/render/mask_filter.rs
+++ namui/namui-type/src/types/render/mask_filter.rs
@@ -16,3 +16,24 @@ pub enum Blur {
     /// Fuzzy inside, nothing outside
     Inner { sigma: f32 },
 }
+
+/// https://android.googlesource.com/platform/frameworks/base/+/41fceb4/libs/hwui/utils/Blur.cpp
+/// This constant approximates the scaling done in the software path's
+/// "high quality" mode, in SkBlurMask::Blur() (1 / sqrt(3)).
+const BLUR_SIGMA_SCALE: f32 = 0.57735;
+
+impl Blur {
+    pub fn convert_radius_to_sigma(radius: f32) -> f32 {
+        if radius <= 0.0 {
+            return 0.0;
+        }
+        radius * BLUR_SIGMA_SCALE + 0.5
+    }
+
+    pub fn convert_sigma_to_radius(sigma: f32) -> f32 {
+        if sigma <= 0.5 {
+            return 0.0;
+        }
+        (sigma - 0.5) / BLUR_SIGMA_SCALE
+    }
+}
