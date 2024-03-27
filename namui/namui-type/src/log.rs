06a7067c4d0364f0deb8ff632905b738f3606cdc
namse <skatpgusskat@naver.com>
2024-03-27 15:05:58 +0000

    Clippy

--- namui/namui-type/src/log.rs
+++ namui/namui-type/src/log.rs
@@ -9,7 +9,7 @@ pub fn set_log(log: impl Fn(&str) + 'static) {
 
 pub fn log(content: impl AsRef<str>) {
     unsafe {
-        if let Some(log) = &LOG_FN {
+        if let Some(log) = LOG_FN.as_ref() {
             log(content.as_ref());
         }
     }
