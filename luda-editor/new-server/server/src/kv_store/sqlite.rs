8fba74e0867a2fbf83993b7d1914c23a7037c146
namse <skatpgusskat@naver.com>
2024-05-14 10:14:21 +0000

    Custom backup

--- luda-editor/new-server/server/src/kv_store/sqlite.rs
+++ luda-editor/new-server/server/src/kv_store/sqlite.rs
@@ -1,40 +1,68 @@
 use super::{HeapArchived, KvStore};
 use anyhow::Result;
-use r2d2::Pool;
-use r2d2_sqlite::{rusqlite::OptionalExtension, SqliteConnectionManager};
 use rkyv::ser::serializers::AllocSerializer;
-use std::future::Future;
+use rusqlite::{Connection, OpenFlags, OptionalExtension};
+use std::{
+    future::Future,
+    sync::{Arc, Mutex, MutexGuard},
+};
 
 #[derive(Clone)]
 pub struct SqliteKvStore {
-    pool: Pool<SqliteConnectionManager>,
+    write: Arc<Mutex<Connection>>,
+    sqlite3: *mut rusqlite::ffi::sqlite3,
 }
+const DB_PATH: &str = "db.sqlite";
 impl SqliteKvStore {
     pub fn new() -> Self {
-        const DB_PATH: &str = "db.sqlite";
-        let manager = SqliteConnectionManager::file(DB_PATH).with_init(|c| {
-            c.execute(
-                "CREATE TABLE IF NOT EXISTS kv_store (key TEXT PRIMARY KEY, value BLOB, version INTEGER)",
-                [],
-            )
-            .map(|_| ())
-        });
+        try_fetch_db_file_from_s3(DB_PATH);
 
-        Self {
-            pool: Pool::new(manager).unwrap(),
+        let conn = Connection::open(DB_PATH).unwrap();
+
+        conn.execute("PRAGMA journal_mode = WAL;", []).unwrap();
+        conn.execute("PRAGMA synchronous = NORMAL;", []).unwrap();
+        conn.execute(
+                        "CREATE TABLE IF NOT EXISTS kv_store (key TEXT PRIMARY KEY, value BLOB, version INTEGER)",
+                [],
+        ).unwrap();
+
+        let sqlite3 = conn.db.borrow_mut().db;
+        let write = Arc::new(Mutex::new(conn));
+
+        Self { write, sqlite3 }
+    }
+    async fn backup(&self) -> Result<()> {
+        const BACKUP_PATH: &str = "db.sqlite.backup";
+        let _ = std::fs::remove_file(BACKUP_PATH);
+
+        rusqlite::backup::Backup::custom_backup(self.sqlite3, BACKUP_PATH, 256, || async move {
+            tokio::time::sleep(tokio::time::Duration::from_millis(250)).await;
+        })
+        .await?;
+
+        Ok(())
+    }
+    fn read_conn<T>(&self, f: impl FnOnce(&Connection) -> T) -> T {
+        thread_local! {
+            static READ: Connection = Connection::open_with_flags(DB_PATH, OpenFlags::SQLITE_OPEN_READ_ONLY).unwrap();
         }
+        READ.with(f)
+    }
+    fn write_conn(&self) -> MutexGuard<Connection> {
+        self.write.lock().unwrap()
     }
 }
 
 impl KvStore for SqliteKvStore {
     fn get<T: rkyv::Archive>(&self, key: impl AsRef<str>) -> Result<Option<HeapArchived<T>>> {
-        let conn = self.pool.get().unwrap();
-        let mut stmt = conn.prepare("SELECT value FROM kv_store WHERE key = ?")?;
-        let vec: Option<Vec<_>> = stmt
-            .query_row([key.as_ref()], |row| row.get(0))
-            .optional()?;
+        self.read_conn(|read_conn| {
+            let mut stmt = read_conn.prepare("SELECT value FROM kv_store WHERE key = ?")?;
+            let vec: Option<Vec<_>> = stmt
+                .query_row([key.as_ref()], |row| row.get(0))
+                .optional()?;
 
-        Ok(vec.map(|vec| HeapArchived::new(vec)))
+            Ok(vec.map(|vec| HeapArchived::new(vec)))
+        })
     }
 
     fn put<T: rkyv::Serialize<AllocSerializer<0>>>(
@@ -42,9 +70,9 @@ impl KvStore for SqliteKvStore {
         key: impl AsRef<str>,
         value: &T,
     ) -> Result<()> {
-        let conn = self.pool.get().unwrap();
-        let mut stmt =
-            conn.prepare("INSERT OR REPLACE INTO kv_store (key, value, version) VALUES (?, ?, 0)")?;
+        let write_conn = self.write_conn();
+        let mut stmt = write_conn
+            .prepare("INSERT OR REPLACE INTO kv_store (key, value, version) VALUES (?, ?, 0)")?;
         let buffer = rkyv::to_bytes(value)?;
         assert_eq!(stmt.execute((key.as_ref(), buffer.as_slice()))?, 1);
 
@@ -52,8 +80,8 @@ impl KvStore for SqliteKvStore {
     }
 
     fn delete(&self, key: impl AsRef<str>) -> Result<()> {
-        let conn = self.pool.get().unwrap();
-        let mut stmt = conn.prepare("DELETE FROM kv_store WHERE key = ?")?;
+        let write_conn = self.write_conn();
+        let mut stmt = write_conn.prepare("DELETE FROM kv_store WHERE key = ?")?;
         stmt.execute([key.as_ref()])?;
 
         Ok(())
@@ -68,13 +96,13 @@ impl KvStore for SqliteKvStore {
         T: rkyv::Archive + rkyv::Serialize<AllocSerializer<0>>,
         Fut: Future<Output = Option<Option<T>>>,
     {
-        let conn = self.pool.get().unwrap();
-
-        let output: Option<(Vec<_>, usize)> = {
-            let mut stmt = conn.prepare("SELECT (value, version) FROM kv_store WHERE key = ?")?;
+        let output: Option<(Vec<_>, usize)> = self.read_conn(|read_conn| {
+            let mut stmt =
+                read_conn.prepare("SELECT (value, version) FROM kv_store WHERE key = ?")?;
             stmt.query_row([key.as_ref()], |row| Ok((row.get(0)?, row.get(1)?)))
-                .optional()?
-        };
+                .optional()
+        })?;
+
         let (old_value, version) = match output {
             Some((vec, version)) => (Some(HeapArchived::new(vec)), Some(version)),
             None => (None, None),
@@ -84,8 +112,9 @@ impl KvStore for SqliteKvStore {
             return Ok(true);
         };
 
+        let write_conn = self.write_conn();
         let Some(new_value) = new_value else {
-            let mut stmt = conn.prepare("DELETE FROM kv_store WHERE key = ?")?;
+            let mut stmt = write_conn.prepare("DELETE FROM kv_store WHERE key = ?")?;
             stmt.execute([key.as_ref()])?;
             return Ok(true);
         };
@@ -93,7 +122,7 @@ impl KvStore for SqliteKvStore {
         let buffer = rkyv::to_bytes(&new_value)?;
 
         if let Some(version) = version {
-            let mut stmt = conn.prepare(
+            let mut stmt = write_conn.prepare(
                 "UPDATE kv_store SET value = ?, version = version + 1 WHERE key = ? AND version = ?",
             )?;
             let changed = stmt.execute((buffer.as_slice(), key.as_ref(), version))?;
@@ -101,7 +130,7 @@ impl KvStore for SqliteKvStore {
         }
 
         let mut stmt =
-            conn.prepare("INSERT INTO kv_store (key, value, version) VALUES (?, ?, 0)")?;
+            write_conn.prepare("INSERT INTO kv_store (key, value, version) VALUES (?, ?, 0)")?;
 
         match stmt.execute((key.as_ref(), buffer.as_slice())) {
             Ok(count) => {
@@ -109,13 +138,38 @@ impl KvStore for SqliteKvStore {
                 Ok(true)
             }
             Err(err) => {
-                if let r2d2_sqlite::rusqlite::Error::SqliteFailure(err, _) = err {
-                    if let r2d2_sqlite::rusqlite::ErrorCode::ConstraintViolation = err.code {
+                if let rusqlite::Error::SqliteFailure(err, _) = err {
+                    if let rusqlite::ErrorCode::ConstraintViolation = err.code {
                         return Ok(false);
                     }
                 }
                 Err(err.into())
             }
         }
+    }
+}
+
+fn try_fetch_db_file_from_s3(db_path: &str) {
+    if std::fs::metadata(db_path).is_ok() {
+        return;
+    }
+
+    let output = std::process::Command::new("aws")
+        .args([
+            "s3",
+            "cp",
+            &format!("s3://{}/db.sqlite", std::env::var("BUCKET_NAME").unwrap()),
+            db_path,
+        ])
+        .output()
+        .unwrap();
+
+    let std_err = String::from_utf8_lossy(&output.stderr);
+    if !output.status.success()
+        && !std_err.starts_with(
+            "fatal error: An error occurred (404) when calling the HeadObject operation:",
+        )
+    {
+        panic!("Failed to download db.sqlite: {}", std_err);
     }
 }
