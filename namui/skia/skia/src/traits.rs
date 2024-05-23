07db790fa5e22a82b3d51ece8e0aff15afd06957
namse <skatpgusskat@naver.com>
2024-05-23 05:45:40 +0000

    Fix test

--- namui/skia/skia/src/traits.rs
+++ namui/skia/skia/src/traits.rs
@@ -1,7 +1,9 @@
 use crate::*;
-use anyhow::Result;
+pub use anyhow::Result;
 use namui_type::*;
 use std::sync::Arc;
+
+pub type JoinHandle<T> = tokio::task::JoinHandle<T>;
 
 pub trait SkSkia: SkCalculate {
     fn move_to_next_frame(&mut self);
@@ -18,21 +20,13 @@ pub trait SkSkia: SkCalculate {
 pub trait SkCalculate {
     fn group_glyph(&self, font: &Font, paint: &Paint) -> Arc<dyn GroupGlyph>;
     fn font_metrics(&self, font: &Font) -> Option<FontMetrics>;
-    fn load_typeface(
-        &self,
-        typeface_name: String,
-        bytes: Vec<u8>,
-    ) -> tokio::task::JoinHandle<Result<()>>;
+    fn load_typeface(&self, typeface_name: String, bytes: Vec<u8>) -> JoinHandle<Result<()>>;
     fn path_contains_xy(&self, path: &Path, paint: Option<&Paint>, xy: Xy<Px>) -> bool;
     fn path_bounding_box(&self, path: &Path, paint: Option<&Paint>) -> Option<Rect<Px>>;
     #[cfg(not(target_family = "wasm"))]
-    fn load_image_from_encoded(&self, bytes: &[u8]) -> tokio::task::JoinHandle<Image>;
+    fn load_image_from_encoded(&self, bytes: &[u8]) -> JoinHandle<Image>;
     #[cfg(not(target_family = "wasm"))]
-    fn load_image_from_raw(
-        &self,
-        image_info: ImageInfo,
-        bytes: &[u8],
-    ) -> tokio::task::JoinHandle<Image>;
+    fn load_image_from_raw(&self, image_info: ImageInfo, bytes: &[u8]) -> JoinHandle<Image>;
 }
 
 pub trait SkSurface {
