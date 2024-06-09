a2a0fe9f560eef3a74db9faa789555c0e6c7a15d
bigfood <satcom1600@hanmail.net>
2024-06-09 23:40:58 +0900

    Add LevelSelectPage

--- jigcle/src/level.rs
+++ jigcle/src/level.rs
@@ -1,4 +1,5 @@
-use namui::{file::bundle, Result, Wh};
+use file::bundle;
+use namui::*;
 use serde::{Deserialize, Serialize};
 
 const LEVEL_LIST_PATH: &str = "bundle:level/list.yaml";
@@ -9,11 +10,16 @@ pub async fn load_level_list() -> Result<Vec<Level>> {
     Ok(level_list)
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Level {
     pub name: String,
     pub puzzle_size: Wh<usize>,
-    pub image_ratio: f32,
+    pub aspect_ratio: f32,
     pub image_filename: String,
     pub audio_filename: String,
+}
+impl Level {
+    pub fn image_path(&self) -> String {
+        format!("bundle:level/{}/{}", self.name, self.image_filename)
+    }
 }
