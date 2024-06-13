9adb47153d864f81f800a79db027541943bbd256
bigfoodk <satcom1600@hanmail.net>
2024-06-13 22:35:37 +0900

    Extract wasi-sdk with stripped

--- namui/namui-cli/build.rs
+++ namui/namui-cli/build.rs
@@ -91,7 +91,17 @@ fn download_wasi_sdk() -> Result<()> {
 
     let mut d = flate2::read::GzDecoder::new(response);
     let mut archive = tar::Archive::new(&mut d);
-    archive.unpack(&dist)?;
+    for entry in archive.entries()? {
+        let mut entry = entry?;
+        let Some(stripped_path) = entry.path().map_or(None, |path| {
+            path.strip_prefix("wasi-sdk-22.0")
+                .map(|path| path.to_path_buf())
+                .ok()
+        }) else {
+            continue;
+        };
+        entry.unpack(&dist.join(stripped_path)).unwrap();
+    }
 
     Ok(())
 }
