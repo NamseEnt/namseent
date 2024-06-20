822033861311b6b8a2c96d4a302dcc6bb87547d5
namse <skatpgusskat@naver.com>
2024-06-20 13:21:07 +0000

    Remove exports

--- namui/namui-cli/webCode/src/exports.ts
+++ namui/namui-cli/webCode/src/exports.ts
@@ -2,9 +2,4 @@ export type Exports = {
     wasi_thread_start: (tid: number, startArgPtr: number) => void;
     _malloc: (size: number) => number;
     _free: (ptr: number) => void;
-
-    on_web_socket_open: (id: number) => void;
-    on_web_socket_close: (id: number) => void;
-    web_socket_message_alloc: (data_len: number) => number;
-    on_web_socket_message: (id: number, data_ptr: number) => void;
 };
