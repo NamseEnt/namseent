22f2d08e4f910f7b43ba3d8be8fab3db3ef2a406
namse <skatpgusskat@naver.com>
2024-06-20 12:57:17 +0000

    Change websocket with polling buffer

---
+++ namui/namui-cli/webCode/src/webSocket.ts
@@ -0,0 +1,290 @@
+import { sendMessageToMainThread } from "./interWorkerProtocol";
+
+/*
+# eventBuffer protocol
+
+[ws id: u32][message type: u8][message data: ...]
+
+- 0x01: on open
+- 0x02: on close
+- 0x03: on small message (<= 64KB)
+    - u16: byte length
+    - u8[data length]: data
+- 0x04: on big message start (< 4GB)
+    - u32: total byte length
+    - u16: chunk count
+- 0x05: on big message chunk
+    - u16: chunk byte length
+    - u8[data length]: data
+*/
+
+export function webSocketImports({ memory }: { memory: WebAssembly.Memory }) {
+    // ASSUME: this imports only run on specific one thread.
+
+    const writtenBuffer = new SharedArrayBuffer(4);
+
+    return {
+        _init_web_socket_thread: (
+            eventBufferPtr: number,
+            eventBufferLen: number,
+        ) => {
+            const eventBuffer = memory.buffer.slice(
+                eventBufferPtr,
+                eventBufferPtr + eventBufferLen,
+            );
+            if (!(eventBuffer instanceof SharedArrayBuffer)) {
+                throw new Error("eventBuffer must be SharedArrayBuffer");
+            }
+
+            sendMessageToMainThread({
+                type: "init-web-socket-thread",
+                eventBuffer,
+                writtenBuffer,
+            });
+        },
+        _web_socket_event_poll: (): number => {
+            Atomics.wait(new Int32Array(writtenBuffer), 0, 0);
+            return Atomics.load(new Int32Array(writtenBuffer), 0);
+        },
+        _web_socket_event_commit: (len: number) => {
+            Atomics.sub(new Int32Array(writtenBuffer), 0, len);
+        },
+        _new_web_socket: (urlPtr: number, urlLen: number): number => {
+            const urlBuffer = new Uint8Array(urlLen);
+            urlBuffer.set(new Uint8Array(memory.buffer, urlPtr, urlLen));
+            const url = new TextDecoder().decode(urlBuffer);
+
+            const idBuffer = new SharedArrayBuffer(4);
+            sendMessageToMainThread({
+                type: "new-web-socket",
+                url,
+                idBuffer,
+            });
+
+            Atomics.wait(new Int32Array(idBuffer), 0, 0);
+            const id = new Uint32Array(idBuffer)[0];
+
+            return id;
+        },
+        _web_socket_send: (id: number, data_ptr: number, data_len: number) => {
+            const data = new Uint8Array(data_len);
+            data.set(new Uint8Array(memory.buffer, data_ptr, data_len));
+            sendMessageToMainThread({
+                type: "web-socket-send",
+                id,
+                data,
+            });
+        },
+    };
+}
+
+export function webSocketHandleOnMainThread({
+    eventBuffer,
+    writtenBuffer,
+}: {
+    eventBuffer: SharedArrayBuffer;
+    writtenBuffer: SharedArrayBuffer;
+}) {
+    let nextId = 1;
+    const webSockets = new Map<number, WebSocket>();
+    const messages: {
+        id: number;
+        data: ArrayBuffer;
+    }[] = [];
+    let writerIndex = 0;
+
+    function onNewWebSocket({
+        url,
+        idBuffer,
+    }: {
+        url: string;
+        idBuffer: SharedArrayBuffer;
+    }) {
+        const webSocket = new WebSocket(url);
+        const id = nextId++;
+        webSockets.set(id, webSocket);
+
+        webSocket.onopen = async () => {
+            await write(["u32", id], ["u8", 0x01]);
+        };
+        webSocket.onclose = async () => {
+            await write(["u32", id], ["u8", 0x02]);
+        };
+        webSocket.onmessage = (event: MessageEvent<ArrayBuffer>) => {
+            messages.push({
+                id,
+                data: event.data,
+            });
+            if (messages.length === 1) {
+                loopSendingMessage();
+            }
+        };
+
+        new Uint32Array(idBuffer)[0] = id;
+        Atomics.notify(new Int32Array(idBuffer), 0);
+    }
+
+    function send({ id, data }: { id: number; data: ArrayBuffer }) {
+        webSockets.get(id)?.send(data);
+    }
+
+    return {
+        onNewWebSocket,
+        send,
+    };
+
+    // below implementations
+    /**
+     * Only one execution at a time.
+     */
+    async function loopSendingMessage() {
+        const message = messages[0];
+        if (!message) {
+            return;
+        }
+
+        const _64KB = 64 * 1024;
+        const { data, id } = message;
+
+        const isSmallMessage = data.byteLength <= _64KB;
+        if (isSmallMessage) {
+            await write(
+                ["u32", id],
+                ["u8", 0x03],
+                ["u16", data.byteLength],
+                ["bytes", data],
+            );
+        } else {
+            const chunkCount = Math.ceil(data.byteLength / _64KB);
+
+            await write(
+                ["u32", id],
+                ["u8", 0x04],
+                ["u32", data.byteLength],
+                ["u16", chunkCount],
+            );
+
+            for (
+                let sentBytes = 0;
+                sentBytes < data.byteLength;
+                sentBytes += _64KB
+            ) {
+                const chunkSize = Math.min(_64KB, data.byteLength - sentBytes);
+
+                write(
+                    ["u32", id],
+                    ["u8", 0x05],
+                    ["u16", chunkSize],
+                    ["bytes", data.slice(sentBytes, sentBytes + chunkSize)],
+                );
+            }
+        }
+
+        messages.shift();
+        loopSendingMessage();
+    }
+
+    async function write(
+        ...tuples: (
+            | ["u8", number]
+            | ["u16", number]
+            | ["u32", number]
+            | ["bytes", ArrayBuffer]
+        )[]
+    ) {
+        const totalByteLength = tuples.reduce((a, b) => {
+            switch (b[0]) {
+                case "u8":
+                    return a + 1;
+                case "u16":
+                    return a + 2;
+                case "u32":
+                    return a + 4;
+                case "bytes":
+                    return a + b[1].byteLength;
+            }
+        }, 0);
+
+        await waitForBufferAvailable(totalByteLength);
+
+        for (const tuple of tuples) {
+            write(tuple);
+        }
+
+        Atomics.add(new Int32Array(writtenBuffer), 0, totalByteLength);
+        Atomics.notify(new Int32Array(writtenBuffer), 1);
+
+        return; // below implementations
+
+        function write(
+            tuple:
+                | ["u8", number]
+                | ["u16", number]
+                | ["u32", number]
+                | ["bytes", ArrayBuffer],
+        ) {
+            if (eventBuffer.byteLength <= writerIndex) {
+                writerIndex = 0;
+            }
+
+            const type = tuple[0];
+            let value: ArrayBuffer;
+            switch (type) {
+                case "u8": {
+                    value = new Uint8Array([tuple[1]]);
+                    break;
+                }
+                case "u16": {
+                    value = new Uint16Array([tuple[1]]);
+                    break;
+                }
+                case "u32": {
+                    value = new Uint32Array([tuple[1]]);
+                    break;
+                }
+                case "bytes": {
+                    value = tuple[1];
+                    break;
+                }
+                default: {
+                    throw new Error(`Unsupported type: ${type}`);
+                }
+            }
+
+            const bufferRight = eventBuffer.byteLength - writerIndex;
+            new Uint8Array(eventBuffer).set(
+                new Uint8Array(value, 0, bufferRight),
+                writerIndex,
+            );
+
+            if (value.byteLength <= bufferRight) {
+                writerIndex += value.byteLength;
+                return value.byteLength;
+            }
+
+            const left = value.byteLength - bufferRight;
+            new Uint8Array(eventBuffer).set(
+                new Uint8Array(value, bufferRight),
+                0,
+            );
+            writerIndex = left;
+        }
+        async function waitForBufferAvailable(byteLength: number) {
+            while (true) {
+                const written = Atomics.load(new Int32Array(writtenBuffer), 0);
+                const bufferAvailable = eventBuffer.byteLength - written;
+                if (byteLength <= bufferAvailable) {
+                    return;
+                }
+                const { async, value } = Atomics.waitAsync(
+                    new Int32Array(writtenBuffer),
+                    0,
+                    written,
+                );
+                if (async) {
+                    await value;
+                }
+            }
+        }
+    }
+}
--- namui/namui/src/namui/system/network/ws/wasi.rs
+++ namui/namui/src/namui/system/network/ws/wasi.rs
@@ -1,56 +1,145 @@
 use anyhow::Result;
 use dashmap::DashMap;
-use std::sync::OnceLock;
+use std::{borrow::Cow, sync::OnceLock};
 
 /// For easy implementation, I chose to use a single thread for all WebSocket connections.
-struct WsThread {
-    job_tx: std::sync::mpsc::Sender<WsThreadJob>,
-}
+///
+/// ```text
+/// # eventBuffer protocol
+/// [ws id: u32][message type: u8][message data: ...]
+/// - 0x01: on open
+/// - 0x02: on close
+/// - 0x03: on small message (<= 64KB)
+///     - u16: byte length
+///     - u8[data length]: data
+/// - 0x04: on big message start (< 4GB)
+///     - u32: total byte length
+///     - u16: chunk count
+/// - 0x05: on big message chunk
+///     - u16: chunk byte length
+///     - u8[data length]: data
+/// ```
+struct WsThread {}
 static WS_THREAD: OnceLock<WsThread> = OnceLock::new();
+struct EventBuffer {
+    buffer: Box<[u8]>,
+    buffer_index: usize,
+    read_count: usize,
+}
+impl EventBuffer {
+    fn new() -> Self {
+        Self {
+            buffer: vec![0; 4 * 1024 * 1024].into_boxed_slice(),
+            buffer_index: 0,
+            read_count: 0,
+        }
+    }
+    fn flush_read_count(&mut self) -> usize {
+        let read_count = self.read_count;
+        self.read_count = 0;
+        read_count
+    }
+    fn read_bytes(&mut self, byte_length: usize) -> Cow<'_, [u8]> {
+        if self.buffer_index + byte_length <= self.buffer.len() {
+            let slice = &self.buffer[self.buffer_index..self.buffer_index + byte_length];
+            self.buffer_index += byte_length;
+            Cow::Borrowed(slice)
+        } else {
+            let mut vec = vec![0; byte_length];
+            vec.copy_from_slice(&self.buffer[self.buffer_index..]);
+            self.buffer_index = byte_length - (self.buffer.len() - self.buffer_index);
+            Cow::from(vec)
+        }
+    }
+    fn read_u8(&mut self) -> u8 {
+        self.read_bytes(1)[0]
+    }
+    fn read_u16(&mut self) -> u16 {
+        let bytes = self.read_bytes(2);
+        u16::from_le_bytes([bytes[0], bytes[1]])
+    }
+    fn read_u32(&mut self) -> u32 {
+        let bytes = self.read_bytes(4);
+        u32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]])
+    }
+}
 impl WsThread {
     fn new() -> Self {
-        let (job_tx, job_rx) = std::sync::mpsc::channel();
         std::thread::spawn(move || {
-            while let Ok(job) = job_rx.recv() {
-                match job {
-                    WsThreadJob::NewWebSocket { url, result } => {
-                        let id = unsafe { new_web_socket(url.as_ptr(), url.len()) };
-                        let _ = result.send(id);
+            let mut event_buffer = EventBuffer::new();
+
+            unsafe {
+                _init_web_socket_thread(event_buffer.buffer.as_ptr(), event_buffer.buffer.len());
+            }
+
+            let send_event = |id, event: WsEvent| {
+                let Some(tx) = WS_EVENT_TX.get().unwrap().get(&id) else {
+                    return;
+                };
+                let _ = tx.send(event);
+            };
+
+            loop {
+                let byte_length = unsafe { _web_socket_event_poll() };
+                assert!(byte_length != 0);
+
+                let id = event_buffer.read_u32();
+                let message_type = event_buffer.read_u8();
+                match message_type {
+                    0x01 => {
+                        send_event(id, WsEvent::Open);
+                        unsafe { _web_socket_event_commit(event_buffer.flush_read_count()) }
                     }
-                    WsThreadJob::Send { id, data } => {
-                        unsafe { web_socket_send(id, data.as_ptr(), data.len()) };
+                    0x02 => {
+                        send_event(id, WsEvent::Close);
+                        unsafe { _web_socket_event_commit(event_buffer.flush_read_count()) }
                     }
+                    0x03 => {
+                        let data_length = event_buffer.read_u16() as usize;
+                        let data = event_buffer
+                            .read_bytes(data_length)
+                            .into_owned()
+                            .into_boxed_slice();
+                        send_event(id, WsEvent::Message(data));
+                        unsafe { _web_socket_event_commit(event_buffer.flush_read_count()) }
+                    }
+                    0x04 => {
+                        let total_byte_length = event_buffer.read_u32() as usize;
+                        let chunk_count = event_buffer.read_u16() as usize;
+
+                        unsafe { _web_socket_event_commit(event_buffer.flush_read_count()) }
+
+                        let mut data = Vec::with_capacity(total_byte_length);
+                        for _ in 0..chunk_count {
+                            let chunk_byte_length = event_buffer.read_u16() as usize;
+                            let chunk_data =
+                                event_buffer.read_bytes(chunk_byte_length).into_owned();
+
+                            data.extend_from_slice(&chunk_data);
+
+                            unsafe { _web_socket_event_commit(event_buffer.flush_read_count()) }
+                        }
+
+                        send_event(id, WsEvent::Message(data.into_boxed_slice()));
+                    }
+                    _ => unreachable!(),
                 }
             }
         });
-        Self { job_tx }
-    }
-
-    async fn new_web_socket(&self, url: impl AsRef<str>) -> u32 {
-        let (result_tx, result_rx) = tokio::sync::oneshot::channel();
-        self.job_tx
-            .send(WsThreadJob::NewWebSocket {
-                url: url.as_ref().to_string(),
-                result: result_tx,
-            })
-            .unwrap();
-        result_rx.await.unwrap()
+        Self {}
     }
 }
-enum WsThreadJob {
-    NewWebSocket {
-        url: String,
-        result: tokio::sync::oneshot::Sender<u32>,
-    },
-    Send {
-        id: u32,
-        data: Box<[u8]>,
-    },
-}
 
-pub async fn connect(url: impl AsRef<str>) -> Result<(WsSender, WsReceiver)> {
-    let ws_thread = WS_THREAD.get_or_init(WsThread::new);
-    let id = ws_thread.new_web_socket(url).await;
+pub async fn connect(url: impl ToString) -> Result<(WsSender, WsReceiver)> {
+    let _ = WS_THREAD.get_or_init(WsThread::new);
+    let id = {
+        let url = url.to_string();
+        tokio::task::spawn_blocking(move || unsafe {
+            _new_web_socket(url.as_ptr(), url.as_bytes().len())
+        })
+        .await
+        .unwrap()
+    };
 
     let (event_tx, mut event_rx) = tokio::sync::mpsc::unbounded_channel();
 
@@ -101,12 +190,7 @@ pub struct WsSender {
 impl WsSender {
     pub fn send(&self, data: &[u8]) {
         let data = data.to_vec().into_boxed_slice();
-        WS_THREAD
-            .get()
-            .unwrap()
-            .job_tx
-            .send(WsThreadJob::Send { id: self.id, data })
-            .unwrap();
+        unsafe { _web_socket_send(self.id, data.as_ptr(), data.len()) };
     }
 }
 pub type WsReceiver = tokio::sync::mpsc::UnboundedReceiver<Box<[u8]>>;
@@ -121,58 +205,9 @@ static WS_EVENT_TX: OnceLock<DashMap<u32, tokio::sync::mpsc::UnboundedSender<WsE
     OnceLock::new();
 
 extern "C" {
-    fn new_web_socket(url_ptr: *const u8, url_len: usize) -> u32;
-    fn web_socket_send(id: u32, data_ptr: *const u8, data_len: usize);
-}
-
-#[no_mangle]
-pub extern "C" fn on_web_socket_open(id: u32) {
-    let _ = WS_EVENT_TX
-        .get()
-        .unwrap()
-        .get(&id)
-        .unwrap()
-        .send(WsEvent::Open);
-}
-
-#[no_mangle]
-pub extern "C" fn on_web_socket_close(id: u32) {
-    let _ = WS_EVENT_TX
-        .get()
-        .unwrap()
-        .get(&id)
-        .unwrap()
-        .send(WsEvent::Close);
-}
-
-static MESSAGE_HEAP: OnceLock<DashMap<MessagePtr, Box<[u8]>>> = OnceLock::new();
-
-#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
-struct MessagePtr {
-    ptr: *mut u8,
-}
-unsafe impl Send for MessagePtr {}
-unsafe impl Sync for MessagePtr {}
-
-#[no_mangle]
-pub extern "C" fn web_socket_message_alloc(data_len: usize) -> *mut u8 {
-    let data = vec![0; data_len].into_boxed_slice();
-    let ptr = data.as_ptr() as *mut u8;
-    assert!(MESSAGE_HEAP
-        .get_or_init(Default::default)
-        .insert(MessagePtr { ptr }, data)
-        .is_none());
-    ptr
-}
-
-#[no_mangle]
-pub extern "C" fn on_web_socket_message(id: u32, data_ptr: *mut u8) {
-    let (_, data) = MESSAGE_HEAP
-        .get_or_init(Default::default)
-        .remove(&MessagePtr { ptr: data_ptr })
-        .unwrap();
-
-    if let Some(tx) = WS_EVENT_TX.get().unwrap().get(&id) {
-        let _ = tx.send(WsEvent::Message(data));
-    }
+    fn _init_web_socket_thread(event_buffer_ptr: *const u8, event_buffer_len: usize);
+    fn _web_socket_event_poll() -> usize;
+    fn _web_socket_event_commit(byte_length: usize);
+    fn _new_web_socket(url_ptr: *const u8, url_len: usize) -> u32;
+    fn _web_socket_send(id: u32, data_ptr: *const u8, data_len: usize);
 }
