b11d85b8b9b8360653236ad31e7ea0e7884fab6a
bigfoodk <satcom1600@hanmail.net>
2024-06-23 06:56:54 +0900

    Start namui with rendering function

--- jigcle/src/lib.rs
+++ jigcle/src/lib.rs
@@ -11,32 +11,27 @@ use namui_prebuilt::simple_rect;
 const BACKGROUND: &str = "bundle:background.jpg";
 
 pub fn main() {
-    namui::start(|| Game)
+    namui::start(render)
 }
 
-pub struct Game;
+fn render(ctx: &RenderCtx) {
+    let wh = screen::size().into_type::<Px>();
+    let background = ctx.image(BACKGROUND);
 
-impl Component for Game {
-    fn render(self, ctx: &RenderCtx) {
-        let Self {} = self;
-        let wh = screen::size().into_type::<Px>();
-        let background = ctx.image(BACKGROUND);
+    ctx.compose(|ctx| {
+        ctx.add(LevelSelect);
+    });
 
-        ctx.compose(|ctx| {
-            ctx.add(LevelSelect);
+    ctx.compose(|ctx| {
+        let Some(Ok(image)) = background.as_ref() else {
+            ctx.add(simple_rect(wh, Color::TRANSPARENT, 0.px(), Color::BLACK));
+            return;
+        };
+        ctx.add(ImageDrawCommand {
+            rect: wh.to_rect(),
+            image: image.clone(),
+            fit: ImageFit::Cover,
+            paint: None,
         });
-
-        ctx.compose(|ctx| {
-            let Some(Ok(image)) = background.as_ref() else {
-                ctx.add(simple_rect(wh, Color::TRANSPARENT, 0.px(), Color::BLACK));
-                return;
-            };
-            ctx.add(ImageDrawCommand {
-                rect: wh.to_rect(),
-                image: image.clone(),
-                fit: ImageFit::Cover,
-                paint: None,
-            });
-        });
-    }
+    });
 }
