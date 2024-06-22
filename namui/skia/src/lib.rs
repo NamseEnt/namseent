8380b7bdeb0837003dcba028a6e13da9ffefdcd3
bigfoodk <satcom1600@hanmail.net>
2024-06-23 07:06:31 +0900

    Expose OrderedFloat

--- namui/skia/src/lib.rs
+++ namui/skia/src/lib.rs
@@ -8,7 +8,7 @@ pub use bounding_box::*;
 use derive_macro::type_derives;
 use namui_type::*;
 pub use native::*;
-use ordered_float::OrderedFloat;
+pub use ordered_float::OrderedFloat;
 pub use render::*;
 pub use traits::*;
 pub use xy_in::*;
