922662a9b1688aa7182b252e041ec8615200adfe
bigfoodk <satcom1600@hanmail.net>
2024-06-23 17:34:09 +0900

    Use into for OrderedFloat conversion

--- jigcle/src/level_select.rs
+++ jigcle/src/level_select.rs
@@ -177,7 +177,7 @@ impl Component for Thumbnail<'_> {
                 fit: ImageFit::Cover,
                 paint: Some(
                     Paint::new(Color::BLACK).set_image_filter(ImageFilter::Blur {
-                        sigma_xy: Xy::single(16.0),
+                        sigma_xy: Xy::single(16.0.into()),
                         tile_mode: Some(TileMode::Mirror),
                         input: None,
                         crop_rect: None,
