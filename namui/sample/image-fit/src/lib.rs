3f361cfdc6398d388767e71ebe3b1d2aa55d09ee
bigfoodk <satcom1600@hanmail.net>
2024-06-24 21:47:00 +0900

    Update image-fit sample

--- namui/sample/image-fit/src/lib.rs
+++ namui/sample/image-fit/src/lib.rs
@@ -1,4 +1,5 @@
 use namui::*;
+use std::ops::Deref;
 
 pub fn main() {
     namui::start(|ctx| {
@@ -12,8 +13,9 @@ impl Component for App {
     fn render(self, ctx: &RenderCtx) {
         let size = namui::screen::size();
         let (image_fit, set_image_fit) = ctx.state(|| ImageFit::Fill);
-        let (image_url, set_image_url) =
-            ctx.state(|| Url::parse("bundle:resources/0.jpg").unwrap());
+        let (image_location, set_image_location) =
+            ctx.state(|| ResourceLocation::bundle("resources/0.jpg"));
+        let image = ctx.image(&image_location);
 
         let image_xy = size.into_type::<Px>().as_xy() / 4.0;
         let image_wh = size.into_type::<Px>() / 2.0;
@@ -28,38 +30,42 @@ impl Component for App {
             16.int_px(),
         ));
 
-        ctx.add(
-            namui::image(ImageParam {
-                rect: Rect::from_xy_wh(image_xy, image_wh),
-                source: ImageSource::Url {
-                    url: image_url.to_string(),
-                },
-                style: ImageStyle {
-                    fit: *image_fit,
-                    paint: None,
-                },
-            })
-            .attach_event(|event| {
-                if let Event::KeyDown { event } = event {
-                    match event.code {
-                        Code::Digit1 => set_image_fit.set(ImageFit::Fill),
-                        Code::Digit2 => set_image_fit.set(ImageFit::Contain),
-                        Code::Digit3 => set_image_fit.set(ImageFit::Cover),
-                        Code::Digit4 => set_image_fit.set(ImageFit::ScaleDown),
-                        Code::Digit5 => set_image_fit.set(ImageFit::None),
-                        Code::KeyR => {
-                            if *image_url == Url::parse("bundle:resources/0.jpg").unwrap() {
-                                set_image_url
-                                    .set(Url::parse("bundle:resources/rotated.jpg").unwrap());
-                            } else {
-                                set_image_url.set(Url::parse("bundle:resources/0.jpg").unwrap());
+        ctx.compose(|ctx| {
+            let Some(Ok(image)) = image.deref() else {
+                return;
+            };
+            ctx.add(
+                namui::image(ImageParam {
+                    rect: Rect::from_xy_wh(image_xy, image_wh),
+                    image: image.clone(),
+                    style: ImageStyle {
+                        fit: *image_fit,
+                        paint: None,
+                    },
+                })
+                .attach_event(|event| {
+                    if let Event::KeyDown { event } = event {
+                        match event.code {
+                            Code::Digit1 => set_image_fit.set(ImageFit::Fill),
+                            Code::Digit2 => set_image_fit.set(ImageFit::Contain),
+                            Code::Digit3 => set_image_fit.set(ImageFit::Cover),
+                            Code::Digit4 => set_image_fit.set(ImageFit::ScaleDown),
+                            Code::Digit5 => set_image_fit.set(ImageFit::None),
+                            Code::KeyR => {
+                                if *image_location == ResourceLocation::bundle("resources/0.jpg") {
+                                    set_image_location
+                                        .set(ResourceLocation::bundle("resources/rotated.jpg"));
+                                } else {
+                                    set_image_location
+                                        .set(ResourceLocation::bundle("resources/0.jpg"));
+                                }
                             }
+                            _ => {}
                         }
-                        _ => {}
                     }
-                }
-            }),
-        );
+                }),
+            );
+        });
 
         ctx.add(namui::rect(RectParam {
             rect: Rect::from_xy_wh(image_xy, image_wh),
