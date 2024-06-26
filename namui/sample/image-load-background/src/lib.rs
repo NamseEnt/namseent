c971d206eaf940db2b8f19c0cdc9e895d677661e
bigfoodk <satcom1600@hanmail.net>
2024-06-26 20:36:57 +0900

    Use ImageRender instead SampleImage

--- namui/sample/image-load-background/src/lib.rs
+++ namui/sample/image-load-background/src/lib.rs
@@ -31,55 +31,28 @@ impl Component for App {
         ctx.compose(|ctx| {
             for x in 0..x_index_length {
                 for y in 0..y_index_length {
-                    ctx.compose(|ctx| {
-                        let Some(resource_location) = image_resource_locations.get(x + y * 6)
-                        else {
-                            return;
-                        };
-                        let key = format!("{}-{}", x, y);
-                        let rect = Rect::Xywh {
-                            x: image_width * x,
-                            y: image_height * y,
-                            width: image_width,
-                            height: image_height,
-                        };
-                        ctx.add_with_key(
-                            key,
-                            SampleImage {
-                                resource_location,
-                                rect,
-                            },
-                        );
-                    });
+                    let Some(resource_location) = image_resource_locations.get(x + y * 6).cloned()
+                    else {
+                        continue;
+                    };
+                    let key = format!("{}-{}", x, y);
+                    let rect = Rect::Xywh {
+                        x: image_width * x,
+                        y: image_height * y,
+                        width: image_width,
+                        height: image_height,
+                    };
+                    ctx.add_with_key(
+                        key,
+                        ImageRender {
+                            rect,
+                            source: ImageSource::ResourceLocation { resource_location },
+                            fit: ImageFit::Contain,
+                            paint: None,
+                        },
+                    );
                 }
             }
-        });
-    }
-}
-
-struct SampleImage<'a> {
-    resource_location: &'a ResourceLocation,
-    rect: Rect<Px>,
-}
-impl Component for SampleImage<'_> {
-    fn render(self, ctx: &RenderCtx) {
-        let Self {
-            resource_location,
-            rect,
-        } = self;
-        let image = ctx.image(resource_location);
-        ctx.compose(|ctx| {
-            let Some(Ok(image)) = image.as_ref() else {
-                return;
-            };
-            ctx.add(namui::image(ImageParam {
-                rect,
-                image: image.clone(),
-                style: ImageStyle {
-                    fit: ImageFit::Contain,
-                    paint: None,
-                },
-            }));
         });
     }
 }
