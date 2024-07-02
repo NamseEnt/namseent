3fd48ce539a7e80c3cba700ae890880bb55d4d8b
namse <skatpgusskat@naver.com>
2024-07-02 06:44:36 +0000

    Rename to kv_store

--- namui/namui/src/namui/system/file/kv_store/posix.rs
+++ namui/namui/src/namui/system/file/kv_store/posix.rs
@@ -6,7 +6,7 @@ use std::path::PathBuf;
 pub async fn init() -> InitResult {
     sqlite(|conn| {
         conn.execute(
-            "CREATE TABLE IF NOT EXISTS local_storage (key TEXT PRIMARY KEY, value BLOB)",
+            "CREATE TABLE IF NOT EXISTS kv_store (key TEXT PRIMARY KEY, value BLOB)",
             [],
         )?;
         anyhow::Ok(())
@@ -20,7 +20,7 @@ pub fn get(key: impl AsRef<str>) -> Result<Option<Vec<u8>>> {
     sqlite(|conn| {
         Ok(conn
             .query_row(
-                "SELECT value FROM local_storage WHERE key = ?",
+                "SELECT value FROM kv_store WHERE key = ?",
                 [key],
                 |row| row.get::<usize, Vec<u8>>(0),
             )
@@ -32,7 +32,7 @@ pub fn set(key: impl AsRef<str>, content: &[u8]) -> Result<()> {
     let key = key.as_ref();
     sqlite(|conn| {
         conn.execute(
-            "INSERT OR REPLACE INTO local_storage (key, value) VALUES (?, ?)",
+            "INSERT OR REPLACE INTO kv_store (key, value) VALUES (?, ?)",
             (key, content),
         )?;
         Ok(())
@@ -42,20 +42,20 @@ pub fn set(key: impl AsRef<str>, content: &[u8]) -> Result<()> {
 pub fn delete(key: impl AsRef<str>) -> Result<()> {
     let key = key.as_ref();
     sqlite(|conn| {
-        conn.execute("DELETE FROM local_storage WHERE key = ?", [key])?;
+        conn.execute("DELETE FROM kv_store WHERE key = ?", [key])?;
         Ok(())
     })
 }
 
 fn sqlite<T>(func: impl FnOnce(&Connection) -> T) -> T {
     thread_local! {
-        static SQLITE: Connection = Connection::open(local_storage_sqlite_path().unwrap()).unwrap();
+        static SQLITE: Connection = Connection::open(kv_store_sqlite_path().unwrap()).unwrap();
     }
 
     SQLITE.with(|sqlite| func(sqlite))
 }
 
-fn local_storage_sqlite_path() -> Result<PathBuf> {
+fn kv_store_sqlite_path() -> Result<PathBuf> {
     Ok(std::env::current_exe()?
         .parent()
         .ok_or_else(|| anyhow!("No parent of current_exe"))?
