0a24bfa2e1436a3ab507f10e52aea8eecb7065c5
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2024-07-19 22:52:31 +0900

    Support ToOnwed for Dependencies

--- namui/namui-hooks/src/render_ctx/dependencies.rs
+++ namui/namui-hooks/src/render_ctx/dependencies.rs
@@ -1,13 +1,25 @@
 use crate::*;
 
-pub trait Dependencies<T: 'static> {
-    fn cloned(&self) -> T;
+pub trait Dependencies<T> {
+    fn cloned(self) -> T;
 }
-impl Dependencies<()> for () {
-    fn cloned(&self) {}
+
+impl<T, U> Dependencies<U> for &T
+where
+    T: ToOwned<Owned = U> + ?Sized,
+    U: 'static,
+{
+    fn cloned(self) -> U {
+        self.to_owned()
+    }
 }
-impl<T: 'static + Send + Clone, R: std::borrow::Borrow<T>> Dependencies<T> for Sig<'_, T, R> {
-    fn cloned(&self) -> T {
+
+impl<T, R> Dependencies<T> for Sig<'_, T, R>
+where
+    T: 'static + Send + Clone,
+    R: std::borrow::Borrow<T>,
+{
+    fn cloned(self) -> T {
         self.as_ref().clone()
     }
 }
@@ -22,12 +34,12 @@ macro_rules! dependencies_impl {
     ) => {
         $(
             impl<
-                $($T: 'static + Send + Clone,)*
-                $($R: std::borrow::Borrow<$T>,)*
-            > Dependencies<($($T,)*)> for ($(Sig<'_, $T, $R>,)*)
+                $($T: Dependencies<$R>,)*
+                $($R: 'static,)*
+            > Dependencies<($($R,)*)> for ($($T,)*)
             {
-                fn cloned(&self) -> ($($T,)*) {
-                    ($(self.$i.as_ref().clone(),)*)
+                fn cloned(self) -> ($($R,)*) {
+                    ($(self.$i.cloned(),)*)
                 }
             }
         )*
@@ -44,3 +56,21 @@ dependencies_impl!(
     (T0, 0, R0, T1, 1, R1, T2, 2, R2, T3, 3, R3, T4, 4, R4, T5, 5, R5, T6, 6, R6),
     (T0, 0, R0, T1, 1, R1, T2, 2, R2, T3, 3, R3, T4, 4, R4, T5, 5, R5, T6, 6, R6, T7, 7, R7),
 );
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn str_dependencies_should_be_string() {
+        let str_ref = "hello";
+        let cloned: String = str_ref.cloned();
+    }
+
+    #[test]
+    fn sig_dependencies_should_be_inner() {
+        let world = World::new();
+        let sig = Sig::new(1, SigId::new(), &world);
+        let cloned: i32 = sig.cloned();
+    }
+}
