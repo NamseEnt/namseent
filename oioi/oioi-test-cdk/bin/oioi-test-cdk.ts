1b177d5e16cb588eff93d177caa0a95e82710c81
namse <skatpgusskat@naver.com>
2024-08-02 11:16:59 +0000

    Log stack name

--- oioi/oioi-test-cdk/bin/oioi-test-cdk.ts
+++ oioi/oioi-test-cdk/bin/oioi-test-cdk.ts
@@ -5,6 +5,8 @@ import { OioiTestCdkStack } from "../lib/oioi-test-cdk-stack";
 
 const app = new cdk.App();
 
+console.log(`STACK NAME: ${process.env.STACK_NAME}`);
+
 new OioiTestCdkStack(app, process.env.STACK_NAME!!, {
     stackName: process.env.STACK_NAME,
 });
