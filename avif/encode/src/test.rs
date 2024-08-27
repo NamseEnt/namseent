c8a20573b09526edcf623f0fed66157c0e697d40
namse <skatpgusskat@naver.com>
2024-08-27 14:24:16 +0000

    Test r,rgb,rgba

--- avif/encode/src/test.rs
+++ avif/encode/src/test.rs
@@ -1,7 +1,7 @@
 use crate::Encoder;
 
 #[test]
-fn lossless_test() {
+fn lossless_rgb_test() {
     let rgb = vec![0, 0, 0, 255, 255, 255];
 
     let encoder = Encoder::new(true).unwrap();
@@ -10,10 +10,47 @@ fn lossless_test() {
 }
 
 #[test]
-fn lossy_test() {
+fn lossy_rgb_test() {
     let rgb = vec![0, 0, 0, 255, 255, 255];
 
     let encoder = Encoder::new(false).unwrap();
 
     let _encoded = encoder.encode(2, 1, &rgb).unwrap();
+}
+
+#[test]
+fn lossless_r8_test() {
+    let r8 = vec![0, 255];
+
+    let encoder = Encoder::new(true).unwrap();
+
+    let _encoded = encoder.encode(2, 1, &r8).unwrap();
+}
+
+#[test]
+fn lossy_r8_test() {
+    let r8 = vec![0, 255];
+
+    let encoder = Encoder::new(false).unwrap();
+
+    let _encoded = encoder.encode(2, 1, &r8).unwrap();
+}
+
+#[test]
+fn lossless_rgba_test() {
+    let rgba = vec![0, 0, 0, 255, 255, 255, 255, 255];
+
+    let encoder = Encoder::new(true).unwrap();
+
+    let _encoded = encoder.encode(2, 1, &rgba).unwrap();
+}
+
+#[test]
+
+fn lossy_rgba_test() {
+    let rgba = vec![0, 0, 0, 255, 255, 255, 255, 255];
+
+    let encoder = Encoder::new(false).unwrap();
+
+    let _encoded = encoder.encode(2, 1, &rgba).unwrap();
 }
