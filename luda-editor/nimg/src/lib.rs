90477e3f08d2de28f625fb3ce7ab63408033f64f
namse <skatpgusskat@naver.com>
2024-08-29 15:14:41 +0000

    nimg: Jpg and Zstd

--- luda-editor/nimg/src/lib.rs
+++ luda-editor/nimg/src/lib.rs
@@ -3,6 +3,9 @@ mod test;
 
 use anyhow::Result;
 
+const ZSTD_ENCODE_LEVEL: i32 = 9;
+const JPEG_QUALITY: u8 = 80;
+
 #[derive(Debug, Clone)]
 
 pub struct Nimg {
@@ -12,22 +15,44 @@ pub struct Nimg {
 
 #[derive(Debug, Clone, Copy)]
 pub enum ColorType {
-    RgbA8888,
+    Rgba8888,
     A8,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
+#[allow(clippy::enum_variant_names)]
 enum Encoded {
-    Rgb8A8 { rgb: Vec<u8>, a: Vec<u8> },
-    A8 { a: Vec<u8> },
+    Rgba8888Zstd { rgba_zstd: Vec<u8> },
+    Rgb888JpegA8Zstd { rgb_jpeg: Vec<u8>, a_zstd: Vec<u8> },
+    A8Zstd { a_zstd: Vec<u8> },
 }
 
-/// alpha channel ignored
-pub fn encode_rgba8(width: usize, height: usize, pixels: &[u8]) -> Result<Nimg> {
-    let rgb = {
+impl std::fmt::Debug for Encoded {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Encoded::Rgba8888Zstd { rgba_zstd } => f
+                .debug_struct("Rgba8888Zstd")
+                .field("rgba", &rgba_zstd.len())
+                .finish(),
+            Encoded::Rgb888JpegA8Zstd { rgb_jpeg, a_zstd } => f
+                .debug_struct("Rgb888JpegA8Zstd")
+                .field("rgb", &rgb_jpeg.len())
+                .field("a", &a_zstd.len())
+                .finish(),
+            Encoded::A8Zstd { a_zstd } => {
+                f.debug_struct("A8Zstd").field("a", &a_zstd.len()).finish()
+            }
+        }
+    }
+}
+
+pub fn encode_rgba8888(width: usize, height: usize, pixels: &[u8]) -> Result<Nimg> {
+    assert_eq!(pixels.len(), width * height * 4);
+
+    let rgb_jpeg = {
         let mut output = Vec::with_capacity(width * height * 3);
 
-        jpeg_encoder::Encoder::new(&mut output, 80).encode(
+        jpeg_encoder::Encoder::new(&mut output, JPEG_QUALITY).encode(
             pixels,
             width as u16,
             height as u16,
@@ -37,82 +62,86 @@ pub fn encode_rgba8(width: usize, height: usize, pixels: &[u8]) -> Result<Nimg> 
         output
     };
 
-    let a = {
+    let a_zstd = {
         let a_pixels = pixels
             .chunks_exact(4)
             .map(|rgba| rgba[3])
             .collect::<Vec<_>>();
 
-        encode_a8_impl(width, height, &a_pixels)?
+        zstd::encode_all(a_pixels.as_slice(), ZSTD_ENCODE_LEVEL)?
     };
 
-    Ok(Nimg {
-        color_type: ColorType::RgbA8888,
-        encoded: Encoded::Rgb8A8 { rgb, a },
-    })
+    let rgba_zstd = zstd::encode_all(pixels, ZSTD_ENCODE_LEVEL)?;
+
+    if rgb_jpeg.len() + a_zstd.len() < rgba_zstd.len() {
+        Ok(Nimg {
+            color_type: ColorType::Rgba8888,
+            encoded: Encoded::Rgb888JpegA8Zstd { rgb_jpeg, a_zstd },
+        })
+    } else {
+        Ok(Nimg {
+            color_type: ColorType::Rgba8888,
+            encoded: Encoded::Rgba8888Zstd { rgba_zstd },
+        })
+    }
 }
 
-pub fn encode_a8(width: usize, height: usize, data: &[u8]) -> Result<Nimg> {
+pub fn encode_a8(data: &[u8]) -> Result<Nimg> {
     Ok(Nimg {
         color_type: ColorType::A8,
-        encoded: Encoded::A8 {
-            a: encode_a8_impl(width, height, data)?,
+        encoded: Encoded::A8Zstd {
+            a_zstd: zstd::encode_all(data, ZSTD_ENCODE_LEVEL)?,
         },
     })
-}
-
-fn encode_a8_impl(width: usize, height: usize, data: &[u8]) -> Result<Vec<u8>> {
-    let mut encoder = jpegxl_rs::encoder_builder()
-        .color_encoding(jpegxl_rs::encode::ColorEncoding::SrgbLuma)
-        .has_alpha(false)
-        .lossless(true)
-        .uses_original_profile(true)
-        .speed(jpegxl_rs::encode::EncoderSpeed::Lightning)
-        .decoding_speed(4)
-        .build()?;
-
-    let frame = jpegxl_rs::encode::EncoderFrame::new(data).num_channels(1);
-
-    Ok(encoder
-        .encode_frame::<u8, u8>(&frame, width as _, height as _)?
-        .data)
 }
 
 impl Nimg {
     pub fn decode(&self) -> Result<Vec<u8>> {
         let Self { encoded, .. } = self;
         match encoded {
-            Encoded::Rgb8A8 { rgb, a } => {
-                let a_pixels = decode_a8(a)?;
-
+            Encoded::Rgba8888Zstd { rgba_zstd } => Ok(zstd::decode_all(rgba_zstd.as_slice())?),
+            Encoded::Rgb888JpegA8Zstd { rgb_jpeg, a_zstd } => {
                 let mut rgba = {
-                    use zune_jpeg::zune_core::*;
-                    let mut decoder = zune_jpeg::JpegDecoder::new(rgb);
+                    let mut decoder = zune_jpeg::JpegDecoder::new(rgb_jpeg.as_slice());
+
                     decoder.set_options(
-                        options::DecoderOptions::default()
-                            .jpeg_set_out_colorspace(colorspace::ColorSpace::RGBA),
+                        zune_jpeg::zune_core::options::DecoderOptions::default()
+                            .jpeg_set_out_colorspace(
+                                zune_jpeg::zune_core::colorspace::ColorSpace::RGBA,
+                            ),
                     );
 
                     decoder.decode()?
                 };
+                let a = zstd::decode_all(a_zstd.as_slice())?;
 
                 rgba.chunks_exact_mut(4)
-                    .zip(a_pixels.iter())
+                    .zip(a.iter())
                     .for_each(|(rgba, a)| rgba[3] = *a);
 
                 Ok(rgba)
             }
-            Encoded::A8 { a } => Ok(decode_a8(a)?),
+            Encoded::A8Zstd { a_zstd } => Ok(zstd::decode_all(a_zstd.as_slice())?),
         }
     }
-}
 
-fn decode_a8(data: &[u8]) -> Result<Vec<u8>> {
-    let (_, pixels) = jpegxl_rs::decoder_builder().build()?.decode(data)?;
+    pub fn image_encoded_byte_size(&self) -> usize {
+        let Self { encoded, .. } = self;
+        match encoded {
+            Encoded::Rgba8888Zstd { rgba_zstd } => rgba_zstd.len(),
+            Encoded::Rgb888JpegA8Zstd { rgb_jpeg, a_zstd } => rgb_jpeg.len() + a_zstd.len(),
+            Encoded::A8Zstd { a_zstd } => a_zstd.len(),
+        }
+    }
 
-    if let jpegxl_rs::decode::Pixels::Uint8(pixels) = pixels {
-        Ok(pixels)
-    } else {
-        unreachable!()
+    pub fn image_encoded_bytes(&self) -> Vec<Vec<u8>> {
+        let Self { encoded, .. } = self;
+        match encoded {
+            Encoded::Rgba8888Zstd { rgba_zstd } => vec![rgba_zstd.clone()],
+            Encoded::Rgb888JpegA8Zstd { rgb_jpeg, a_zstd } => {
+                vec![rgb_jpeg.clone(), a_zstd.clone()]
+            }
+            Encoded::A8Zstd { a_zstd } => vec![a_zstd.clone()],
+        }
     }
 }
