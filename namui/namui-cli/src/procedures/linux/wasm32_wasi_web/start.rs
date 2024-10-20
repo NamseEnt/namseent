e65efb9f2d8ea9af8f20684b596036848ece2542
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2024-10-20 16:00:13 +0900

    Build wasm32-wasi-web (#977)

--- namui/namui-cli/src/procedures/linux/wasm32_wasi_web/start.rs
+++ namui/namui-cli/src/procedures/linux/wasm32_wasi_web/start.rs
@@ -1,12 +1,10 @@
+use super::vite_config::{update_vite_config, ViteConfig};
 use crate::cli::Target;
 use crate::*;
 use services::build_status_service::{BuildStatusCategory, BuildStatusService};
-use services::bundle::NamuiBundleManifest;
 use services::runtime_project::{wasm::generate_runtime_project, GenerateRuntimeProjectArgs};
 use services::rust_build_service::{self, BuildOption};
 use services::rust_project_watch_service::RustProjectWatchService;
-use std::path::Path;
-use tokio::fs::{create_dir_all, remove_dir_all};
 use tokio::process::Child;
 use util::get_cli_root_path;
 
@@ -48,7 +46,7 @@ pub async fn start(manifest_path: impl AsRef<std::path::Path>, release: bool) ->
         .await;
 
     update_vite_config(&vite_config).await?;
-    let _web_code = start_web_code().await?;
+    let _web_builder = start_web_code().await?;
 
     build_status_service
         .build_finished(BuildStatusCategory::WebRuntime, vec![], vec![])
@@ -89,62 +87,4 @@ async fn start_web_code() -> Result<Child> {
         .spawn()?;
 
     Ok(process)
-}
-
-struct ViteConfig<'a> {
-    project_root_path: &'a Path,
-    release: bool,
-}
-async fn update_vite_config(config: &ViteConfig<'_>) -> Result<()> {
-    let bundle_manifest = NamuiBundleManifest::parse(config.project_root_path)?;
-
-    let target_project_path = config.project_root_path.join(format!(
-        "target/namui/target/wasm32-wasip1-threads/{}",
-        if config.release { "release" } else { "debug" }
-    ));
-    let namui_runtime_wasm_path = target_project_path.join("namui-runtime-wasm.wasm");
-    let bundle_sqlite_path = target_project_path.join("bundle.sqlite");
-    bundle_manifest.bundle_to_sqlite(&bundle_sqlite_path)?;
-
-    let generated_dist = get_cli_root_path().join("webCode/src/__generated__");
-
-    let _ = remove_dir_all(&generated_dist).await;
-    create_dir_all(&generated_dist).await?;
-
-    tokio::fs::write(
-        get_cli_root_path().join("webCode/vite.config.js"),
-        format!(
-            r#"
-import {{ defineConfig }} from "vite";
-
-export default defineConfig({{
-    clearScreen: false,
-    server: {{
-        headers: {{
-            "Cross-Origin-Resource-Policy": "same-origin",
-            "Cross-Origin-Embedder-Policy": "require-corp",
-            "Cross-Origin-Opener-Policy": "same-origin",
-            "Referrer-Policy": "no-referrer-when-downgrade",
-        }},
-        allow: [
-            "{namui_runtime_wasm}",
-            "{cli_root}/",
-        ],
-    }},
-    resolve: {{
-        alias: {{
-            "namui-runtime-wasm.wasm?url": "{namui_runtime_wasm}?url",
-            "bundle.sqlite?url": "{bundle_sqlite}?url",
-        }}
-    }},
-}});
-"#,
-            namui_runtime_wasm = namui_runtime_wasm_path.to_string_lossy(),
-            cli_root = get_cli_root_path().to_string_lossy(),
-            bundle_sqlite = bundle_sqlite_path.to_string_lossy(),
-        ),
-    )
-    .await?;
-
-    Ok(())
 }
