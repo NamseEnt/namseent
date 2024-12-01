32b8bf7817f0d069dbab5f8ebee7d8805448ba01
bigfoodk <satcom1600@hanmail.net>
2024-12-02 07:58:53 +0900

    Use speaker CRD api

--- luda-editor/new-client/src/episode_editor/speaker_selector.rs
+++ luda-editor/new-client/src/episode_editor/speaker_selector.rs
@@ -1,86 +1,38 @@
 use crate::*;
 use list_view::AutoListView;
-use luda_rpc::Scene;
+use luda_rpc::Speaker;
 use namui::*;
 use namui_prebuilt::*;
-use std::collections::BTreeMap;
 
 pub struct SpeakerSelector<'a> {
     pub wh: Wh<Px>,
-    pub scene: &'a Scene,
-    pub project_id: u128,
-    pub episode_id: u128,
     pub select_speaker: &'a dyn Fn(u128),
+    pub add_speaker: &'a dyn Fn(String),
+    pub save_speaker_slots: &'a dyn Fn(Vec<u128>),
+    pub speakers: Sig<'a, Vec<Speaker>>,
+    pub speaker_slots: Sig<'a, Vec<u128>>,
 }
 
 impl Component for SpeakerSelector<'_> {
     fn render(self, ctx: &RenderCtx) {
         let Self {
             wh,
-            scene,
-            project_id,
-            episode_id,
             select_speaker,
+            add_speaker,
+            save_speaker_slots,
+            speakers,
+            speaker_slots,
         } = self;
-        let (speaker_ids, set_speaker_ids) = ctx.state::<Option<Vec<u128>>>(|| None);
-        let (speakers, set_speakers) = ctx.state::<Option<BTreeMap<u128, Option<String>>>>(|| None);
-        let (error_msg, set_error_msg) = ctx.state::<Option<String>>(|| None);
         let (show_edit_speaker_modal, set_show_edit_speaker_modal) = ctx.state(|| false);
 
-        ctx.async_effect(
-            "load speaker ids",
-            &episode_id,
-            move |episode_id| async move {
-                use crate::rpc::episode_editor::load_speaker_slots::*;
-                match server_connection()
-                    .load_speaker_slots(RefRequest { episode_id })
-                    .await
-                {
-                    Ok(response) => {
-                        set_speaker_ids.set(Some(response.speaker_ids));
-                    }
-                    Err(err) => {
-                        set_error_msg.set(Some(err.to_string()));
-                    }
-                }
-            },
-        );
+        let speakers_in_slot = ctx.memo(|| {
+            speakers
+                .iter()
+                .filter(|x| speaker_slots.contains(&x.id))
+                .cloned()
+                .collect::<Vec<_>>()
+        });
 
-        ctx.async_effect(
-            "load speaker names",
-            (speaker_ids, &project_id),
-            move |(speaker_ids, project_id)| async move {
-                set_speakers.set(None);
-                let Some(speaker_ids) = speaker_ids else {
-                    return;
-                };
-                use crate::rpc::episode_editor::get_speaker_names::*;
-                match server_connection()
-                    .get_speaker_names(RefRequest {
-                        language_code: "kor",
-                        project_id,
-                        speaker_ids: &speaker_ids,
-                    })
-                    .await
-                {
-                    Ok(response) => {
-                        set_speakers.set(Some(
-                            speaker_ids
-                                .into_iter()
-                                .zip(response.speaker_names)
-                                .collect(),
-                        ));
-                    }
-                    Err(err) => {
-                        set_error_msg.set(Some(err.to_string()));
-                    }
-                }
-            },
-        );
-
-        let add_speaker = |name: &str| {
-            todo!();
-        };
         let close_modal = move || {
             set_show_edit_speaker_modal.set(false);
         };
@@ -90,8 +42,10 @@ impl Component for SpeakerSelector<'_> {
                 return;
             }
             ctx.add(EditSpeakerModal {
-                speakers: speakers.as_ref(),
-                add_speaker: &add_speaker,
+                speakers,
+                speaker_slots,
+                add_speaker,
+                save_speaker_slots,
                 close: &close_modal,
             });
         });
@@ -100,26 +54,23 @@ impl Component for SpeakerSelector<'_> {
             let items = {
                 let mut items = vec![];
 
-                if let Some(speakers) = speakers.as_ref() {
-                    for (speaker_id, name) in speakers {
-                        items.push(table::fixed(16.px(), |_wh, _ctx| {}));
-                        items.push(table::ratio(1, |wh, ctx| {
-                            ctx.add(button::TextButton {
-                                rect: wh.to_rect(),
-                                text: name.as_ref().unwrap_or(&"N/A".to_string()),
-                                text_color: Color::WHITE,
-                                stroke_color: Color::WHITE,
-                                stroke_width: 1.px(),
-                                fill_color: Color::BLACK,
-                                mouse_buttons: vec![MouseButton::Left],
-                                on_mouse_up_in: |_event| {
-                                    select_speaker(*speaker_id);
-                                },
-                            });
-                        }));
-                    }
+                for speaker in speakers_in_slot.iter() {
+                    items.push(table::fixed(16.px(), |_wh, _ctx| {}));
+                    items.push(table::ratio(1, |wh, ctx| {
+                        ctx.add(button::TextButton {
+                            rect: wh.to_rect(),
+                            text: speaker.name_l10n.get("kor").unwrap_or(&"N/A".to_string()),
+                            text_color: Color::WHITE,
+                            stroke_color: Color::WHITE,
+                            stroke_width: 1.px(),
+                            fill_color: Color::BLACK,
+                            mouse_buttons: vec![MouseButton::Left],
+                            on_mouse_up_in: |_event| {
+                                select_speaker(speaker.id);
+                            },
+                        });
+                    }));
                 }
-
                 items.push(table::ratio(1, |_wh, _ctx| {}));
                 // TODO: Fix table::fit
                 items.push(table::fixed(96.px(), |wh, ctx| {
@@ -145,18 +96,36 @@ impl Component for SpeakerSelector<'_> {
 }
 
 struct EditSpeakerModal<'a> {
-    speakers: &'a Option<BTreeMap<u128, Option<String>>>,
-    add_speaker: &'a dyn Fn(&str),
+    speakers: Sig<'a, Vec<Speaker>>,
+    speaker_slots: Sig<'a, Vec<u128>>,
+    add_speaker: &'a dyn Fn(String),
+    save_speaker_slots: &'a dyn Fn(Vec<u128>),
     close: &'a dyn Fn(),
 }
 impl Component for EditSpeakerModal<'_> {
     fn render(self, ctx: &RenderCtx) {
         let Self {
             speakers,
+            speaker_slots,
             add_speaker,
+            save_speaker_slots,
             close,
         } = self;
         const MODAL_WH: Wh<Px> = Wh::new(px(512.0), px(480.0));
+
+        let splitted_speakers = ctx.memo(|| {
+            let mut left = vec![];
+            let mut right = vec![];
+            for speaker in speakers.iter().cloned() {
+                if speaker_slots.contains(&speaker.id) {
+                    right.push(speaker);
+                } else {
+                    left.push(speaker);
+                }
+            }
+            (left, right)
+        });
+        let (left_speakers, right_speakers) = splitted_speakers.as_ref();
 
         let screen_wh = screen::size().map(|x| x.into_px());
         let (new_speaker_name, set_new_speaker_name) = ctx.state(|| "".to_string());
@@ -186,7 +155,7 @@ impl Component for EditSpeakerModal<'_> {
                                     ratio(1, |wh, ctx| {
                                         ctx.add(SpeakerList {
                                             wh,
-                                            speakers,
+                                            speakers: left_speakers,
                                             select_speaker: &select_left_speaker,
                                             selected_speaker_id: left_selected_speaker_id.as_ref(),
                                         });
@@ -207,7 +176,18 @@ impl Component for EditSpeakerModal<'_> {
                                                         fill_color: Color::BLUE,
                                                         mouse_buttons: vec![MouseButton::Left],
                                                         on_mouse_up_in: |_event| {
-                                                            todo!("");
+                                                            let Some(speaker_id) =
+                                                                left_selected_speaker_id.as_ref()
+                                                            else {
+                                                                return;
+                                                            };
+                                                            if speaker_slots.contains(speaker_id) {
+                                                                return;
+                                                            }
+                                                            let mut speaker_slots =
+                                                                speaker_slots.clone_inner();
+                                                            speaker_slots.push(*speaker_id);
+                                                            save_speaker_slots(speaker_slots);
                                                         },
                                                     });
                                                 }),
@@ -222,7 +202,20 @@ impl Component for EditSpeakerModal<'_> {
                                                         fill_color: Color::BLUE,
                                                         mouse_buttons: vec![MouseButton::Left],
                                                         on_mouse_up_in: |_event| {
-                                                            todo!("");
+                                                            let Some(speaker_id) =
+                                                                right_selected_speaker_id.as_ref()
+                                                            else {
+                                                                return;
+                                                            };
+                                                            if !speaker_slots.contains(speaker_id) {
+                                                                return;
+                                                            }
+                                                            let speaker_slots = speaker_slots
+                                                                .iter()
+                                                                .filter(|x| x != &speaker_id)
+                                                                .cloned()
+                                                                .collect();
+                                                            save_speaker_slots(speaker_slots);
                                                         },
                                                     });
                                                 }),
@@ -233,7 +226,7 @@ impl Component for EditSpeakerModal<'_> {
                                     ratio(1, |wh, ctx| {
                                         ctx.add(SpeakerList {
                                             wh,
-                                            speakers,
+                                            speakers: right_speakers,
                                             select_speaker: &select_right_speaker,
                                             selected_speaker_id: right_selected_speaker_id.as_ref(),
                                         });
@@ -294,7 +287,7 @@ impl Component for EditSpeakerModal<'_> {
                                                     toast::negative("이름을 입력해주세요.");
                                                     return;
                                                 }
-                                                add_speaker(new_speaker_name.as_ref());
+                                                add_speaker(new_speaker_name.clone_inner());
                                             },
                                         });
                                     }),
@@ -353,7 +346,7 @@ impl Component for EditSpeakerModal<'_> {
 
 struct SpeakerList<'a> {
     wh: Wh<Px>,
-    speakers: &'a Option<BTreeMap<u128, Option<String>>>,
+    speakers: &'a Vec<Speaker>,
     select_speaker: &'a dyn Fn(u128),
     selected_speaker_id: &'a Option<u128>,
 }
@@ -371,26 +364,23 @@ impl Component for SpeakerList<'_> {
         let item_wh = Wh::new(wh.width - SCROLL_BAR_WIDTH, 36.px());
 
         ctx.compose(|ctx| {
-            let Some(speakers) = speakers else {
-                return;
-            };
             ctx.add(AutoListView {
                 height: wh.height,
                 scroll_bar_width: 4.px(),
                 item_wh,
-                items: speakers.iter().map(|(speaker_id, name)| {
+                items: speakers.iter().map(|speaker| {
                     let is_on = selected_speaker
                         .as_ref()
-                        .map(|x| x == speaker_id)
+                        .map(|x| x == &speaker.id)
                         .unwrap_or_default();
                     (
-                        speaker_id.to_string(),
+                        speaker.id.to_string(),
                         simple_toggle_button(
                             item_wh,
-                            name.clone().unwrap_or_default(),
+                            speaker.name_l10n.get("kor").cloned().unwrap_or_default(),
                             is_on,
                             |_event| {
-                                select_speaker(*speaker_id);
+                                select_speaker(speaker.id);
                             },
                         ),
                     )
