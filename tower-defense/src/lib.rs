7a301f832b7e0099f94cab4c930fa988ca74eb5e
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2025-01-31 18:19:45 +0900

    Set up render stuffs (#1008)

--- tower-defense/src/lib.rs
+++ tower-defense/src/lib.rs
@@ -2,14 +2,19 @@ mod card;
 mod game_state;
 mod hand;
 mod palette;
+mod route;
 mod status;
 mod tower;
-mod route_find;
 
 use hand::Hand;
 use namui::*;
 use namui_prebuilt::simple_rect;
 use status::{Flow, FLOW_ATOM};
+
+type BlockUnit = usize;
+type BlockUnitF32 = f32;
+type MapCoord = Xy<BlockUnit>;
+type MapCoordF32 = Xy<BlockUnitF32>;
 
 pub fn main() {
     namui::start(|ctx| {
@@ -22,6 +27,7 @@ impl Component for Game {
     fn render(self, ctx: &RenderCtx) {
         let screen_wh = screen::size().into_type::<Px>();
         let _ = ctx.init_atom(&FLOW_ATOM, || Flow::SelectingTower);
+        let game_state = game_state::init_game_state(ctx);
 
         ctx.add(Hand { screen_wh });
 
@@ -31,5 +37,7 @@ impl Component for Game {
             0.px(),
             palette::SURFACE_CONTAINER_LOWEST,
         ));
+
+        ctx.add(game_state.as_ref());
     }
 }
---
+++ tower-defense/src/route/mod.rs
@@ -0,0 +1,31 @@
+mod move_on_route;
+mod route_find;
+
+use crate::*;
+pub use move_on_route::*;
+pub use route_find::*;
+use std::sync::Arc;
+
+pub struct Route {
+    map_coords: Vec<MapCoord>,
+}
+
+pub fn calculate_routes(
+    blockers: &[MapCoord],
+    travel_points: &[MapCoord],
+    map_wh: Wh<usize>,
+) -> Option<Arc<Route>> {
+    let mut map_coords = vec![];
+
+    for i in 0..travel_points.len() - 1 {
+        let start_xy = travel_points[i];
+        let end_xy = travel_points[i + 1];
+        let Some(route) = crate::route::find_shortest_route(map_wh, start_xy, end_xy, blockers)
+        else {
+            return None;
+        };
+        map_coords.extend(route);
+    }
+
+    Some(Arc::new(Route { map_coords }))
+}
