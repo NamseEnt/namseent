30ca6d1f139af84b32b8930ed4750b570136cf1c
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2025-02-10 19:56:55 +0900

    Projectile (#1015)

--- tower-defense/src/game_state/monster_spawn.rs
+++ tower-defense/src/game_state/monster_spawn.rs
@@ -53,6 +53,8 @@ pub fn spawn_tick(game_state: &mut GameState, now: Instant) {
     game_state.monsters.push(Monster {
         move_on_route: MoveOnRoute::new(game_state.route.clone(), *velocity),
         kind: *monster_kind,
+        projectile_target_indicator: ProjectileTargetIndicator::new(),
+        hp: monster_kind.max_hp(),
     });
 
     if left_spawn_count.get() == 1 {
--- tower-defense/src/game_state/tick.rs
+++ tower-defense/src/game_state/tick.rs
@@ -26,6 +26,8 @@ impl Component for Ticker {
 fn tick(game_state: &mut GameState, dt: Duration, now: Instant) {
     crate::game_state::spawn_tick(game_state, now);
     move_monsters(game_state, dt);
+    move_projectiles(game_state, dt);
+    shoot_projectiles(game_state, now);
 }
 
 fn move_monsters(game_state: &mut GameState, dt: Duration) {
@@ -37,4 +39,66 @@ fn move_monsters(game_state: &mut GameState, dt: Duration) {
     game_state
         .monsters
         .retain(|monster| !monster.move_on_route.is_finished());
+}
+
+fn move_projectiles(game_state: &mut GameState, dt: Duration) {
+    let GameState {
+        projectiles,
+        monsters,
+        ..
+    } = game_state;
+
+    projectiles.retain_mut(|projectile| {
+        let start_xy = projectile.xy;
+
+        let Some(monster_index) = monsters.iter().enumerate().find_map(|(i, monster)| {
+            if monster.projectile_target_indicator == projectile.target_indicator {
+                Some(i)
+            } else {
+                None
+            }
+        }) else {
+            return false;
+        };
+
+        let monster = &mut monsters[monster_index];
+
+        let monster_xy = monster.move_on_route.xy();
+
+        if (monster_xy - start_xy).length() > projectile.velocity * dt {
+            projectile.move_by(dt, monster_xy);
+            return true;
+        }
+
+        monster.get_damage(projectile.damage);
+
+        if monster.dead() {
+            monsters.swap_remove(monster_index);
+        }
+
+        return false;
+    });
+}
+
+fn shoot_projectiles(game_state: &mut GameState, now: Instant) {
+    let projectiles = game_state
+        .towers
+        .iter_mut()
+        .map(|(tower_xy, tower)| {
+            if tower.in_cooltime(now) {
+                return None;
+            }
+
+            let Some(target) = game_state.monsters.iter().find(|monster| {
+                (monster.move_on_route.xy() - tower_xy.map(|t| t as f32)).length()
+                    < tower.attack_range_radius
+            }) else {
+                return None;
+            };
+
+            Some(tower.shoot(target.projectile_target_indicator, now))
+        })
+        .flatten();
+
+    game_state.projectiles.extend(projectiles);
 }
