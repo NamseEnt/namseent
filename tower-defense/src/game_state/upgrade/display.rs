dcd2c2618b71643fa9e583bbc912314c9f9fb084
bigfoodk <satcom1600@hanmail.net>
2025-03-17 00:54:03 +0900

    Update upgrade name and description

--- tower-defense/src/game_state/upgrade/display.rs
+++ tower-defense/src/game_state/upgrade/display.rs
@@ -22,27 +22,37 @@ impl UpgradeKind {
                 format!("{suit} 공격 속도 배수 증가")
             }
             UpgradeKind::SuitAttackRangePlus { suit, .. } => format!("{suit} 공격 범위 증가"),
-            UpgradeKind::HandAttackDamagePlus { .. } => "족보 공격력 증가".to_string(),
-            UpgradeKind::HandAttackDamageMultiply { .. } => "족보 공격력 배수 증가".to_string(),
-            UpgradeKind::HandAttackSpeedPlus { .. } => "족보 공격 속도 증가".to_string(),
-            UpgradeKind::HandAttackSpeedMultiply { .. } => "족보 공격 속도 배수 증가".to_string(),
-            UpgradeKind::HandAttackRangePlus { .. } => "족보 공격 범위 증가".to_string(),
+            UpgradeKind::HandAttackDamagePlus { tower_kind, .. } => {
+                format!("{tower_kind} 족보 공격력 증가")
+            }
+            UpgradeKind::HandAttackDamageMultiply { tower_kind, .. } => {
+                format!("{tower_kind} 족보 공격력 배수 증가")
+            }
+            UpgradeKind::HandAttackSpeedPlus { tower_kind, .. } => {
+                format!("{tower_kind} 족보 공격 속도 증가")
+            }
+            UpgradeKind::HandAttackSpeedMultiply { tower_kind, .. } => {
+                format!("{tower_kind} 족보 공격 속도 배수 증가")
+            }
+            UpgradeKind::HandAttackRangePlus { tower_kind, .. } => {
+                format!("{tower_kind} 족보 공격 범위 증가")
+            }
             UpgradeKind::ShopSlotExpansion => "상점 슬롯 확장".to_string(),
             UpgradeKind::QuestSlotExpansion => "퀘스트 슬롯 확장".to_string(),
             UpgradeKind::QuestBoardExpansion => "퀘스트 게시판 확장".to_string(),
             UpgradeKind::RerollCountPlus => "리롤 횟수 증가".to_string(),
-            UpgradeKind::LowCardTowerDamagePlus { .. } => "적은 카드 타워 공격력 증가".to_string(),
+            UpgradeKind::LowCardTowerDamagePlus { .. } => "3장 이하 타워 공격력 증가".to_string(),
             UpgradeKind::LowCardTowerDamageMultiply { .. } => {
-                "적은 카드 타워 공격력 배수 증가".to_string()
+                "3장 이하 타워 공격력 배수 증가".to_string()
             }
             UpgradeKind::LowCardTowerAttackSpeedPlus { .. } => {
-                "적은 카드 타워 공격 속도 증가".to_string()
+                "3장 이하 타워 공격 속도 증가".to_string()
             }
             UpgradeKind::LowCardTowerAttackSpeedMultiply { .. } => {
-                "적은 카드 타워 공격 속도 배수 증가".to_string()
+                "3장 이하 타워 공격 속도 배수 증가".to_string()
             }
             UpgradeKind::LowCardTowerAttackRangePlus { .. } => {
-                "적은 카드 타워 공격 범위 증가".to_string()
+                "3장 이하 타워 공격 범위 증가".to_string()
             }
             UpgradeKind::ShopItemPriceMinus => "상점 아이템 가격 감소".to_string(),
             UpgradeKind::ShopRefreshPlus => "상점 새로고침 횟수 증가".to_string(),
@@ -62,37 +72,66 @@ impl UpgradeKind {
             UpgradeKind::NoRerollTowerAttackRangePlus { .. } => {
                 "리롤 안하면 공격 범위 증가".to_string()
             }
-            UpgradeKind::EvenOddTowerAttackDamagePlus { .. } => {
-                "짝수/홀수 타워 공격력 증가".to_string()
+            UpgradeKind::EvenOddTowerAttackDamagePlus { even, .. } => {
+                format!("{} 타워 공격력 증가", if *even { "짝수" } else { "홀수" })
             }
-            UpgradeKind::EvenOddTowerAttackDamageMultiply { .. } => {
-                "짝수/홀수 타워 공격력 배수 증가".to_string()
+            UpgradeKind::EvenOddTowerAttackDamageMultiply { even, .. } => {
+                format!(
+                    "{} 타워 공격력 배수 증가",
+                    if *even { "짝수" } else { "홀수" }
+                )
             }
-            UpgradeKind::EvenOddTowerAttackSpeedPlus { .. } => {
-                "짝수/홀수 타워 공격 속도 증가".to_string()
+            UpgradeKind::EvenOddTowerAttackSpeedPlus { even, .. } => {
+                format!(
+                    "{} 타워 공격 속도 증가",
+                    if *even { "짝수" } else { "홀수" }
+                )
             }
-            UpgradeKind::EvenOddTowerAttackSpeedMultiply { .. } => {
-                "짝수/홀수 타워 공격 속도 배수 증가".to_string()
+            UpgradeKind::EvenOddTowerAttackSpeedMultiply { even, .. } => {
+                format!(
+                    "{} 타워 공격 속도 배수 증가",
+                    if *even { "짝수" } else { "홀수" }
+                )
             }
-            UpgradeKind::EvenOddTowerAttackRangePlus { .. } => {
-                "짝수/홀수 타워 공격 범위 증가".to_string()
+            UpgradeKind::EvenOddTowerAttackRangePlus { even, .. } => {
+                format!(
+                    "{} 타워 공격 범위 증가",
+                    if *even { "짝수" } else { "홀수" }
+                )
             }
-            UpgradeKind::FaceNumberCardTowerAttackDamagePlus { .. } => {
-                "그림/숫자 카드 타워 공격력 증가".to_string()
+            UpgradeKind::FaceNumberCardTowerAttackDamagePlus { face, .. } => {
+                format!(
+                    "{} 카드 타워 공격력 증가",
+                    if *face { "그림" } else { "숫자" }
+                )
             }
-            UpgradeKind::FaceNumberCardTowerAttackDamageMultiply { .. } => {
-                "그림/숫자 카드 타워 공격력 배수 증가".to_string()
+            UpgradeKind::FaceNumberCardTowerAttackDamageMultiply { face, .. } => {
+                format!(
+                    "{} 카드 타워 공격력 배수 증가",
+                    if *face { "그림" } else { "숫자" }
+                )
             }
-            UpgradeKind::FaceNumberCardTowerAttackSpeedPlus { .. } => {
-                "그림/숫자 카드 타워 공격 속도 증가".to_string()
+            UpgradeKind::FaceNumberCardTowerAttackSpeedPlus { face, .. } => {
+                format!(
+                    "{} 카드 타워 공격 속도 증가",
+                    if *face { "그림" } else { "숫자" }
+                )
             }
-            UpgradeKind::FaceNumberCardTowerAttackSpeedMultiply { .. } => {
-                "그림/숫자 카드 타워 공격 속도 배수 증가".to_string()
+            UpgradeKind::FaceNumberCardTowerAttackSpeedMultiply { face, .. } => {
+                format!(
+                    "{} 카드 타워 공격 속도 배수 증가",
+                    if *face { "그림" } else { "숫자" }
+                )
             }
-            UpgradeKind::FaceNumberCardTowerAttackRangePlus { .. } => {
-                "그림/숫자 카드 타워 공격 범위 증가".to_string()
+            UpgradeKind::FaceNumberCardTowerAttackRangePlus { face, .. } => {
+                format!(
+                    "{} 카드 타워 공격 범위 증가",
+                    if *face { "그림" } else { "숫자" }
+                )
             }
-            UpgradeKind::ShortenStraightFlushTo4Cards => "스트레이트를 4장으로 단축".to_string(),
+            UpgradeKind::ShortenStraightFlushTo4Cards => {
+                "스트레이트와 플러시를 4장으로 단축".to_string()
+            }
             UpgradeKind::SkipRankForStraight => "스트레이트에서 랭크 하나 건너뛰기".to_string(),
             UpgradeKind::TreatSuitsAsSame => "색이 같으면 같은 문양으로 취급".to_string(),
             UpgradeKind::RerollTowerAttackDamagePlus { .. } => {
@@ -149,105 +188,186 @@ impl UpgradeKind {
             UpgradeKind::SuitAttackRangePlus { suit, range_plus } => {
                 format!("{suit} 공격 범위가 {range_plus:.1} 증가합니다.")
             }
-            UpgradeKind::HandAttackDamagePlus { .. } => "족보 공격력이 증가합니다.".to_string(),
-            UpgradeKind::HandAttackDamageMultiply { .. } => {
-                "족보 공격력이 배수로 증가합니다.".to_string()
+            UpgradeKind::HandAttackDamagePlus {
+                tower_kind,
+                damage_plus,
+            } => {
+                format!("{tower_kind}타워 공격력이 {damage_plus:.0} 증가합니다.")
             }
-            UpgradeKind::HandAttackSpeedPlus { .. } => "족보 공격 속도가 증가합니다.".to_string(),
-            UpgradeKind::HandAttackSpeedMultiply { .. } => {
-                "족보 공격 속도가 배수로 증가합니다.".to_string()
+            UpgradeKind::HandAttackDamageMultiply {
+                tower_kind,
+                damage_multiplier,
+            } => format!("{tower_kind}타워 공격력이 {damage_multiplier:.1}배 증가합니다."),
+            UpgradeKind::HandAttackSpeedPlus {
+                tower_kind,
+                speed_plus,
+            } => {
+                format!("{tower_kind}타워 공격 속도가 {speed_plus:.0} 증가합니다.")
             }
-            UpgradeKind::HandAttackRangePlus { .. } => "족보 공격 범위가 증가합니다.".to_string(),
+            UpgradeKind::HandAttackSpeedMultiply {
+                tower_kind,
+                speed_multiplier,
+            } => format!("{tower_kind}타워 공격 속도가 {speed_multiplier:.1}배 증가합니다."),
+            UpgradeKind::HandAttackRangePlus {
+                tower_kind,
+                range_plus,
+            } => {
+                format!("{tower_kind}타워 공격 범위가 {range_plus:.1} 증가합니다.")
+            }
             UpgradeKind::ShopSlotExpansion => "상점 슬롯이 확장됩니다.".to_string(),
             UpgradeKind::QuestSlotExpansion => "퀘스트 슬롯이 확장됩니다.".to_string(),
             UpgradeKind::QuestBoardExpansion => "퀘스트 게시판이 확장됩니다.".to_string(),
             UpgradeKind::RerollCountPlus => "리롤 횟수가 증가합니다.".to_string(),
-            UpgradeKind::LowCardTowerDamagePlus { .. } => {
-                "적은 카드 타워의 공격력이 증가합니다.".to_string()
+            UpgradeKind::LowCardTowerDamagePlus { damage_plus } => {
+                format!(
+                    "3장 이하로 타워를 만들 때 타워의 공격력이 {damage_plus:.0} 증가합니다. 이 효과는 타워를 새로 만들 때 적용됩니다."
+                )
             }
-            UpgradeKind::LowCardTowerDamageMultiply { .. } => {
-                "적은 카드 타워의 공격력이 배수로 증가합니다.".to_string()
+            UpgradeKind::LowCardTowerDamageMultiply { damage_multiplier } => {
+                format!(
+                    "3장 이하로 타워를 만들 때 타워의 공격력이 {damage_multiplier:.1}배 증가합니다. 이 효과는 타워를 새로 만들 때 적용됩니다."
+                )
             }
-            UpgradeKind::LowCardTowerAttackSpeedPlus { .. } => {
-                "적은 카드 타워의 공격 속도가 증가합니다.".to_string()
+            UpgradeKind::LowCardTowerAttackSpeedPlus { speed_plus } => {
+                format!(
+                    "3장 이하로 타워를 만들 때 타워의 공격 속도가 {speed_plus:.0} 증가합니다. 이 효과는 타워를 새로 만들 때 적용됩니다."
+                )
             }
-            UpgradeKind::LowCardTowerAttackSpeedMultiply { .. } => {
-                "적은 카드 타워의 공격 속도가 배수로 증가합니다.".to_string()
+            UpgradeKind::LowCardTowerAttackSpeedMultiply { speed_multiplier } => {
+                format!(
+                    "3장 이하로 타워를 만들 때 타워의 공격 속도가 {speed_multiplier:.1}배 증가합니다. 이 효과는 타워를 새로 만들 때 적용됩니다."
+                )
             }
-            UpgradeKind::LowCardTowerAttackRangePlus { .. } => {
-                "적은 카드 타워의 공격 범위가 증가합니다.".to_string()
+            UpgradeKind::LowCardTowerAttackRangePlus { range_plus } => {
+                format!(
+                    "3장 이하로 타워를 만들 때 타워의 공격 범위가 {range_plus:.1} 증가합니다. 이 효과는 타워를 새로 만들 때 적용됩니다."
+                )
             }
             UpgradeKind::ShopItemPriceMinus => "상점 아이템 가격이 감소합니다.".to_string(),
             UpgradeKind::ShopRefreshPlus => "상점 새로고침 횟수가 증가합니다.".to_string(),
             UpgradeKind::QuestBoardRefreshPlus => {
                 "퀘스트 게시판 새로고침 횟수가 증가합니다.".to_string()
             }
-            UpgradeKind::NoRerollTowerAttackDamagePlus { .. } => {
-                "리롤하지 않으면 타워 공격력이 증가합니다.".to_string()
+            UpgradeKind::NoRerollTowerAttackDamagePlus { damage_plus } => {
+                format!(
+                    "리롤하지 않고 타워를 만들면 타워의 공격력이 {damage_plus:.0} 증가합니다. 이 효과는 타워를 새로 만들 때 적용됩니다."
+                )
             }
-            UpgradeKind::NoRerollTowerAttackDamageMultiply { .. } => {
-                "리롤하지 않으면 타워 공격력이 배수로 증가합니다.".to_string()
+            UpgradeKind::NoRerollTowerAttackDamageMultiply { damage_multiplier } => {
+                format!(
+                    "리롤하지 않고 타워를 만들면 타워의 공격력이 {damage_multiplier:.1}배 증가합니다. 이 효과는 타워를 새로 만들 때 적용됩니다."
+                )
             }
-            UpgradeKind::NoRerollTowerAttackSpeedPlus { .. } => {
-                "리롤하지 않으면 타워 공격 속도가 증가합니다.".to_string()
+            UpgradeKind::NoRerollTowerAttackSpeedPlus { speed_plus } => {
+                format!(
+                    "리롤하지 않고 타워를 만들면 타워의 공격 속도가 {speed_plus:.0} 증가합니다. 이 효과는 타워를 새로 만들 때 적용됩니다."
+                )
             }
-            UpgradeKind::NoRerollTowerAttackSpeedMultiply { .. } => {
-                "리롤하지 않으면 타워 공격 속도가 배수로 증가합니다.".to_string()
+            UpgradeKind::NoRerollTowerAttackSpeedMultiply { speed_multiplier } => {
+                format!(
+                    "리롤하지 않고 타워를 만들면 타워의 공격 속도가 {speed_multiplier:.1}배 증가합니다. 이 효과는 타워를 새로 만들 때 적용됩니다."
+                )
             }
-            UpgradeKind::NoRerollTowerAttackRangePlus { .. } => {
-                "리롤하지 않으면 타워 공격 범위가 증가합니다.".to_string()
+            UpgradeKind::NoRerollTowerAttackRangePlus { range_plus } => {
+                format!(
+                    "리롤하지 않고 타워를 만들면 타워의 공격 범위가 {range_plus:.1} 증가합니다. 이 효과는 타워를 새로 만들 때 적용됩니다."
+                )
             }
-            UpgradeKind::EvenOddTowerAttackDamagePlus { .. } => {
-                "짝수/홀수 타워의 공격력이 증가합니다.".to_string()
+            UpgradeKind::EvenOddTowerAttackDamagePlus { even, damage_plus } => {
+                format!(
+                    "{} 타워의 공격력이 {damage_plus:.0} 증가합니다.",
+                    if *even { "짝수" } else { "홀수" }
+                )
             }
-            UpgradeKind::EvenOddTowerAttackDamageMultiply { .. } => {
-                "짝수/홀수 타워의 공격력이 배수로 증가합니다.".to_string()
+            UpgradeKind::EvenOddTowerAttackDamageMultiply {
+                even,
+                damage_multiplier,
+            } => format!(
+                "{} 타워의 공격력이 {damage_multiplier:.1}배 증가합니다.",
+                if *even { "짝수" } else { "홀수" }
+            ),
+            UpgradeKind::EvenOddTowerAttackSpeedPlus { even, speed_plus } => {
+                format!(
+                    "{} 타워의 공격 속도가 {speed_plus:.0} 증가합니다.",
+                    if *even { "짝수" } else { "홀수" }
+                )
             }
-            UpgradeKind::EvenOddTowerAttackSpeedPlus { .. } => {
-                "짝수/홀수 타워의 공격 속도가 증가합니다.".to_string()
+            UpgradeKind::EvenOddTowerAttackSpeedMultiply {
+                even,
+                speed_multiplier,
+            } => format!(
+                "{} 타워의 공격 속도가 {speed_multiplier:.1}배 증가합니다.",
+                if *even { "짝수" } else { "홀수" }
+            ),
+            UpgradeKind::EvenOddTowerAttackRangePlus { even, range_plus } => {
+                format!(
+                    "{} 타워의 공격 범위가 {range_plus:.1} 증가합니다.",
+                    if *even { "짝수" } else { "홀수" }
+                )
             }
-            UpgradeKind::EvenOddTowerAttackSpeedMultiply { .. } => {
-                "짝수/홀수 타워의 공격 속도가 배수로 증가합니다.".to_string()
+            UpgradeKind::FaceNumberCardTowerAttackDamagePlus { face, damage_plus } => {
+                format!(
+                    "{} 카드 타워의 공격력이 {damage_plus:.0} 증가합니다.",
+                    if *face { "그림" } else { "숫자" }
+                )
             }
-            UpgradeKind::EvenOddTowerAttackRangePlus { .. } => {
-                "짝수/홀수 타워의 공격 범위가 증가합니다.".to_string()
+            UpgradeKind::FaceNumberCardTowerAttackDamageMultiply {
+                face,
+                damage_multiplier,
+            } => format!(
+                "{} 카드 타워의 공격력이 {damage_multiplier:.1}배 증가합니다.",
+                if *face { "그림" } else { "숫자" }
+            ),
+            UpgradeKind::FaceNumberCardTowerAttackSpeedPlus { face, speed_plus } => {
+                format!(
+                    "{} 카드 타워의 공격 속도가 {speed_plus:.0} 증가합니다.",
+                    if *face { "그림" } else { "숫자" }
+                )
             }
-            UpgradeKind::FaceNumberCardTowerAttackDamagePlus { .. } => {
-                "그림/숫자 카드 타워의 공격력이 증가합니다.".to_string()
-            }
-            UpgradeKind::FaceNumberCardTowerAttackDamageMultiply { .. } => {
-                "그림/숫자 카드 타워의 공격력이 배수로 증가합니다.".to_string()
-            }
-            UpgradeKind::FaceNumberCardTowerAttackSpeedPlus { .. } => {
-                "그림/숫자 카드 타워의 공격 속도가 증가합니다.".to_string()
-            }
-            UpgradeKind::FaceNumberCardTowerAttackSpeedMultiply { .. } => {
-                "그림/숫자 카드 타워의 공격 속도가 배수로 증가합니다.".to_string()
-            }
-            UpgradeKind::FaceNumberCardTowerAttackRangePlus { .. } => {
-                "그림/숫자 카드 타워의 공격 범위가 증가합니다.".to_string()
+            UpgradeKind::FaceNumberCardTowerAttackSpeedMultiply {
+                face,
+                speed_multiplier,
+            } => format!(
+                "{} 카드 타워의 공격 속도가 {speed_multiplier:.1}배 증가합니다.",
+                if *face { "그림" } else { "숫자" }
+            ),
+            UpgradeKind::FaceNumberCardTowerAttackRangePlus { face, range_plus } => {
+                format!(
+                    "{} 카드 타워의 공격 범위가 {range_plus:.1} 증가합니다.",
+                    if *face { "그림" } else { "숫자" }
+                )
             }
             UpgradeKind::ShortenStraightFlushTo4Cards => {
-                "스트레이트 플러시를 4장으로 단축합니다.".to_string()
+                "스트레이트와 플러시를 4장으로 단축합니다.".to_string()
             }
             UpgradeKind::SkipRankForStraight => {
-                "스트레이트에서 랭크 하나를 건너뜁니다.".to_string()
+                "스트레이트를 만들 때 랭크 하나를 건너뛸 수 있습니다.".to_string()
             }
             UpgradeKind::TreatSuitsAsSame => "같은 색의 문양을 동일하게 취급합니다.".to_string(),
-            UpgradeKind::RerollTowerAttackDamagePlus { .. } => {
-                "리롤하면 타워 공격력이 증가합니다.".to_string()
+            UpgradeKind::RerollTowerAttackDamagePlus { damage_plus } => {
+                format!(
+                    "리롤할 때마다 타워의 공격력이 {damage_plus:.0} 증가합니다. 이 효과는 타워를 새로 만들 때 적용됩니다."
+                )
             }
-            UpgradeKind::RerollTowerAttackDamageMultiply { .. } => {
-                "리롤하면 타워 공격력이 배수로 증가합니다.".to_string()
+            UpgradeKind::RerollTowerAttackDamageMultiply { damage_multiplier } => {
+                format!(
+                    "리롤할 때마다 타워의 공격력이 {damage_multiplier:.1}배 증가합니다. 이 효과는 타워를 새로 만들 때 적용됩니다."
+                )
             }
-            UpgradeKind::RerollTowerAttackSpeedPlus { .. } => {
-                "리롤하면 타워 공격 속도가 증가합니다.".to_string()
+            UpgradeKind::RerollTowerAttackSpeedPlus { speed_plus } => {
+                format!(
+                    "리롤할 때마다 타워의 공격 속도가 {speed_plus:.0} 증가합니다. 이 효과는 타워를 새로 만들 때 적용됩니다."
+                )
             }
-            UpgradeKind::RerollTowerAttackSpeedMultiply { .. } => {
-                "리롤하면 타워 공격 속도가 배수로 증가합니다.".to_string()
+            UpgradeKind::RerollTowerAttackSpeedMultiply { speed_multiplier } => {
+                format!(
+                    "리롤할 때마다 타워의 공격 속도가 {speed_multiplier:.1}배 증가합니다. 이 효과는 타워를 새로 만들 때 적용됩니다."
+                )
             }
-            UpgradeKind::RerollTowerAttackRangePlus { .. } => {
-                "리롤하면 타워 공격 범위가 증가합니다.".to_string()
+            UpgradeKind::RerollTowerAttackRangePlus { range_plus } => {
+                format!(
+                    "리롤할 때마다 타워의 공격 범위가 {range_plus:.1} 증가합니다. 이 효과는 타워를 새로 만들 때 적용됩니다."
+                )
             }
         }
     }
