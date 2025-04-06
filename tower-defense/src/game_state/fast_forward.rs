6766462a50e4621c1d064218a6a96013d0d40843
bigfoodk <satcom1600@hanmail.net>
2025-04-07 02:47:21 +0900

    Fast forward up to x16

--- tower-defense/src/game_state/fast_forward.rs
+++ tower-defense/src/game_state/fast_forward.rs
@@ -7,6 +7,7 @@ pub enum FastForwardMultiplier {
     X2,
     X4,
     X8,
+    X16,
 }
 impl FastForwardMultiplier {
     pub fn next(self) -> Self {
@@ -14,7 +15,8 @@ impl FastForwardMultiplier {
             FastForwardMultiplier::X1 => FastForwardMultiplier::X2,
             FastForwardMultiplier::X2 => FastForwardMultiplier::X4,
             FastForwardMultiplier::X4 => FastForwardMultiplier::X8,
-            FastForwardMultiplier::X8 => FastForwardMultiplier::X8,
+            FastForwardMultiplier::X8 => FastForwardMultiplier::X16,
+            FastForwardMultiplier::X16 => FastForwardMultiplier::X16,
         }
     }
     pub fn prev(self) -> Self {
@@ -23,6 +25,7 @@ impl FastForwardMultiplier {
             FastForwardMultiplier::X2 => FastForwardMultiplier::X1,
             FastForwardMultiplier::X4 => FastForwardMultiplier::X2,
             FastForwardMultiplier::X8 => FastForwardMultiplier::X4,
+            FastForwardMultiplier::X16 => FastForwardMultiplier::X8,
         }
     }
     pub fn time_scale(self) -> NonZeroUsize {
@@ -31,6 +34,7 @@ impl FastForwardMultiplier {
             FastForwardMultiplier::X2 => 2,
             FastForwardMultiplier::X4 => 4,
             FastForwardMultiplier::X8 => 8,
+            FastForwardMultiplier::X16 => 16,
         })
         .unwrap()
     }
