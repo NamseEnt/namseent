3a7f5ddf795eedc2c1bc2e2b2ef8653a842f1284
namse <skatpgusskat@naver.com>
2025-04-11 04:42:26 +0000

    Particle System

--- namui/namui-prebuilt/src/lib.rs
+++ namui/namui-prebuilt/src/lib.rs
@@ -2,12 +2,12 @@ pub mod button;
 pub mod dropdown;
 pub mod event_trap;
 pub mod list_view;
+pub mod particle;
 pub mod rich_text;
 pub mod scroll_view;
 mod simple_rect;
 pub mod table;
 pub mod typography;
-pub mod vfs;
 pub mod vh_list_view;
 
 // TODO
