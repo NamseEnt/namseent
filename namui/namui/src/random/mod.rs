cf4397e559cee4a2e2d1c16b8974099faa72417c
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2025-04-11 13:51:40 +0900

    Rollback random

--- namui/namui/src/random/mod.rs
+++ namui/namui/src/random/mod.rs
@@ -1,15 +1,9 @@
 pub use namui_type::uuid;
-use rand::RngCore;
 
 pub fn random(length: usize) -> Vec<u8> {
+    use rand::RngCore;
+
     let mut array = vec![0u8; length];
     rand::thread_rng().fill_bytes(&mut array);
     array
-}
-
-pub fn rand_random<T>() -> T
-where
-    rand::distributions::Standard: rand::distributions::Distribution<T>,
-{
-    rand::random()
 }
