dab3dfec1c355e3672f33b924253d3cce9def40c
bigfoodk <satcom1600@hanmail.net>
2025-05-16 00:04:23 +0900

    Add icons

--- tower-defense/src/asset_loader/mod.rs
+++ tower-defense/src/asset_loader/mod.rs
@@ -1,17 +1,22 @@
-mod background_asset_loader;
-mod tower_asset_loader;
+pub mod background_asset_loader;
+pub mod icon_asset_loader;
+pub mod tower_asset_loader;
 
+pub use self::icon_asset_loader::IconAssetLoader;
 use background_asset_loader::{BackgroundAssetLoader, BackgroundAssetLoaderInitializer};
+use icon_asset_loader::IconAssetLoaderInitializer;
 use namui::*;
 use tower_asset_loader::{TowerAssetLoader, TowerAssetLoaderInitializer};
 
 pub static TOWER_ASSET_LOADER_ATOM: Atom<TowerAssetLoader> = namui::Atom::uninitialized();
 pub static BACKGROUND_ASSET_LOADER_ATOM: Atom<BackgroundAssetLoader> = namui::Atom::uninitialized();
+pub static ICON_ASSET_LOADER_ATOM: namui::Atom<IconAssetLoader> = namui::Atom::uninitialized();
 
 pub struct AssetLoader {}
 impl Component for AssetLoader {
     fn render(self, ctx: &namui::RenderCtx) {
         ctx.add(TowerAssetLoaderInitializer {});
         ctx.add(BackgroundAssetLoaderInitializer {});
+        ctx.add(IconAssetLoaderInitializer {});
     }
 }
