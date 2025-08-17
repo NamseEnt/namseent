e982f3d866d002048200c15c00400331ea3768ee
namse <namse@users.noreply.github.com>
2025-08-17 22:45:23 +0900

    y-axis monster

--- game-idea-lab/tack/src/lib.rs
+++ game-idea-lab/tack/src/lib.rs
@@ -7,24 +7,56 @@ pub fn main() {
 
 fn game(ctx: &RenderCtx) {
     let screen_wh = screen::size().into_type::<Px>();
+    let now = Instant::now();
+
     let (motion_fsm, set_motion_fsm) = ctx.state(|| MotionFsm::Idle);
-    let (parrying_signal, set_parrying_signal) = ctx.state::<Option<Instant>>(|| None);
     let (first_tick, set_first_tick) = ctx.state(|| true);
     let (last_key, set_last_key) = ctx.state(|| None);
+    let (monster_state, set_monster_state) = ctx.state(|| MonsterState::Idle);
+
+    let parrying_time = 0.5.sec();
+    let rollback_time = 0.3.sec();
+    let monster_idle_xy = Xy::new(screen_wh.width / 2., screen_wh.height / 4.);
+    let player_xy = Xy::new(screen_wh.width / 2., screen_wh.height * 3 / 4.);
+
+    let monster_xy = {
+        match monster_state.as_ref() {
+            MonsterState::Idle => monster_idle_xy,
+            MonsterState::Attacking { start_at } => {
+                let velocity = Per::new(player_xy - monster_idle_xy, parrying_time);
+                monster_idle_xy + velocity * (now - start_at)
+            }
+            MonsterState::Parried { start_xy, start_at } => {
+                let velocity = Per::new(monster_idle_xy - start_xy, rollback_time);
+                *start_xy + velocity * (now - start_at)
+            }
+        }
+    };
+
+    ctx.interval(
+        "handle monster timeout",
+        (60. / 1000.).sec(),
+        |_dt| match monster_state.as_ref() {
+            MonsterState::Attacking { start_at } => {
+                if now - start_at > parrying_time {
+                    set_monster_state.set(MonsterState::Idle);
+                }
+            }
+            MonsterState::Parried { start_at, .. } => {
+                if now - start_at > rollback_time {
+                    set_monster_state.set(MonsterState::Idle);
+                }
+            }
+            _ => {}
+        },
+    );
 
     ctx.interval("parrying", 3.sec(), |_dt| {
         if *first_tick {
             set_first_tick.set(false);
             return;
         }
-        if parrying_signal.is_some() {
-            set_parrying_signal.set(None);
-            set_motion_fsm.set(MotionFsm::Attacked {
-                start_at: Instant::now(),
-            });
-            return;
-        }
-        set_parrying_signal.set(Some(Instant::now()));
+        set_monster_state.set(MonsterState::Attacking { start_at: now });
     });
 
     ctx.on_raw_event(|event| {
@@ -40,21 +72,23 @@ fn game(ctx: &RenderCtx) {
             return;
         };
 
-        let Some(parrying_signal) = parrying_signal.as_ref() else {
+        let MonsterState::Attacking { start_at } = monster_state.as_ref() else {
             return;
         };
         let is_up = event.code == Code::ArrowUp;
-        let now = Instant::now();
-        let signal_dt = now - parrying_signal;
+        let signal_dt = now - start_at;
 
         if !is_up && signal_dt < 0.5.sec() {
             // do nothing. give chance.
             return;
         }
 
-        set_parrying_signal.set(None);
+        set_monster_state.set(MonsterState::Parried {
+            start_xy: monster_xy,
+            start_at: now,
+        });
 
-        let parrying_success = is_up && signal_dt < 1.sec();
+        let parrying_success = is_up && signal_dt < parrying_time;
         set_motion_fsm.set(if parrying_success {
             MotionFsm::Parring
         } else {
@@ -68,7 +102,7 @@ fn game(ctx: &RenderCtx) {
             return;
         };
 
-        if parrying_signal.is_some() {
+        if let MonsterState::Attacking { .. } = monster_state.as_ref() {
             return;
         }
 
@@ -76,8 +110,6 @@ fn game(ctx: &RenderCtx) {
         if !is_left_or_right {
             return;
         }
-
-        let now = Instant::now();
 
         match motion_fsm.as_ref() {
             MotionFsm::Idle => set_motion_fsm.set(MotionFsm::BasicAttack1),
@@ -114,17 +146,25 @@ fn game(ctx: &RenderCtx) {
         MotionFsm::PostParingCombo3 => "PostParingCombo3",
         MotionFsm::PostParingCombo4 => "PostParingCombo4",
     };
-    let parrying_text = match parrying_signal.as_ref() {
-        None => "_",
-        Some(_) => "Parrying!!",
-    };
     let keyboard_text = match last_key.as_ref() {
         None => "_".to_string(),
         Some(code) => code.to_string(),
     };
+
+    ctx.translate(monster_xy).add(simple_rect(
+        Wh::single(10.px()),
+        Color::TRANSPARENT,
+        0.px(),
+        match monster_state.as_ref() {
+            MonsterState::Idle => Color::GREEN,
+            MonsterState::Attacking { .. } => Color::RED,
+            MonsterState::Parried { .. } => Color::from_f01(1., 1., 0., 1.),
+        },
+    ));
+
     ctx.add(namui_prebuilt::typography::center_text(
         screen_wh,
-        format!("{}\n{}\n{}", motion_text, parrying_text, keyboard_text),
+        format!("{motion_text}\n{keyboard_text}"),
         Color::WHITE,
         100.int_px(),
     ));
@@ -150,4 +190,10 @@ enum MotionFsm {
     PostParingCombo2,
     PostParingCombo3,
     PostParingCombo4,
+}
+
+enum MonsterState {
+    Idle,
+    Attacking { start_at: Instant },
+    Parried { start_xy: Xy<Px>, start_at: Instant },
 }
