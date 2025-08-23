039a1b7deb197c21aba35ab815755072db90da50
bigfoodk <satcom1600@hanmail.net>
2025-08-23 22:32:30 +0900

    Show tower skill thumbnail

--- tower-defense/src/tower_selecting_hand/tower_preview/tower_skill.rs
+++ tower-defense/src/tower_selecting_hand/tower_preview/tower_skill.rs
@@ -1,3 +1,7 @@
+use crate::icon::IconKind;
+use crate::thumbnail::ThumbnailComposer;
+use crate::thumbnail::constants::OVERLAY_SIZE_RATIO;
+use crate::thumbnail::overlay_rendering::OverlayPosition;
 use crate::{
     game_state::tower::{TowerSkillKind, TowerSkillTemplate},
     game_state::use_game_state,
@@ -19,7 +23,6 @@ pub struct TowerSkillTemplateIcon<'a> {
     pub on_mouse_move_in_effect_icon: &'a dyn Fn(&TowerSkillTemplate, Xy<Px>),
     pub on_mouse_move_out_effect_icon: &'a dyn Fn(&TowerSkillTemplate),
 }
-// TODO: Use image instead of text
 impl Component for TowerSkillTemplateIcon<'_> {
     fn render(self, ctx: &RenderCtx) {
         let Self {
@@ -28,33 +31,19 @@ impl Component for TowerSkillTemplateIcon<'_> {
             on_mouse_move_in_effect_icon,
             on_mouse_move_out_effect_icon,
         } = self;
-        // let symbol = match skill.kind {
-        //     TowerSkillKind::NearbyTowerDamageMul { .. } => "E",
-        //     TowerSkillKind::NearbyTowerDamageAdd { .. } => "E",
-        //     TowerSkillKind::NearbyTowerAttackSpeedAdd { .. } => "H",
-        //     TowerSkillKind::NearbyTowerAttackSpeedMul { .. } => "H",
-        //     TowerSkillKind::NearbyTowerAttackRangeAdd { .. } => "R",
-        //     TowerSkillKind::NearbyMonsterSpeedMul { .. } => "D",
-        //     TowerSkillKind::MoneyIncomeAdd { .. } => "B",
-        //     TowerSkillKind::TopCardBonus { rank, .. } => match rank {
-        //         Rank::Seven => "7",
-        //         Rank::Eight => "8",
-        //         Rank::Nine => "9",
-        //         Rank::Ten => "10",
-        //         Rank::Jack => "J",
-        //         Rank::Queen => "Q",
-        //         Rank::King => "K",
-        //         Rank::Ace => "A",
-        //     },
-        // };
-        // ctx.add(typography::body::center(wh, symbol, palette::ON_SURFACE));
-        ctx.add(simple_rect(
+        // compose skill thumbnail using existing thumbnail composer utilities
+        let thumbnail = compose_skill_thumbnail(wh, &skill.kind);
+
+        // background frame + composed thumbnail
+        let bg = simple_rect(
             wh,
             palette::OUTLINE,
             1.px(),
             palette::SURFACE_CONTAINER_HIGH,
-        ))
-        .attach_event(|event| {
+        );
+        let composed = namui::render(vec![thumbnail, bg]);
+
+        ctx.add(composed).attach_event(|event| {
             match event {
                 Event::MouseMove { event } => {
                     match event.is_local_xy_in() {
@@ -68,6 +57,59 @@ impl Component for TowerSkillTemplateIcon<'_> {
                 _ => {}
             };
         });
+    }
+}
+
+fn compose_skill_thumbnail(wh: Wh<Px>, kind: &TowerSkillKind) -> RenderingTree {
+    match *kind {
+        TowerSkillKind::NearbyTowerDamageMul { .. } => ThumbnailComposer::new(wh)
+            .with_icon_base(IconKind::AttackDamage)
+            .add_icon_overlay(
+                IconKind::Multiply,
+                OverlayPosition::BottomRight,
+                OVERLAY_SIZE_RATIO,
+            )
+            .build(),
+        TowerSkillKind::NearbyTowerDamageAdd { .. } => ThumbnailComposer::new(wh)
+            .with_icon_base(IconKind::AttackDamage)
+            .add_plus_overlay()
+            .build(),
+        TowerSkillKind::NearbyTowerAttackSpeedAdd { .. } => ThumbnailComposer::new(wh)
+            .with_icon_base(IconKind::AttackSpeed)
+            .add_plus_overlay()
+            .build(),
+        TowerSkillKind::NearbyTowerAttackSpeedMul { .. } => ThumbnailComposer::new(wh)
+            .with_icon_base(IconKind::AttackSpeed)
+            .add_icon_overlay(
+                IconKind::Multiply,
+                OverlayPosition::BottomRight,
+                OVERLAY_SIZE_RATIO,
+            )
+            .build(),
+        TowerSkillKind::NearbyTowerAttackRangeAdd { .. } => ThumbnailComposer::new(wh)
+            .with_icon_base(IconKind::AttackRange)
+            .add_plus_overlay()
+            .build(),
+        TowerSkillKind::NearbyMonsterSpeedMul { mul, .. } => {
+            let dir_icon = if mul < 1.0 {
+                IconKind::Down
+            } else {
+                IconKind::Up
+            };
+            ThumbnailComposer::new(wh)
+                .with_icon_base(IconKind::MoveSpeed)
+                .add_icon_overlay(dir_icon, OverlayPosition::BottomRight, OVERLAY_SIZE_RATIO)
+                .build()
+        }
+        TowerSkillKind::MoneyIncomeAdd { .. } => ThumbnailComposer::new(wh)
+            .with_icon_base(IconKind::Gold)
+            .add_plus_overlay()
+            .build(),
+        TowerSkillKind::TopCardBonus { rank, .. } => ThumbnailComposer::new(wh)
+            .with_icon_base(IconKind::AttackDamage)
+            .add_rank_overlay(rank)
+            .add_plus_overlay()
+            .build(),
     }
 }
 
