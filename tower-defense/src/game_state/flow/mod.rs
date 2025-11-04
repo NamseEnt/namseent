dd3a8cdaba179f1857658d3ef531e73b08e4b15b
bigfoodk <satcom1600@hanmail.net>
2025-11-04 20:16:56 +0900

    Remove default barricades

--- tower-defense/src/game_state/flow/mod.rs
+++ tower-defense/src/game_state/flow/mod.rs
@@ -87,7 +87,7 @@ impl GameState {
     }
 
     pub fn goto_placing_tower(&mut self, tower_template: TowerTemplate) {
-        let barricade_count = 4 + self.stage_modifiers.get_barricade_cards_per_stage();
+        let barricade_count = self.stage_modifiers.get_barricade_cards_per_stage();
         let mut barricades = vec![];
         for _ in 0..barricade_count {
             barricades.push(TowerTemplate::barricade());
