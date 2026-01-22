ef4daf7ca0445654fd843a0149fb417c62d4205d
bigfoodK <38313680+bigfoodK@users.noreply.github.com>
2026-01-22 09:19:35 +0900

    Disable named monster system (#1227)

--- tower-defense/src/game_state/modal/mod.rs
+++ tower-defense/src/game_state/modal/mod.rs
@@ -1,18 +1,14 @@
-mod challenge_modal;
 mod settings;
 mod upgrade_board;
 
 #[cfg(feature = "debug-tools")]
 use crate::game_state::debug_tools::DebugToolsModal;
-use crate::game_state::modal::{
-    challenge_modal::ChallengeModal, settings::SettingsModal, upgrade_board::UpgradeBoardModal,
-};
+use crate::game_state::modal::{settings::SettingsModal, upgrade_board::UpgradeBoardModal};
 use namui::*;
 
 #[derive(State)]
 pub enum Modal {
     UpgradeBoard,
-    Challenge,
     Settings,
     #[cfg(feature = "debug-tools")]
     DebugTools,
@@ -22,7 +18,6 @@ impl Component for &Modal {
     fn render(self, ctx: &RenderCtx) {
         match self {
             Modal::UpgradeBoard => ctx.add(UpgradeBoardModal),
-            Modal::Challenge => ctx.add(ChallengeModal),
             Modal::Settings => ctx.add(SettingsModal),
             #[cfg(feature = "debug-tools")]
             Modal::DebugTools => ctx.add(DebugToolsModal),
