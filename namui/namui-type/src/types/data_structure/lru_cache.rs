914008f8d96730de21df9d53e2e482983cd01014
bigfood <satcom1600@hanmail.net>
2026-02-16 10:37:53 +0000

    Refactoring

--- namui/namui-type/src/types/data_structure/lru_cache.rs
+++ namui/namui-type/src/types/data_structure/lru_cache.rs
@@ -15,27 +15,19 @@ impl<Key: Hash + Eq + Clone, Value, const CAPACITY: usize> LruCache<Key, Value, 
         }
     }
 
+    fn map_mutex(&self) -> &Mutex<lru::LruCache<Key, Arc<Value>>> {
+        self.map
+            .get_or_init(|| Mutex::new(lru::LruCache::new(NonZeroUsize::new(CAPACITY).unwrap())))
+    }
+
+    fn with_map<R>(&self, f: impl FnOnce(&mut lru::LruCache<Key, Arc<Value>>) -> R) -> R {
+        let mut map = self.map_mutex().lock().unwrap();
+        f(&mut map)
+    }
+
     pub fn get_or_create(&self, key: &Key, create: impl FnOnce(&Key) -> Value) -> Arc<Value> {
-        let map = self
-            .map
-            .get_or_init(|| Mutex::new(lru::LruCache::new(NonZeroUsize::new(CAPACITY).unwrap())));
-
-        {
-            let mut map = map.lock().unwrap();
-            if let Some(value) = map.get(key) {
-                return value.clone();
-            }
-        }
-
-        let created = Arc::new(create(key));
-
-        let mut map = map.lock().unwrap();
-        if let Some(value) = map.get(key) {
-            value.clone()
-        } else {
-            map.put(key.clone(), created.clone());
-            created
-        }
+        self.get_or_try_create(key, |key| Some(create(key)))
+            .expect("get_or_create closure must always return a value")
     }
 
     pub fn get_or_try_create(
@@ -43,46 +35,30 @@ impl<Key: Hash + Eq + Clone, Value, const CAPACITY: usize> LruCache<Key, Value, 
         key: &Key,
         try_create: impl FnOnce(&Key) -> Option<Value>,
     ) -> Option<Arc<Value>> {
-        let map = self
-            .map
-            .get_or_init(|| Mutex::new(lru::LruCache::new(NonZeroUsize::new(CAPACITY).unwrap())));
-
-        {
-            let mut map = map.lock().unwrap();
-            if let Some(value) = map.get(key) {
-                return Some(value.clone());
-            }
+        if let Some(value) = self.with_map(|map| map.get(key).cloned()) {
+            return Some(value);
         }
 
         let created = Arc::new(try_create(key)?);
 
-        let mut map = map.lock().unwrap();
-        if let Some(value) = map.get(key) {
-            Some(value.clone())
-        } else {
-            map.put(key.clone(), created.clone());
-            Some(created)
-        }
+        Some(self.with_map(|map| {
+            if let Some(value) = map.get(key) {
+                value.clone()
+            } else {
+                map.put(key.clone(), created.clone());
+                created
+            }
+        }))
     }
 
     pub fn get(&self, key: &Key) -> Option<Arc<Value>> {
-        let map = self
-            .map
-            .get_or_init(|| Mutex::new(lru::LruCache::new(NonZeroUsize::new(CAPACITY).unwrap())));
-
-        let mut map = map.lock().unwrap();
-
-        map.get(key).cloned()
+        self.with_map(|map| map.get(key).cloned())
     }
 
     pub fn put(&self, key: Key, value: Value) {
-        let map = self
-            .map
-            .get_or_init(|| Mutex::new(lru::LruCache::new(NonZeroUsize::new(CAPACITY).unwrap())));
-
-        let mut map = map.lock().unwrap();
-
-        map.put(key, Arc::new(value));
+        self.with_map(|map| {
+            map.put(key, Arc::new(value));
+        });
     }
 }
 
